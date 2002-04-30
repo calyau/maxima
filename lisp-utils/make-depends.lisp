72837a3d388d598ea3976bad1a06cdf7544b7329
rtoy <rtoy>
2002-04-30 18:04:41 +0000

    Generate multiple lines for the dependency file instead of one very long line.  (Solution by James Amundson, to work around a line-length limit in some utilities.)

--- lisp-utils/make-depends.lisp
+++ lisp-utils/make-depends.lisp
@@ -1,14 +1,17 @@
 (in-package :mk)
 (defvar *oos-dependency-stream* t
   "Output stream for list-dependencies-operation")
+(defvar *oos-dependency-target* "target"
+  "Output target for list-dependencies-operation")
 (defun list-dependencies-operation (component force)
-  (format *oos-dependency-stream* " ~A.~A"
+  (format *oos-dependency-stream* "~A : ~A.~A~%"
+	  *oos-dependency-target*
 	  (component-source-pathname component)
 	  (component-source-extension component)))
 (component-operation :list-dependencies 'list-dependencies-operation)
 (defun create-dependency-file (target file)
   (setq dependency-file (open file :direction :output))
-  (format dependency-file "~A:" target)
+  (setq *oos-dependency-target* target)
   (setq *oos-dependency-stream* dependency-file)
   (setq oos-output (operate-on-system 'maxima :list-dependencies))
   (format dependency-file "~%")
