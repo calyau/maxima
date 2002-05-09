a5cd86574c2d8c2d9c3f6ecb652014f86b881784
rtoy <rtoy>
2002-05-09 19:42:22 +0000

    Export the needed routines for the Bessel K function.

--- src/numerical/slatec.lisp
+++ src/numerical/slatec.lisp
@@ -13,6 +13,9 @@
 
    ;; Bessel function: I
    "DBESI0" "DBESI1" "DBESI" "DBSI0E" "DBSI1E" "ZBESI"
+
+   ;; Bessel function : K
+   "DBESK0" "DBESK1" "DBESK" "ZBESK"
    
    ;; Airy functions
    "DAI"
