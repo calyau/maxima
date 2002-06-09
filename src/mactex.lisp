70f5736114f45aa68adff8e189ec7e191252e03d
amundson <amundson>
2002-06-09 22:40:42 +0000

    Utilities for mactex courtesy Barton Willis. Excerpts from his mail message:
    Here is utility that provides a maxima level interface to
    putprop. Specifically, texput allows a user to change
    the behavior of maxima's tex function.
    
    A few examples.
    
    /* tex mue tex as "\mu_e",  etc  */
    
    (C2) texput(mue, "\\mu_e")$
    (C3) texput(e, "\\mathbf{E}")$
    (C4) texput(b, "\\mathbf{B}")$
    
    /* Let's try it */
    (C5) tex([mue,e,b,e.b]);
    $$\left[ \mu_e,\mathbf{E},\mathbf{B},\mathbf{E}\cdot \mathbf{B} \right] $$
    (D5)                          FALSE
    
    /* Define a matchfix operator || and tex it as \|.  To do this,
    use three arguments to texput
    */
    
    (C6) matchfix("||","||")$
    (C7) texput("||","\\|","\\|")$
    (C8) tex(|| e + b ||);
    $$\|\mathbf{E}+\mathbf{B}\|$$
    (D8)                          FALSE
    
    /* One more time.  Before texput */
    
    (C9) matchfix("<<",">>")$
    (C10) tex(<<x,y>>);
    $$<<\left(x,y\right)$$
    (D10)                         FALSE
    (C11) texput("<<","\\langle ", " \\rangle ")$
    
    /* after texput */
    
    (C12) tex(<<x,y>>);
    $$\langle x,y \rangle $$
    (D12)                         FALSE
    (C13) tex(<<x>>);
    $$\langle x \rangle $$
    (D13)                         FALSE
    
    
    1.  I wrote this function this morning; it hasn't been thoroughly
    tested.  Let me know if you see any problems with it.
    
    2.  Commercial Macsyma used qput to do the same thing; it seemed
    to me that I'd have to modify mactex.lisp  in a handful of
    places to use qput in this way.

--- src/mactex.lisp
+++ src/mactex.lisp
@@ -850,3 +850,158 @@
 ;; hypergeometric fns, alternative summation notations  0<=n<=inf, etc.
 
 ;;Undone and really pretty hard: line breaking
+
+
+;; Additional mactex utilities.
+;; All functions except texput are based on code by Richard J. Fateman, 
+;; copyright 1987. Fateman's code was ported to Common Lisp by William 
+;; Schelter.  The texput function was written by Barton Willis.
+
+
+;; texput is a maxima-level interface to putprop that allows a user
+;; to modify the behavior of maxima's tex function. Here are a few eamples:
+
+;;        (C1) texput(me,"\\mu_e");
+;;        (D1) 				     \mu_e
+;;        (C2) tex(me);
+;;                $$\mu_e$$
+
+;;        (C4) texput("<<",[" \\langle ", " \\rangle "],matchfix);
+;;        (D4) 		         \langle ( \rangle)
+;;        (C5) matchfix("<<",">>");
+;;        (D5) 				     "<<"
+;;        (C6) tex(<<a>>);
+;;              $$ \langle a \rangle $$
+;;        (D6) 				     FALSE
+;;        (C7) tex(<<a,b>>);
+;;              $$ \langle a , b \rangle $$
+
+;;  To change the argument separtor from a comma to a character ch,
+;;  make the third element of the second argument of texput that character.  For
+;;  example, to make Dirac brackets, use
+
+;;        (C12) texput("<<",[" \\langle ", " \\rangle "," \\, | \\,"],matchfix);
+;;        (D12) 		         \langle ( \rangle ,  \, | \,)
+;;        (C13) tex(<<a>>);
+;;                    $$ \langle a \rangle $$
+;;        (D13) 				     FALSE
+;;        (C14) tex(<<a,b>>);
+;;                      $$ \langle a \, | \,b \rangle $$
+
+;;        (C15) texput(grad," \\nabla ",prefix);
+;;        (D15) 				      70
+;;        (C16) prefix("grad");
+;;        (D16) 				    "GRAD"
+;;        (C17) tex(grad f);
+;;                    $$ \nabla f$$
+
+;;        (C19) texput("~"," \\times ",infix);
+;;        (D19) 				      180
+;;        (C20) tex(a~b);
+;;                    $$a \times b$$
+
+;;        (C22) postfix(@);
+;;        (D22) 				      "@"
+;;        (C23) texput("@","!!",postfix);
+;;        (D23) 				      160
+;;        (C24) tex(x @);
+;;              $$x!!$$
+
+
+(defun $texput (e s &optional tx)
+
+  (cond ((mstringp e)
+	 (setq e (define-symbol (string-left-trim '(#\&) e)))))
+
+  (cond (($listp s)
+	 (setq s (margs s)))
+	(t
+	 (setq s (list s))))
+  
+  (setq s (mapcar #'stripdollar s))
+
+  (cond ((null tx)
+	 (putprop e (nth 0 s) 'texword))
+	
+	((eq tx '$matchfix)
+	 (putprop e 'tex-matchfix 'tex)
+	 (cond ((< (length s) 2)
+		(merror "Improper 2nd argument to TEXPUT for matchfix operator."))
+	       ((eq (length s) 2)
+		(putprop e (list (list (nth 0 s)) (nth 1 s)) 'texsym))
+	       (t
+		(putprop e (list (list (nth 0 s)) (nth 1 s) (nth 2 s)) 'texsym))))
+
+          ; The left and right binding powers may be wrong.
+
+	((eq tx '$prefix)
+	 (putprop e 'tex-prefix 'tex)
+	 (putprop e s 'texsym)
+	 (putprop e 200 'tex-lbp)
+	 (putprop e 180 'tex-rbp))
+		
+	((eq tx '$infix)
+	 (putprop e 'tex-infix 'tex)
+	 (putprop e  s 'texsym)
+	 (putprop e 200 'tex-lbp)
+	 (putprop e 180 'tex-rbp))
+
+	((eq tx '$postfix)
+	 (putprop e 'tex-postfix 'tex)
+	 (putprop e  s 'texsym)
+	 (putprop e 160 'tex-lbp))))
+
+;; If you want LaTeX style quotients, first load mactex and second
+;; define tex-mquotient as follows
+
+(defun tex-mquotient (x l r)
+  (if (or (null (cddr x)) (cdddr x)) (wna-err (caar x)))
+  (setq l (tex (cadr x) (append l '("\\frac{")) nil 'mparen 'mparen)
+	r (tex (caddr x) (list "}{") (append '("}") r) 'mparen 'mparen))
+  (append l r))
+
+;; The following defprop controls the space between terms in a product. 
+;; To use thin spaces, use 
+;;    (defprop mtrimes "\\, " texsym)
+;; and to use no space at all, use
+
+(defprop mtimes " " texsym)
+
+;; To use amsmath style matrices, define tex-matrix as
+;; (to TeX the code, you'll need to \usepackage{amsmath})
+
+(defun tex-matrix (x l r) ;; matrix looks like ((mmatrix)((mlist) a b) ...)
+  (append l `("\\begin{pmatrix}")
+	 (mapcan #'(lambda(y)
+			  (tex-list (cdr y) nil (list " \\\\ ") " & "))
+		 (cdr x))
+	 '("\\end{pmatrix}") r))
+
+;; Here is a version of tex-limit that doesn't ignore the direction of
+;; the limit; it uses down and up arrows to indicate the direction.
+
+(defun tex-limit(x l r)
+  (let* ((s1 (tex (cadr x) nil nil 'mparen rop))
+	 (arrow
+	  (cond ((eq (nth 4 x) '$minus)
+		 " \\,  \\downarrow  \\,")
+		((eq (nth 4 x) '$plus)
+		 " \\, \\uparrow \\, " )
+		(t
+		  " \\rightarrow ")))
+	 (subfun  (subst arrow '=
+			 (tex `((mequal simp) ,(caddr x),(cadddr x))
+			      nil nil 'mparen 'mparen))))
+    (append l `("\\lim_{" ,@subfun "}{" ,@s1 "}") r)))
+
+;; If you use texput with the optional argument separator 
+;; argument, you'll need to use this hacked version of tex-matchfix.
+
+(defun tex-matchfix (x l r)
+  (setq l (append l (car (texsym (caar x))))
+	;; car of texsym of a matchfix operator is the lead op
+	r (append (list (nth 1 (texsym (caar x)))) r)
+	;; cdr is the trailing op
+	x (tex-list (cdr x) nil r (or (nth 2 (texsym (caar x))) " , ")))
+  (append l x))
+
