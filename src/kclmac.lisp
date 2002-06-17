c52a0bb2fb7272e86aed78cadb7c56af08413cc9
vvzhy <vvzhy>
2002-06-17 19:23:58 +0000

    errset macro fix (for errcatch command) for new ansi gcl

--- src/kclmac.lisp
+++ src/kclmac.lisp
@@ -1,11 +1,17 @@
 (in-package "MAXIMA")
+#-(and gcl ansi-cl)
 (use-package "SERROR")
 
 (defvar errset nil)
 
+#-(and gcl ansi-cl)
 (defmacro errset (form &optional flag) flag
   `(cond-any-error (er)
 	       (list ,form)
 	       ((null errset) nil)))
 
+#+(and gcl ansi-cl)
+(defmacro errset (&rest l)
+   `(cl:handler-case (list ,(car l))
+     (cl:error (e) (if errset (error e)))))
 
