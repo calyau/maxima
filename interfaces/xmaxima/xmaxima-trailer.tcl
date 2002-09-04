b8d0c1acfabda750a2f5acf7b51d69ab635ed887
amundson <amundson>
2002-09-04 02:07:51 +0000

    Updates for windows from Mike Thomas.

--- interfaces/xmaxima/xmaxima-trailer.tcl
+++ interfaces/xmaxima/xmaxima-trailer.tcl
@@ -1,1 +1,19 @@
+if { "$tcl_platform(platform)" == "windows" } {
+    global ws_openMath
+     set ws_openMath(kill) [file join $ws_openMath(maxima_prefix) \
+          bin winkill.exe]
+    catch { load  [file join $ws_openMath(maxima_prefix) \
+          bin tclwinkill.dll]  }
+#    proc setIcon { w } {
+#     global ws_openMath
+#     winico set [winfo toplevel $w] [winico createfrom [file join \
+#             $ws_openMath(maxima_prefix) src max.ico ]]
+#    }
+#    after 2000 { setIcon . }
+#    after 6000 { setIcon . }
+>
+  } else {
+    set ws_openMath(kill) kill
+}
+
 doit .maxima
--- interfaces/xmaxima/xmaxima.tcl
+++ interfaces/xmaxima/xmaxima.tcl
@@ -12367,24 +12367,6 @@ array set xmaximaPreferences {fontAdjust 0 }
 catch { source ~/xmaxima.ini }
 
 
-if { "$tcl_platform(platform)" == "windows" } {
-    global ws_openMath
-     set ws_openMath(kill) [file join $ws_openMath(maxima_prefix) \
-	    src winkill.exe]
-    catch { load  [file join $ws_openMath(maxima_prefix) \
-	    src tclwinkill.dll]  }
-    proc setIcon { w } {
-	global ws_openMath
-	winico set [winfo toplevel $w] [winico createfrom [file join \
-		$ws_openMath(maxima_prefix) src max.ico ]]   
-    }
-    after 2000 { setIcon . }
-    after 6000 { setIcon . }
-    
-  } else {
-    set ws_openMath(kill) kill
-}
-
 proc getkey {key lis } {
    set tem [ lsearch $lis $key ]
     if { $tem >= 0} {lindex $lis [expr 1 + $tem]}
