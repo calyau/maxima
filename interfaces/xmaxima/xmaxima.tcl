9f3ce5f23d6804f8e40f9f90e04f5eb7c566867f
mikeclarkson <mikeclarkson>
2002-09-05 01:48:21 +0000

    Fixed bug 604832 - xmaxima can kill Xwindows

--- interfaces/xmaxima/xmaxima.tcl
+++ interfaces/xmaxima/xmaxima.tcl
@@ -1,7 +1,16 @@
 # -*-mode: tcl; fill-column: 75; tab-width: 8; coding: iso-latin-1-unix -*-
 #
-#       $Id: xmaxima.tcl,v 1.6 2002-09-04 23:00:43 mikeclarkson Exp $
+#       $Id: xmaxima.tcl,v 1.7 2002-09-05 01:48:21 mikeclarkson Exp $
 #
+
+# The following files are prepended, and could be sourced instead.
+# The only problem about sourcing them is that the way of finding
+# the directory they're in may differ in a wrapped executable.
+# Note that the order of required files may be important.
+
+# Source Tkmaxima/Constants.tcl ;# required - must not be autoloaded
+
+
 ###### maxima-browser.tcl ######
 
 ## source maxima-local.tcl
@@ -14,7 +23,6 @@
 #proc start_program { } {runOneMaxima .temp } ; source maxima-local.tcl; set argv "maxima 10 billy -debug" ; set argc 4 ; set argv0 ./run-one.tcl ; source run-one.tcl
 # on other side ; openConnection localhost 5099 billy maxima
 
-set dontstart 1
 ## source preamble.tcl
 
 ###### preamble.tcl ######
@@ -12007,69 +12015,6 @@ proc markForProgram { w args } {
 set ws_openMath(counter) 0
 	
 # $w.text configure -state disabled
-if { ![info exists dontstart] } {
-## source "startup.tcl"
-
-###### startup.tcl ######
-############################################################
-# Netmath       Copyright (C) 1998 William F. Schelter     #
-# For distribution under GNU public License.  See COPYING. # 
-############################################################
-# try to fix up so that we dont write to stdout in the browser.. 
-
-# source preamble.tcl
-
-if { "[info command wm]" != "wm" } {
-        proc wm { args } {}
-    }
-if { [info exists embed_args(width)] || [info exists embed_args(embed_mode)]} {
-  set ws_openMath(inbrowser) 1
-# rebind puts    
- rename puts _joel
- proc puts {args} {
-    global badout ;
-     switch [llength $args] {
-	 2 {_joel [lindex $args 0] [lindex $args 1]}
-	 1 {append badout $args}
-	 3 {_joel [lindex $args 0] [lindex $args 1] [lindex $args 2]}
-  }
- }
-  
-#  rename flush _flush1
-#  proc flush { x } {
-#       if { "$x" != "stdout" } {
-# 	  _flush1 $x
-#       }
-#   }
-
-}
-
-set toeval "OpenMathOpenUrl http://www.ma.utexas.edu/users/wfs/netmath/netmath.html"
-
-
-catch { if { "[lindex $argv 0]" != "" } {
-    wm minsize . 500 400
-    after 7000  wm minsize . 0 0
-    set toeval "OpenMathOpenUrl [lindex $argv 0]"
-    
-}
-}
-catch { set toeval [assoc eval [getattr browserArgs]] }
-
-if { [catch { eval $toeval } err ] }  {
-    if { [regexp "unreachable|couldn't open" $err] 
-        &&  (1 || [catch { socket www.yahoo.com 80 } ])
-       && (![info exists ws_openMath(proxy,http)] ||
-           [catch { eval socket $ws_openMath(proxy,http) } ]) } {
-	if { 0 == [tk_dialog .jil 0 "Connection Problem:$err" "" 0 \
-	 {Configure proxy server?} {Connect Later}] } {
-	     fontDialog .top
-	 }
-     } else {error "$toeval:\n   error: $err" }
- }
-
-## endsource "startup.tcl"
-}
 ########
 
 ## endsource preamble.tcl
@@ -12768,11 +12713,14 @@ proc closeMaxima { win } {
     global pdata
     linkLocal $win maximaSocket pid 
     foreach v [array names pdata maxima*] { unset pdata($v) }
-    
-    catch {
-	CMkill -TERM $pid
-	unset pid
-     }
+
+    if {$pid != "" && [string is int $pid]} {
+	catch {
+	    CMkill -TERM $pid
+	    unset pid
+	}
+    }
+
     catch {    close $maximaSocket
                  unset maximaSocket
       }
