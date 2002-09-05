17cd0db0f46af4fa9d24a0d3fdf5a9b6cce60cbe
mikeclarkson <mikeclarkson>
2002-09-05 08:28:06 +0000

    Fix type in windows code and cleanup

--- interfaces/xmaxima/xmaxima-trailer.tcl
+++ interfaces/xmaxima/xmaxima-trailer.tcl
@@ -1,18 +1,21 @@
-if { "$tcl_platform(platform)" == "windows" } {
-    global ws_openMath
-     set ws_openMath(kill) [file join $ws_openMath(maxima_prefix) \
-          bin winkill.exe]
-    catch { load  [file join $ws_openMath(maxima_prefix) \
-          bin tclwinkill.dll]  }
-#    proc setIcon { w } {
-#     global ws_openMath
-#     winico set [winfo toplevel $w] [winico createfrom [file join \
-#             $ws_openMath(maxima_prefix) src max.ico ]]
-#    }
-#    after 2000 { setIcon . }
-#    after 6000 { setIcon . }
->
-  } else {
+# -*-mode: tcl; fill-column: 75; tab-width: 8; coding: iso-latin-1-unix -*-
+#
+#       $Id: xmaxima-trailer.tcl,v 1.3 2002-09-05 08:28:06 mikeclarkson Exp $
+#
+# Attach this at the bottom of the xmaxima code to start up the interface.
+
+global tcl_platform ws_openMath
+if {$tcl_platform(platform) == "windows" } {
+
+    set ws_openMath(kill) [file join $ws_openMath(maxima_prefix) \
+			       bin winkill.exe]
+    set file [file join $ws_openMath(maxima_prefix) bin tclwinkill.dll]
+    if {[file isfile $file]} {
+	catch {load  $file}
+    }
+    unset file
+} else {
+    # unix
     set ws_openMath(kill) kill
 }
 
