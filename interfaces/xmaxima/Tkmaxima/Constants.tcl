754ce69da0265af5e6f9889a7695e9bd26585a6f
mikeclarkson <mikeclarkson>
2002-09-05 19:55:42 +0000

    Begin to break up xmaxima.tcl and gather global constants in one place

---
+++ interfaces/xmaxima/Tkmaxima/Constants.tcl
@@ -0,0 +1,8 @@
+# -*-mode: tcl; fill-column: 75; tab-width: 8; coding: iso-latin-1-unix -*-
+#
+#       $Id: Constants.tcl,v 1.1 2002-09-05 19:55:42 mikeclarkson Exp $
+#
+
+global ws_openMath
+set ws_openMath(date) 04/28/2002
+
