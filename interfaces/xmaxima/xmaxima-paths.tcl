24f97f2eac56eacc1d0662ebb7835e10b0c35ef5
mikeclarkson <mikeclarkson>
2002-09-07 23:17:58 +0000

    Bring constants derived from enviroment variables up to setMaxDir

--- interfaces/xmaxima/xmaxima-paths.tcl
+++ interfaces/xmaxima/xmaxima-paths.tcl
@@ -1,6 +1,6 @@
 # -*-mode: tcl; fill-column: 75; tab-width: 8; coding: iso-latin-1-unix -*-
 #
-#       $Id: xmaxima-paths.tcl,v 1.4 2002-09-07 10:33:35 mikeclarkson Exp $
+#       $Id: xmaxima-paths.tcl,v 1.5 2002-09-07 23:17:58 mikeclarkson Exp $
 #
 # Attach this near the bottom of the xmaxima code to find the paths needed
 # to start up the interface.
@@ -61,6 +61,32 @@ proc setMaxDir {} {
 	set ws_openMath(xmaxima_maxima) $env(XMAXIMA_MAXIMA) 
     } else {
 	set ws_openMath(xmaxima_maxima) maxima
+    }
+
+    # Bring derived quantities up here too so we can see the
+    # side effects of setting the above variables
+
+    # used in Menu.tcl
+    set ws_openMath(pReferenceToc) \
+	[file join $ws_openMath(maximaPath) info maxima_toc.html]
+
+    # used in RunMaxima.tcl, was deined in xmaxima.tcl
+    if { [auto_execok $ws_openMath(xmaxima_maxima)] != "" } {
+	#mike FIXME: This should break on windows if there is a space in the pathname
+	set ws_openMath(localMaximaServer) "$ws_openMath(xmaxima_maxima) $maxima_opts -p [file join $ws_openMath(maxima_xmaximadir) server.lisp] -r \":lisp (progn (user::setup PORT)(values))\" &"
+    } elseif { [info exists env(XMAXIMA_MAXIMA)] } {
+	tide_faulure "Error. maxima executable XMAXIMA_MAXIMA=$env(XMAXIMA_MAXIMA) not found."
+	exit 1
+    } else {
+	# A gruesome hack. Normally, we communicate to the maxima image
+	# through the maxima script, as above. If the maxima script is not
+	# available, as may happen on windows, directly talk to the GCL 
+	# saved image. jfa 04/28/2002
+	set env(MAXIMA_INT_LISP_PRELOAD) \
+	    "[file join $ws_openMath(maxima_xmaximadir) server.lisp]"
+	set env(MAXIMA_INT_INPUT_STRING) \
+	    ":lisp (progn (user::setup PORT)(values));"
+	set ws_openMath(localMaximaServer) "[file join $ws_openMath(maxima_verpkglibdir) binary-gcl maxima] -eval \"(run)\" -f &"
     }
 }
 
