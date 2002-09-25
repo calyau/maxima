3d9243606bcc188fc0c0483c092af9192bbd46ee
amundson <amundson>
2002-09-25 02:17:20 +0000

    Run rtestode last. Otherwise, it interferes with two other tests.
    This, in itself, deserves investigation, but it will have to be
    investigated after 5.9.0.

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -24,13 +24,12 @@
 (time 
  (sloop with errs = '() for testv in 
 	'(
-          "rtestode"
 	  "rtest1" "rtest1a" "rtest2" "rtest3" "rtest4" "rtest5"
 	  "rtest6" "rtest6a" "rtest6b" "rtest7"
 	  "rtest8"
 	  "rtest9"
 	  "rtest9a" "rtest10" "rtest11" "rtest12" "rtest13" "rtest13s"
-	  "rtest14" "rtest15" "rexamples" 
+	  "rtest14" "rtest15" "rexamples" "rtestode"
 	  )
 	do
 	(format t "~%Testing ~a.mac" testv)
