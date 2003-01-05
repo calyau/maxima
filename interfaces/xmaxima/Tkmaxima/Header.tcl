4176b2f2901eb0407925fb96fc9d62a19e2268e6
amundson <amundson>
2003-01-05 19:46:09 +0000

    Header.tcl is now generated from Header.tcl.in.

--- interfaces/xmaxima/Tkmaxima/Header.tcl
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#  -*- mode: tcl; fill-column: 75; tab-width: 8; coding: iso-latin-1-unix -*- \
-exec wish "$0" "$@"
-#
-#       $Id: Header.tcl,v 1.3 2002-09-13 17:26:26 mikeclarkson Exp $
-#
-# Header to allow for Unix execution
-#
-
