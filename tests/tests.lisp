d6ab3aaec61fae36cb3d984adb0ddd7b9a5e035f
billingd <billingd>
2003-03-26 22:40:58 +0000

    2003-03-27  David Billinghurst <David.Billinghurst@riotinto.com>
    	* tests/test.lisp:  Reorder test files to reduce interaction.
    	That is, run the ones that cause problems last.
    	* tests/rtest13.mac:  Start with kill(all)

--- tests/tests.lisp
+++ tests/tests.lisp
@@ -24,12 +24,12 @@
 (time 
  (sloop with errs = '() for testv in 
 	'(
-	  "rtest1" "rtest1a" "rtest2" "rtest3" "rtest4" "rtest5"
+	  "rtest1" "rtest1a" "rtest2" "rtest4" "rtest5"
 	  "rtest6" "rtest6a" "rtest6b" "rtest7"
-	  "rtest8"
 	  "rtest9"
-	  "rtest9a" "rtest10" "rtest11" "rtest12" "rtest13" "rtest13s"
-	  "rtest14" "rtest15" "rexamples" "rtestode" "rtestflatten"
+	  "rtest9a" "rtest10" "rtest11" "rtest13" "rtest13s"
+	  "rtest14" "rtest15" "rtestode" "rtestflatten"
+	  "rtest3" "rtest8" "rtest12" "rexamples"
 	  )
 	do
 	(format t "~%Testing ~a.mac" testv)
