b366a4f3a3085ba187f59c603ee32f8b017f89c7
wjenkner <wjenkner>
2003-03-27 01:18:58 +0000

    Contains various new definitions.

---
+++ share/affine/compat.lisp
@@ -0,0 +1,17 @@
+(in-package "MAXIMA")
+
+(setf (symbol-function 'ferror) (symbol-function 'merror))
+(setf (symbol-function 'zl-listp) (symbol-function 'listp))
+
+
+;; (format t "~%The functions ~/maxima::tilde-q-fsh/ are inverses"
+;;	(st-rat #$[x+1,1/(x+1)]$))
+;; should print: The functions x+1 and 1/(x+1) are inverses
+
+;; See the last part of polyb for the definition of FSH.
+
+(defun tilde-q-fsh (stream arg colonp at-p) ;atp is special...
+  (declare (ignore colonp at-p))
+  (fsh arg stream))
+
+;;; compat.lisp ends here
