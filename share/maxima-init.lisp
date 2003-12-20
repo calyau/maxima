ad13814bfb74438bcb4215dd5877ebbc0968391e
vvzhy <vvzhy>
2003-12-20 18:18:11 +0000

    New init file for better GCL performance

--- share/maxima-init.lisp
+++ share/maxima-init.lisp
@@ -1,8 +1,33 @@
-;;pre-allocating memory substantial improves GCL performance
+;; Pre-allocating memory and more aggressive allocate growth 
+;; parameters substantially improves GCL performance
 #+gcl
 (progn
-  (si::allocate-relocatable-pages 2000 t)
-  (si::allocate 'cfun 200 t)
-  (si::allocate 'fixnum 200 t)
-  (si::allocate 'cons 400 t)
-  (si::allocate 'symbol 100 t))
+
+  #+gmp (si::set-gmp-allocate-relocatable t)
+
+  ;; If you really need smaller initial memory footprint 
+  ;; comment these pre-allocations out 
+  (si::allocate-relocatable-pages 400 t)
+  (si::allocate 'cons 	1000 t)
+  (si::allocate 'fixnum 100  t)
+  (si::allocate 'symbol 200  t)
+  (si::allocate 'array	100  t)
+  (si::allocate 'string	200  t)
+  (si::allocate 'cfun 	400  t)
+
+  ;; It is not recommended to make hole size less than 1000
+  (si::set-hole-size 4000)
+
+  (si::allocate-growth 'cons 	1 10000 66 33)
+  (si::allocate-growth 'fixnum 	1 10000 66 33)
+  (si::allocate-growth 'symbol 	1 10000 66 33)
+  (si::allocate-growth 'package	1 10000 66 33)
+  (si::allocate-growth 'array 	1 10000 66 33)
+  (si::allocate-growth 'string 	1 10000 66 33)
+  (si::allocate-growth 'cfun 	1 10000 66 33)
+  (si::allocate-growth 'sfun 	1 10000 66 33)
+  
+  (si::allocate-growth 'relocatable  1 10000 66 33)
+  ;(si::allocate-growth 'contiguous   1 10000 66 33)
+
+  )
