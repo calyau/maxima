4f0fe98c45022e94c461dff66b7d02485191c4b5
amundson <amundson>
2004-02-17 03:44:46 +0000

    rudimentary routines to assist in building maxima without autotools.

---
+++ src/maxima-build.lisp
@@ -0,0 +1,8 @@
+(load "../lisp-utils/defsystem.lisp")
+(defun maxima-compile ()
+  (mk:oos "maxima" :compile))
+(defun maxima-load ()
+  (mk:oos "maxima" :load))
+(defun maxima-dump ()
+  #+clisp(ext:saveinitmem "binary-clisp/maxima.mem" 
+		   :init-function (function user::run)))
