4d04bc41773080535ed63ecebf8ab25a12349140
wjenkner <wjenkner>
2004-04-25 03:03:10 +0000

    New files from Valerij Pipin (most of the commit message is taken from a description he sent me in private email).
    lch.mac: More flexible indicial Levi-Civita (permutation) tensor
    package.
    
    ex_calc.mac: Definitions for exterior calculus: exterior product,
    interior product, exterior derivative and the Lie derivative.
    
    ex_calc.dem: illustrates them in action.
    
    car_iden.dem: demonstrates the Cartan identity (commutation of the Lie
    derivative with the exterior one) for order 1,2,3,4 and 5.
    
    ten_alg.dem: demonstrates all the current power of the itensor package
    with respect to the mentioned additions.
    
    tentex.lisp: Some tensor stuff for $tex.

---
+++ share/tensor/tentex.lisp
@@ -0,0 +1,34 @@
+(in-package "MAXIMA")
+
+#+cl
+($bothcases t) ;; allow alpha and Alpha to be different
+;(declare-top
+;	 (special lop rop ccol $gcprint texport $labels $inchar
+;		  vaxima-main-dir
+;		  )
+;	 (*expr tex-lbp tex-rbp))
+
+;; top level command the result of tex'ing the expression x.
+;; Lots of messing around here to get C-labels verbatim printed
+;; and function definitions verbatim "ground"
+
+;(defmspec $tex(l) ;; mexplabel, and optional filename
+;  (let ((args (cdr l)))
+;  (apply 'tex1  args)))
+(defprop $Dl "\\Delta" texword)
+(defprop $divG "(\\nabla\\cdot G)" texword)
+(defprop $OG "(\\Omega\\cdot G)" texword)
+(defprop $Dl_1 "\\Delta^{\\star}" texword)
+(defprop $Dl_c "\\Delta_{\\chi}" texword)
+(defprop %KDELTA "\\delta" texword)
+(defprop $Omega "\\Omega" texword)
+(defprop $Om "\\Omega^{\\star}" texword)
+(defprop $DLT "\\delta" texword)
+(defprop %KDELTA "\\delta" texword)
+(defprop %CURVATURE "\\cal{R}" texword)
+(defprop $CHR1 "\\Gamma" texword)
+(defprop $CHR2 "\\Gamma" texword)
+(defprop $PHI  "\\Phi" texword)
+(defprop $D_T  "\\frac{\\partial}{\\partial t}" texword)
+(defprop $LCH  "\\varepsilon" texword)
+(defprop %LCH  "\\varepsilon" texword)
