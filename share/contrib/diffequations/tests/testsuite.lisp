7ca0f39910e8cdd2abf3c406f88513a8a543da0f
billingd <billingd>
2004-05-16 12:03:16 +0000

    Convert share/contrib/diffequations/tests directory to use the new (April 2004) testsuite driver routines.

---
+++ share/contrib/diffequations/tests/testsuite.lisp
@@ -0,0 +1,16 @@
+;;; The Maxima test suite is defined to be the sum of all the tests
+;;; in the files listed in *testsuite-files*.
+;;; Each entry can either be a plain file name (minus extension), e.g.,
+;;; "testfile.mac", or a list containing a file name followed by the numbers
+;;; of the test problems that are expected to fail, e.g.
+;;; ("testfile.mac" 7 9 13).
+
+(setf *testsuite-files*
+      '( "rtestode_murphy1.mac"
+	 "rtestode_murphy2.mac"
+	 "rtest_sym.mac" 
+	 "rtest_sym2.mac"
+	 "rtest_ode1_riccati.mac"
+        "rtest_ode1_abel.mac"
+	)
+)
