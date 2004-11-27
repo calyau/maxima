59842fb017d86eec8eb26f990728de7a38f5f7b4
wjenkner <wjenkner>
2004-11-27 01:08:46 +0000

    Add the usual setup for running the test suite.

---
+++ share/sym/testsuite.lisp
@@ -0,0 +1,9 @@
+;;; The Maxima test suite is defined to be the sum of all the tests
+;;; in the files listed in *testsuite-files*.
+;;; Each entry can either be a plain file name (minus extension), e.g.,
+;;; "testfile.mac", or a list containing a file name followed by the numbers
+;;; of the test problems that are expected to fail, e.g.
+;;; ("testfile.mac" 7 9 13).
+
+;; Have still to check if these are really errors.
+(setf *testsuite-files* '(("symtest.mac" 11 28)))
