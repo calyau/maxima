fd286a42c63e88f7ba82269ee29e202415e591cd
wjenkner <wjenkner>
2004-11-28 02:42:47 +0000

    The result of the first example for `direct' is correct.

--- share/sym/testsuite.lisp
+++ share/sym/testsuite.lisp
@@ -5,5 +5,4 @@
 ;;; of the test problems that are expected to fail, e.g.
 ;;; ("testfile.mac" 7 9 13).
 
-;; Have still to check if this is really an error.
-(setf *testsuite-files* '(("symtest.mac" 11)))
+(setf *testsuite-files* '("symtest.mac"))
