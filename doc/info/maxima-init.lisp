339fc651da165c8cfe9110d7112fbfd30c8aa74e
vvzhy <vvzhy>
2005-01-12 13:50:06 +0000

    `update_examples' script.  See README.update_examples for details.

---
+++ doc/info/maxima-init.lisp
@@ -0,0 +1,12 @@
+
+;; Default linel value for Maxima manual is 65
+
+(progn (msetchk '$linel 65) (setq $linel 65))
+
+(setf *prompt-prefix* "")
+(setf *prompt-suffix* "")
+(setf *general-display-prefix* "")
+(setf *maxima-prolog* "")
+(setf *maxima-epilog* "")
+
+
