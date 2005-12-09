119d26cc7a9d2e6b673ef4b5e0081040b72105c1
willisbl <willisbl>
2005-12-09 20:43:18 +0000

    Fixing branch cut problems for acosh, atan, asech, and acot.

--- src/logarc.lisp
+++ src/logarc.lisp
@@ -21,6 +21,35 @@
 	(t (recur-apply #'$logarc exp))))
 
 (defmfun logarc (f x)
+  ;;Gives the logarithmic form of arc trig and hyperbolic functions
+
+  (cond ((eq f '%acos)
+	 ;; -%i * log(x + %i*sqrt(1-x^2))
+	 (mul -1 '$%i (take '(%log) (add x (mul '$%i (root (sub 1 (power x 2)) 2))))))
+	((eq f '%asin)
+	 ;; -%i * log(sqrt(1-x^2)+%i*x)
+	 (mul -1 '$%i (take '(%log) (add (mul '$%i x) (root (sub 1 (power x 2)) 2)))))
+	((eq f '%atan)
+	 ;; (log(1 + %i*x) - log(1 - %i*x)) /(2 %i)
+	 (div (sub (take '(%log) (add 1 (mul '$%i x))) (take '(%log) (sub 1 (mul '$%i x))))
+	      (mul 2 '$%i)))
+    	((eq f '%asinh)
+	 ;; log(sqrt(x^2+1)+x)
+	 (take '(%log) (add x (root (add 1 (power x 2)) 2))))
+	((eq f '%acosh)
+	 ;; 2 * log(sqrt((x+1)/2)+sqrt((x-1)/2))
+	 (mul 2 (take '(%log) (add (root (div (add x 1) 2) 2) (root (div (add x -1) 2) 2)))))
+    	((eq f '%atanh)
+	 ;;  (log(x+1)-log(1-x))/2
+	 (div (sub (take '(%log) (add 1 x)) (take '(%log) (sub 1 x))) 2))
+    	((memq f '(%asec %acsc %acot %asech %acsch %acoth))
+	 ;; asec(x) = acos(1/x), and etc.
+	 (logarc (oldget (oldget (get f '$inverse) 'recip) '$inverse) (inv x)))
+	(t (merror "Bad argument to 'logarc'"))))
+
+(defmfun halfangle (f a)
+  (and (mtimesp a)
+       (ratnump (cadr a))(defmfun logarc (f x)
   ;;Gives logarithmic form of arc trig and hyperbolic functions
   (let ((s (memq f '(%acos %atan %asinh %atanh))))
     (cond 
@@ -41,10 +70,6 @@
       ((memq f '(%asec %acsc %asech %acsch))
        (logarc (oldget (oldget (get f '$inverse) 'recip) '$inverse) (inv x)))
       (t (merror "Bad argument to Logarc")))))
-
-(defmfun halfangle (f a)
-  (and (mtimesp a)
-       (ratnump (cadr a))
        (equal (caddr (cadr a)) 2)
        (halfangleaux f (mul 2 a))))
 
