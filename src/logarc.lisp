319d83726a2cea6fa2276019d4c4f241a985af18
willisbl <willisbl>
2005-12-10 15:16:12 +0000

    Fixing paste error from previous version.

--- src/logarc.lisp
+++ src/logarc.lisp
@@ -49,27 +49,7 @@
 
 (defmfun halfangle (f a)
   (and (mtimesp a)
-       (ratnump (cadr a))(defmfun logarc (f x)
-  ;;Gives logarithmic form of arc trig and hyperbolic functions
-  (let ((s (memq f '(%acos %atan %asinh %atanh))))
-    (cond 
-      ((memq f '(%acos %asin))
-       (mul (min%i)
-	    (take '(%log)
-		  (add (mul (if s '$%i 1)
-			    (root (add 1 (neg (power x 2))) 2))
-		       (mul (if s 1 '$%i) x)))))
-      ((memq f '(%atan %acot))
-       (mul (i//2)
-	    (take '(%log) (div (add 1 (morp s (mul '$%i x)))
-			       (add (mul '$%i x) (porm s 1))))))
-      ((memq f '(%asinh %acosh))
-       (take '(%log) (add x (root (add (power x 2) (porm s 1)) 2))))
-      ((memq f '(%atanh %acoth))
-       (mul (half) (take '(%log) (div (add 1 x) (morp s (add x -1))))))
-      ((memq f '(%asec %acsc %asech %acsch))
-       (logarc (oldget (oldget (get f '$inverse) 'recip) '$inverse) (inv x)))
-      (t (merror "Bad argument to Logarc")))))
+       (ratnump (cadr a))
        (equal (caddr (cadr a)) 2)
        (halfangleaux f (mul 2 a))))
 
