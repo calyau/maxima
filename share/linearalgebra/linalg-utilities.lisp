2ab3447e25e2927031baf1b1ad7c2a798ff7d018
willisbl <willisbl>
2006-01-02 18:11:29 +0000

    (1) changed ctranspose to obey matrix_element_transpose.
    (2) deleted while, op-equalp, and mrelationp  --  these are in core
    (and were reinserted after once being deleted...)
    
    (3) changed user documenation for ctranspose
    
    (4) fixed one test in test-linalg for new ctranspose.

--- share/linearalgebra/linalg-utilities.lisp
+++ share/linearalgebra/linalg-utilities.lisp
@@ -9,21 +9,6 @@
 
 ($put '$linalgutilities 1.1 '$version)
 
-(defmacro while (cond &rest body)
-  `(do ()
-       ((not ,cond))
-     ,@body))
-
-;; Return true iff e is an expression with operator op1, op2,...,or opn. 
-
-(defun op-equalp (e &rest op)
-  (and (consp e) (consp (car e)) (some #'(lambda (s) (equal (caar e) s)) op)))
-
-;; Return true iff the operator of e is a Maxima relation operator.
-
-(defun mrelationp (a)
-  (op-equalp a 'mlessp 'mleqp 'mequal 'mgeqp 'mgreaterp))
-
 (defun $listp (e &optional (f nil))
   (and (op-equalp e 'mlist) (or (eq f nil) (every #'(lambda (s) (eq t (mfuncall f s))) (margs e)))))
 
@@ -61,11 +46,7 @@
       (merror "The ~:M argument of the function ~:M must be a positive integer" pos fun)))
 
 (defun $ctranspose (m)
-  (let (($matrix_element_transpose 
-	 `((lambda simp) ((mlist) x)
-	   ((mcond) (($matrixp) x) (($ctranspose) x) t
-	    (($conjugate) x)))))
-    (mfuncall '$transpose m)))
+  (mfuncall '$transpose (mfuncall '$matrixmap '$conjugate m)))
 
 (eval-when (eval compile load)
   (mfuncall '$alias '$copylist '$copy '$copymatrix '$copy))
