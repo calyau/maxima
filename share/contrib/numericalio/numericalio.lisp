411b892e3945fdbe913be4ed90f0d9a7841a17e2
robert_dodier <robert_dodier>
2006-04-16 19:27:17 +0000

    Allow Lisp strings as filenames. Now stuff like read_matrix (file_search ("foo.data")); works OK.

--- share/contrib/numericalio/numericalio.lisp
+++ share/contrib/numericalio/numericalio.lisp
@@ -244,7 +244,10 @@
 
 
 (defun require-string (s)
-  (cond ((mstringp s)
-      (print-invert-case (stripdollar s)))
+  (cond
+    ((stringp s)
+     s)
+    ((mstringp s)
+     (print-invert-case (stripdollar s)))
     (t
       (merror "numericalio: expected a string, instead found a ~:M" (type-of s)))))
