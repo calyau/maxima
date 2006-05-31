9b1c0d48f666f18f1f72ed218bffc53b1a62ca91
willisbl <willisbl>
2006-05-31 18:37:42 +0000

    (1) Adding support for fthreadable, idempotent, and involution. (2) Fixing yet another bug in multiadditive.

--- share/contrib/multiadditive.lisp
+++ share/contrib/multiadditive.lisp
@@ -26,6 +26,13 @@ makes it additive in just its first argument.  Examples:
 (%o6) g(y,b+a)+g(x,b+a)
 |#
 
+;; When e is a mapatom, the function call (oper-apply e z) gives an
+;; error. I think oper-apply should be changed so that its first
+;; argument can be a mapatom. Till then:
+
+(defun protected-oper-apply (e z)
+  (if ($mapatom e) e (oper-apply e z)))
+
 (setq opers (cons '$multiadditive opers)
       *opers-list (cons '($multiadditive . multiadditive) *opers-list))
 
@@ -36,5 +43,45 @@ makes it additive in just its first argument.  Examples:
 	 (let ((op (mop e)) (args (margs e)))
 	   (setq args (mapcar #'(lambda (s) (if (op-equalp s 'mplus) ($args s) `((mlist) ,s))) args))
 	   (setq args (mfuncall '$apply '$outermap ($cons op (cons '(mlist) args))))
-	   (reduce 'add (mapcar #'(lambda (s) (oper-apply s z)) (margs ($flatten args))))))
-	(t (oper-apply e z))))
+	   (reduce 'add (mapcar #'(lambda (s) (protected-oper-apply s z)) (margs ($flatten args))))))
+	(t (protected-oper-apply e z))))
+
+(setq opers (cons '$threadable opers)
+      *opers-list (cons '($threadable . threadable) *opers-list))
+
+(setq $opproperties ($cons '$threadable $opproperties))
+
+;; ((op) bag) --> map(op bag).
+
+(defun threadable (e z)
+  (let ((arg (margs e)) (fop) (bop)) ;; fop = function operator and bop = bag operator.
+    (cond ((and (= 1 (length arg)) (or (mbagp (first arg)) (op-equalp (first arg) '$set)))
+	   (setq arg (first arg))
+	   (setq fop (mop e))
+	   (setq bop (mop arg))
+	   (simplify `((,bop) ,@(mapcar #'(lambda (s) (protected-oper-apply (mfuncall fop s) z)) 
+					(margs arg)))))
+	  (t (protected-oper-apply e z)))))
+
+(setq opers (cons '$idempotent opers)
+      *opers-list (cons '($idempotent . idempotent) *opers-list))
+
+(setq $opproperties ($cons '$idempotent $opproperties))
+
+;; ((op) ((op) x)) --> ((op) x)
+
+(defun idempotent (e z)
+  (protected-oper-apply (if (and (= 1 (length (margs e))) (eq (mop e) (mop (first (margs e)))))
+		  (first (margs e)) e) z))
+
+(setq opers (cons '$involution opers)
+      *opers-list (cons '($involution . involution) *opers-list))
+
+(setq $opproperties ($cons '$involution $opproperties))
+
+;; ((op) ((op) x)) --> x.
+
+(defun involution (e z)
+  (protected-oper-apply (if (and (= 1 (length (margs e))) (eq (mop e) (mop (first (margs e))))
+		       (= 1 (length (margs (first (margs e))))))
+		  (first (margs (first (margs e)))) e) z))
