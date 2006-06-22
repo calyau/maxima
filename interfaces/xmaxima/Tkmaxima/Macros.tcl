aab746bcf8a135fe7203f50c50f98064e497cb91
villate <villate>
2006-06-22 11:48:19 +0000

    Added documentation for the functions.

--- interfaces/xmaxima/Tkmaxima/Macros.tcl
+++ interfaces/xmaxima/Tkmaxima/Macros.tcl
@@ -1,20 +1,34 @@
 # -*-mode: tcl; fill-column: 75; tab-width: 8; coding: iso-latin-1-unix -*-
 #
-#       $Id: Macros.tcl,v 1.3 2002-09-14 17:25:34 mikeclarkson Exp $
+#       $Id: Macros.tcl,v 1.4 2006-06-22 11:48:19 villate Exp $
 #
 ###### Macros.tcl ######
 ############################################################
 # Netmath       Copyright (C) 1998 William F. Schelter     #
 # For distribution under GNU public License.  See COPYING. #
 ############################################################
-proc desetq {lis lis2} {
+#
+#-----------------------------------------------------------------------
+# desetq lis1 lis2 -- sets the values for several variables
+#
+# Result: each variable name in LIS1 is defined with a value in LIS2
+#-----------------------------------------------------------------------
+#
+proc desetq {lis1 lis2} {
     set i 0
-    foreach v $lis {
+    foreach v $lis1 {
 	uplevel 1 set $v [list [lindex $lis2 $i]]
-	set i [expr {$i + 1}]
+	incr i
     }
 }
-
+######  Options parsing functions ######################################
+#
+#-----------------------------------------------------------------------
+# assoc key lis args -- find value of option KEY in options list LIS
+#
+# Result: value of option or first element of list ARGS
+#-----------------------------------------------------------------------
+#
 proc assoc { key lis args } {
     foreach { k val } $lis {
 	if { "$k" == "$key" } {
@@ -23,7 +37,13 @@ proc assoc { key lis args } {
     }
     return [lindex $args 0]
 }
-
+#
+#-----------------------------------------------------------------------
+# delassoc key lis -- remove option KEY from options list LIS
+#
+# Result: an options list without option KEY
+#-----------------------------------------------------------------------
+#
 proc delassoc { key lis } {
     foreach { k val } $lis {
 	if { "$k" != "$key" } {
@@ -32,8 +52,13 @@ proc delassoc { key lis } {
     }
     return $new
 }
-
-
+#
+#-----------------------------------------------------------------------
+# putassoc key lis value -- set VALUE for option KEY in options list LIS
+#
+# Result: an option list with KEY set to VALUE
+#-----------------------------------------------------------------------
+#
 proc putassoc {key lis value } {
     set done 0
     foreach { k val } $lis {
@@ -43,31 +68,29 @@ proc putassoc {key lis value } {
 	}
 	lappend new $k $val
     }
-    if { !$done } {
-	lappend new $key $value
-    }
+    if { !$done } { lappend new $key $value }
     return $new
 }
-
+######  End options parsing functions #################################
+#
+#-----------------------------------------------------------------------
+# intersect lis1 lis2 -- find common elements of two lists
+#
+# Result: a list of values found in LIS1 and LIS2
+#-----------------------------------------------------------------------
+#
 proc intersect { lis1 lis2 } {
     set new ""
     foreach v $lis1 { set there($v) 1 }
     foreach v $lis2 { if { [info exists there($v)] } { lappend new $v }}
     return $new
 }
-
-
-
 #
-#-----------------------------------------------------------------
+#-----------------------------------------------------------------------
+# ldelete item lis --  remove all copies of ITEM from LIS
 #
-# ldelete --  remove all copies of ITEM from LIST
-#
-#  Results: new list without item
-#
-#  Side Effects:
-#
-#----------------------------------------------------------------
+# Result: new list without ITEM
+#-----------------------------------------------------------------------
 #
 proc ldelete { item list } {
     while { [set ind [lsearch $list $item]] >= 0  } {
@@ -75,19 +98,18 @@ proc ldelete { item list } {
     }
     return $list
 }
-
-# apply f a1 a2 a3 [list  u1 u2 ..un]   , should call
-# f with n+3 arguments.
+#
+#-----------------------------------------------------------------------
+# apply f a1 .. am [list u1 .. un] -- apply a function with arguments
+#        A1 .. Am and all the elements U1 .. Un in a list
+#
+# Result: command f is evaluated, in the scope from where APPLY was issued
+#-----------------------------------------------------------------------
+#
 proc apply {f args } {
     set lis1 [lrange $args 0 [expr {[llength $args] -2}]]
     foreach v [lindex $args end] { lappend lis1 $v}
     set lis1 [linsert $lis1  0 $f]
     uplevel 1 $lis1
 }
-
-
-
-
-
-
 ## endsource macros.tcl
