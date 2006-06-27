92aa54c2fc0fe4e2f7dbfcb7cb0c2cf4bf9b3421
villate <villate>
2006-06-27 13:33:42 +0000

    Fixes the default fonts setting for Unix, which was broken.

--- interfaces/xmaxima/Tkmaxima/Fonts.tcl
+++ interfaces/xmaxima/Tkmaxima/Fonts.tcl
@@ -1,106 +1,76 @@
 # -*-mode: tcl; fill-column: 75; tab-width: 8; coding: iso-latin-1-unix -*-
 #
-#       $Id: Fonts.tcl,v 1.7 2004-10-16 09:44:34 vvzhy Exp $
+#       $Id: Fonts.tcl,v 1.8 2006-06-27 13:33:42 villate Exp $
 #
 
 # set font {Courier 8}
-global fontCourier8
-
-global maxima_priv
+global maxima_priv fontCourier8 fixedFont fontSize
 set maxima_priv(fixedFont) Courier
-global fixedFont
-set fixedFont Courier
 
 # Pick a default font size in pixels
 set _screenheight [winfo screenheight .]
-if {$_screenheight < 500} {
-    # for 640x480
-    set _pixel 10
-    set _point 8
-    # Pick a default borderwidth which smaller
-    set _bd 1
-} elseif {$_screenheight < 700} {
-    # for 800x600
-    set _pixel 12
-    set _point 8
-} elseif {$_screenheight < 800} {
-    # for 1024x768
-    set _pixel 12
-    set _point 8
-} elseif {$_screenheight < 1100} {
-    # for 1200x1000
-    set _pixel 14
-    set _point 10
-} else {
-    set _pixel 18
-    set _point 12
-}
+# if {$_screenheight < 500} {
+#     # for 640x480
+#     set _pixel 10
+#     set _point 8
+#     # Pick a default borderwidth which smaller
+#     set _bd 1
+# } elseif {$_screenheight < 700} {
+#     # for 800x600
+#     set _pixel 12
+#     set _point 8
+# } elseif {$_screenheight < 800} {
+#     # for 1024x768
+#     set _pixel 12
+#     set _point 8
+# } elseif {$_screenheight < 1100} {
+#     # for 1200x1000
+#     set _pixel 14
+#     set _point 10
+# } else {
+#     set _pixel 18
+#     set _point 12
+# }
 
 # ZW: this actually produces better result
 set _pixel 10
 set _point 8
+set fontSize $_pixel
 
 # setup defaults depending on the OS and Window Manager
 # Really should do another version for mono
-switch -exact -- $tcl_platform(platform) windows {
-
-    if {$tcl_platform(osVersion) < 5} {
-	set _prop_default "MS Sans Serif"
-    } else {
-	set _prop_default "Tahoma"
-    }
-
-    # make sure this font is installed
-    set _allowed [string tolow [font families]]
-    foreach font [list $_prop_default "MS Sans Serif" Tahoma Arial System] {
-	if {[lsearch -exact $_allowed [string tolow $font]] > -1} {
-	    set _prop_default $font
-	    break
+switch -exact -- $tcl_platform(platform) {
+    windows {
+	if {$tcl_platform(osVersion) < 5} {
+	    set _prop_default {MS Sans Serif}
+	} else {
+	    set _prop_default Tahoma
 	}
+	set _fixed_default {Courier New}
     }
-
-    set _fixed_default {Courier New}
-    # make sure this font is installed
-    foreach font [list $_fixed_default Courier System] {
-	if {[lsearch -exact $_allowed [string tolow $font]] > -1} {
-	    set _fixed_default $font
-	    break
-	}
+    default {
+	set _prop_default {Bitstream Vera Sans}
+	set _fixed_default {Bitstream Vera Sans Mono}
     }
-    set fontCourier8 [list $_fixed_default $_point]
-
-    # FIXME: nuke this
-    global fontSize
-    set fontSize $_point
-
-    set fixedFont [font create -family $_fixed_default -size $_point]
-
-} default {
-    set _prop_default arial
-    # make sure this font is installed
-    set _allowed [string tolow [font families]]
-    foreach font [list $_prop_default helvetica times fixed] {
-	if {[lsearch -exact $_allowed [string tolow $font]] > -1} {
-	    set _prop_default $font
-	    break
-	}
-    }
-    set _fixed_default courier
-    # make sure this font is installed
-    foreach font [list $_fixed_default fixed] {
-	if {[lsearch -exact $_allowed [string tolow $font]] > -1} {
-	    set _fixed_default $font
-	    break
-	}
-    }
-
-    set fontCourier8 [list $_fixed_default -$_pixel]
-    set fixedFont [font create -family $_fixed_default -size -$_pixel]
-
-    # FIXME: nuke this
-    global fontSize
-    set fontSize -$_pixel
 }
+# make sure these fonts are installed
+set _allowed [string tolow [font families]]
+foreach font [list $_prop_default "MS Sans Serif" Tahoma Arial Helvetica \
+		 fixed system] {
+    if {[lsearch -exact $_allowed [string tolow $font]] > -1} {
+	set _prop_default $font
+	break
+    }
+}
+foreach font [list $_fixed_default Courier fixed system] {
+    if {[lsearch -exact $_allowed [string tolow $font]] > -1} {
+	set _fixed_default $font
+	break
+    }
+}
+set fontCourier8 [list $_fixed_default $_pixel]
+set fixedFont [font create -family $_fixed_default -size $_pixel]
+set buttonfont [font create -family $_prop_default -size $_pixel]
 
 
 global maxima_default
