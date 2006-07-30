82f7ab0b0dd1d124a763b9caa7165f37032afe4e
villate <villate>
2006-07-30 19:22:20 +0000

    the info message about function syntax is now more explicit and better formatted.

--- interfaces/xmaxima/Tkmaxima/Parse.tcl
+++ interfaces/xmaxima/Tkmaxima/Parse.tcl
@@ -1,6 +1,6 @@
 # -*-mode: tcl; fill-column: 75; tab-width: 8; coding: iso-latin-1-unix -*-
 #
-#       $Id: Parse.tcl,v 1.5 2006-06-29 13:15:29 villate Exp $
+#       $Id: Parse.tcl,v 1.6 2006-07-30 19:22:20 villate Exp $
 #
 ###### Parse.tcl ######
 ############################################################
@@ -78,9 +78,12 @@ set Parser(reserved) " acos cos hypo sinh asin cosh log sqrt atan exp log10 tan 
 
 set Parser(help) [join [list [mc \
 {
-    The syntax is like C except that it is permitted to write x^n
-    instead of pow(x,n).
-} ] [concat "\n" [mc "Functions:"] "$Parser(reserved)\n\n" [mc "Operators:"] "== % & || ( << <= ) : * >=  + && , | < >> - > ^ ? /" ]] ""]
+The syntax for the definition of functions is like C, except that it is \
+permitted to write x^n instead of pow(x,n).
+} ] [mc {
+Functions:}] $Parser(reserved) [mc {
+
+Operators:}] " == % & || ( << <= ) : * >=  + && , | < >> - > ^ ? /" ] ""]
 
 
 
