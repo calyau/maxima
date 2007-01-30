797c60cd903eb25a0bcab66dc9c02e3f4c6ce6c7
are_muc <are_muc>
2007-01-30 12:15:06 +0000

    removed stale conditionalizations; removed unused macro definitions;

--- src/numerm.lisp
+++ src/numerm.lisp
@@ -9,92 +9,28 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (in-package :maxima)
+
 (macsyma-module numerm macro)
 
-;;; Macros for interface of lisp numerical routines to macsyma,
+;;; Macros for interface of lisp numerical routines to maxima
 ;;; for use with the functions in Maxsrc;Numer.
 
-(defmacro make-array$ (&rest l)
-					;I guess macsyma has to know the default "flonum" type... --gsb
-  #+maclisp
-  `(*array nil 'flonum ,@l)
-  #+(or cl nil) `(make-array (list ,@l) :element-type 'double-float)
-  )
-
-
-(defmacro make-array% (&rest l)
-  #+maclisp
-  `(*array nil 'fixnum ,@l)
-  #+(or cl nil)
-  `(make-array (list ,@l) :element-type 'fixnum)
-  )
-
 (defmacro aref$ (&rest l)
-  #+maclisp
-  `(arraycall flonum ,@l)
-  #+(or cl nil)
-  `(aref (the (simple-array double-float) ,(car l)) ,@(cdr l))
-  #+(or franz)
-  `(aref ,@l)
-  )
-
-(defmacro aref% (&rest l)
-  #+maclisp
-  `(arraycall fixnum ,@l)
-  #+(or cl nil)
-  `(aref (the (simple-array fixnum) ,(car l)) ,@(cdr l))
-  )
-
-(defmacro free-array% (a)
-  #+maclisp
-  `(*rearray ,a)
-  #+(or cl nil)
-  ;; not useful to call return-array unless it is at end of area.
-  ;; programs do better to save arrays as a resource, this works
-  ;; in maclisp too.
-  a
-  )
-(defmacro free-array$ (a)
-  #+maclisp
-  `(*rearray ,a)
-  #+(or cl nil)
-  a
-  )
-
+  `(aref (the (simple-array double-float) ,(car l)) ,@(cdr l)))
 
 (defmacro defbindtramp$ (nargs)
-  (let ((bind-tramp$ #-multics (symbolconc 'bind-tramp nargs '$)
-		     #+multics (implode (mapcan 'exploden
-						(list 'bind-tramp nargs '$))))
-	(tramp$ #-multics (symbolconc 'tramp nargs '$)
-		#+multics (implode (mapcan 'exploden (list 'tramp nargs '$)))))
-;;;When Multics gets symbolconc the above conditionalization can be removed.
-    `(progn 'compile
-      #-cl (if (fboundp 'special) (special ,tramp$))
-      (proclaim (quote (special ,tramp$)))
-      (defmacro ,bind-tramp$ (f g &rest body)
-	`(let ((,',tramp$))
-	  (let ((,f (make-tramp$ ,g ,',nargs)))
-	    ,@body))))))
+  (let ((bind-tramp$ (symbolconc 'bind-tramp nargs '$))
+	(tramp$ (symbolconc 'tramp nargs '$)))
+    `(progn
+       (proclaim (quote (special ,tramp$)))
+       (defmacro ,bind-tramp$ (f g &rest body)
+	 `(let ((,',tramp$))
+	    (let ((,f (make-tramp$ ,g ,',nargs)))
+	      ,@body))))))
 
 (defbindtramp$ 1)
 (defbindtramp$ 2)
 (defbindtramp$ 3)
 
 (defmacro fcall$ (&rest l)
-  #+maclisp
-  `(subrcall flonum ,@l)
-  #+(or cl nil)
-  `(funcall ,@l)
-  )
-
-;; Central location for some important declarations.
-#+maclisp
-(if (fboundp 'flonum)
-    (flonum (gcall1$ nil nil)
-	    (gcall2$ nil nil nil)
-	    (mto-float nil)
-	    ))
-
-
-
+  `(funcall ,@l))
