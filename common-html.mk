8b49e32d49adf149338400ba59eb09669f843148
vvzhy <vvzhy>
2007-02-05 07:41:26 +0000

    -- Make variable htmlinstdir specifies installation directory for HTML files -- New HTML build scheme applied to xmaxima.html

--- common-html.mk
+++ common-html.mk
@@ -1,35 +1,35 @@
 # Installation/uninstallation and distribution for .html files.
-# htmlname variable should be set to html base name,
-# for example maxima or xmaxima
+# htmlname -- html base name (e.g. maxima or xmaxima)
+# htmlinstdir -- html installation directory  
 
 install-data-local: install-maxima-html
 install-maxima-html: $(htmlname).html
 	@$(NORMAL_INSTALL)
-	$(mkinstalldirs) $(DESTDIR)$(genericdir)
+	$(mkinstalldirs) $(DESTDIR)$(htmlinstdir)
 	@srcdirstrip=`echo "$(srcdir)" | sed 's|.|.|g'`; \
 	list="$(srcdir)/$(htmlname).html $(srcdir)/$(htmlname)_*.html" ; \
 	for p in $$list; do \
 	  f=`echo "$$p" | sed "s|^$$srcdirstrip/||"`; \
 	  if test -f $(srcdir)/$$f; then \
-            if test ! -d `dirname $(DESTDIR)$(genericdir)/$$f`; then \
-              $(mkinstalldirs) `dirname $(DESTDIR)$(genericdir)/$$f`; \
+            if test ! -d `dirname $(DESTDIR)$(htmlinstdir)/$$f`; then \
+              $(mkinstalldirs) `dirname $(DESTDIR)$(htmlinstdir)/$$f`; \
             fi; \
-	    echo " $(INSTALL_DATA) $(srcdir)/$$f $(DESTDIR)$(genericdir)/$$f"; \
-	    $(INSTALL_DATA) $(srcdir)/$$f $(DESTDIR)$(genericdir)/$$f; \
+	    echo " $(INSTALL_DATA) $(srcdir)/$$f $(DESTDIR)$(htmlinstdir)/$$f"; \
+	    $(INSTALL_DATA) $(srcdir)/$$f $(DESTDIR)$(htmlinstdir)/$$f; \
 	  else if test -f $$f; then \
-            if test ! -d `dirname $(DESTDIR)$(genericdir)/$$f`; then \
-              $(mkinstalldirs) `dirname $(DESTDIR)$(genericdir)/$$f`; \
+            if test ! -d `dirname $(DESTDIR)$(htmlinstdir)/$$f`; then \
+              $(mkinstalldirs) `dirname $(DESTDIR)$(htmlinstdir)/$$f`; \
             fi; \
-	    echo " $(INSTALL_DATA) $$f $(DESTDIR)$(genericdir)/$$f"; \
-	    $(INSTALL_DATA) $$f $(DESTDIR)$(genericdir)/$$f; \
+	    echo " $(INSTALL_DATA) $$f $(DESTDIR)$(htmlinstdir)/$$f"; \
+	    $(INSTALL_DATA) $$f $(DESTDIR)$(htmlinstdir)/$$f; \
 	  fi; fi; \
 	done
 
 uninstall-local: uninstall-maxima-html
 uninstall-maxima-html:
 	@$(NORMAL_UNINSTALL)
-	rm -f $(DESTDIR)$(genericdir)/$(htmlname).html 
-	rm -f $(DESTDIR)$(genericdir)/$(htmlname)_*.html
+	rm -f $(DESTDIR)$(htmlinstdir)/$(htmlname).html 
+	rm -f $(DESTDIR)$(htmlinstdir)/$(htmlname)_*.html
 
 dist-hook: dist-maxima-html
 dist-maxima-html: $(htmlname).html
