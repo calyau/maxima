3d6b53eae2597f101d765398b4494eb152362ffe
are_muc <are_muc>
2007-02-20 22:13:49 +0000

    replaced fn by lambda.

--- src/rzmac.lisp
+++ src/rzmac.lisp
@@ -17,7 +17,7 @@
 ;;;   *****************************************************************
 
 (defmacro *bind* (bindings &body body)
-  (nconc (list 'do (mapcar (fn (q)
+  (nconc (list 'do (mapcar #'(lambda (q)
 			       (cond ((atom q)
 				      (list q))
 				     ((eq (cadr q) '|<-|)
@@ -25,9 +25,10 @@
 				     (t q)))
 			   bindings)
 	       '(nil))
-	 (maplist (fn (x) (cond ((null (cdr x))
-				 (cons 'return x))
-				((car x))))
+	 (maplist #'(lambda (x)
+		      (cond ((null (cdr x))
+			     (cons 'return x))
+			    ((car x))))
 		  body)))
 
 ;; Returns the negation of VALUE if PREDICATE is true.  Otherwise, just
@@ -35,16 +36,18 @@
 
 (defmacro negate-if (predicate value &aux (temp (gensym)))
   `(let ((,temp ,predicate))
-    (cond (,temp (neg ,value))
-	  (t ,value))))
+    (if ,temp
+	(neg ,value)
+	,value)))
 
 ;; Setq's the first variable to VALUE if SWITCH is true, and sets the second
 ;; variable otherwise.
 
 (defmacro set-either (first-var second-var switch value &aux (temp (gensym)))
   `(let ((,temp ,value))
-    (cond (,switch (setq ,first-var ,temp))
-	  (t (setq ,second-var ,temp)))))
+    (if ,switch
+	(setq ,first-var ,temp)
+	(setq ,second-var ,temp))))
 
 ;; symbolic arithmetic macros
 
@@ -57,12 +60,14 @@
 (defmacro m1- (x) `(add* -1 ,x))
 
 (defmacro m// (a1 &optional (a2 nil 2args))
-  (cond (2args `(div* ,a1 ,a2))
-	(t `(inv* ,a1))))
+  (if 2args
+      `(div* ,a1 ,a2)
+      `(inv* ,a1)))
 
 (defmacro m- (a1 &optional (a2 nil 2args))
-  (cond (2args `(sub* ,a1 ,a2))
-	(t `(mul* -1 ,a1))))
+  (if 2args
+      `(sub* ,a1 ,a2)
+      `(mul* -1 ,a1)))
 
 (defmacro m^ (b e) `(power* ,b ,e))
 
@@ -79,12 +84,14 @@
 (defmacro m1-t (x) `(add -1 ,x))
 
 (defmacro m//t (a1 &optional (a2 nil 2args))
-  (cond (2args `(div ,a1 ,a2))
-	(t `(inv ,a1))))
+  (if 2args
+      `(div ,a1 ,a2)
+      `(inv ,a1)))
 
 (defmacro m-t (a1 &optional (a2 nil 2args))
-  (cond (2args `(sub ,a1 ,a2))
-	(t `(neg ,a1))))
+  (if 2args
+      `(sub ,a1 ,a2)
+      `(neg ,a1)))
 
 (defmacro m^t (b e) `(power ,b ,e))
 
