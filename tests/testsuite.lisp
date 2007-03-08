13f25422a48e78ab0d05b2eb6c3efe6cc5d2d288
robert_dodier <robert_dodier>
2007-03-08 02:39:54 +0000

    New test script rtest_mset.mac. Put rtest_mset on list in testsuite.lisp.

--- tests/testsuite.lisp
+++ tests/testsuite.lisp
@@ -35,4 +35,5 @@
     ((mlist) "rtest_abs" 42 43)
     ((mlist) "rtest_taylor" 66 67 72 88 89 90 91 94 96 99 104 110 112 113 118 119 120 121 122 123 124)
     ((mlist) "rtest_dot")
+    "rtest_mset"
     ))
