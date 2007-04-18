52c7d5838acb888ccbcf7662a6550e1f6b1d7550
andrejv <andrejv>
2007-04-18 08:30:47 +0000

    Changes sconcat to concat in temp_filename.

--- share/contrib/graphs/graph_core.lisp
+++ share/contrib/graphs/graph_core.lisp
@@ -1814,4 +1814,4 @@
 (defun $temp_filename (file)
   (unless ($stringp file)
     ($error "Argument to `temp_filename' is not a string"))
-  (plot-temp-file ($sconcat file)))
+  (plot-temp-file ($concat file)))
