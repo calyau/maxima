30ab06916d4210db98b5bb16a024047a1888fb7e
are_muc <are_muc>
2007-04-27 19:53:02 +0000

    declared *nned-prompt* as special in $example.

--- src/macdes.lisp
+++ src/macdes.lisp
@@ -17,70 +17,63 @@
 (defun combine-path (list)
   (let ((result (first list)))
     (mapc #'(lambda (x) 
-	      (setf result 
-		    (concatenate 'string result "/" x))) (rest list))
+	      (setf result (concatenate 'string result "/" x)))
+	  (rest list))
     result))
 
-(defmspec $example (l)   (setq l (cdr l))
-	  (block
-	      $example
-	    (let ((example (car l))
-		  (file (or (cadr l)  (combine-path 
-				       (list *maxima-demodir* $manual_demo)))))
-	      (or (symbolp example)
-		  (merror
-		   "First arg ~M to example must be a symbol, eg example(functions)"))
-	      (setq file ($file_search1 $manual_demo '((mlist) $file_search_demo)))
-	      (with-open-file
-		  (st file)
-		(let (			;*mread-prompt*
-		      )
-		  (prog ( tem  all c-tag d-tag)
-
-		   again
-		   (setq tem (read-char st nil))
-		   (or tem (go notfound))
-		   (or (eql tem #\&) (go again))
-		   (setq tem (read-char st nil))
-		   (or (eql tem #\&) (go again))
-		   ;; so we are just after having read &&
+(defmspec $example (l)
+  (declare (special *need-prompt*))
+  (setq l (cdr l))
+  (block
+      $example
+    (let ((example (car l))
+	  (file (or (cadr l)
+		    (combine-path (list *maxima-demodir* $manual_demo)))))
+      (unless (symbolp example)
+	(merror "First arg ~M to example must be a symbol, eg example(functions)"))
+      (setq file ($file_search1 $manual_demo '((mlist) $file_search_demo)))
+      (with-open-file (st file)
+	(prog (tem all c-tag d-tag)
+	 again
+	 (setq tem (read-char st nil))
+	 (or tem (go notfound))
+	 (or (eql tem #\&) (go again))
+	 (setq tem (read-char st nil))
+	 (or (eql tem #\&) (go again))
+	 ;; so we are just after having read &&
 	  
-		   (setq tem (read st nil nil))
-		   (or tem (go notfound))
-		   (setq tem (makealias tem))
-		   (cond ((eql tem example)
-			  (go doit))
-			 (t (push tem all)
-			    (go again)))
-		   ;; at this stage we read maxima forms and print and eval
-		   ;; until a peek sees '&' as the first character of next expression.
-		   doit
-		   (setq tem (peek-char nil st nil))
-		   (cond ((or (null tem) (eql tem #\&))
-			  (setf *need-prompt* t)
-			  (return-from $example '$done)))
-		   (setq tem (dbm-read st nil nil))
-		   (setq $linenum (+ 1 $linenum))
-		   (setq c-tag (makelabel $inchar))
-		   (unless $nolabels (set c-tag (nth 2 tem)))
-		   (let ($display2d)
-		     (displa `((mlable) ,c-tag ,(nth 2 tem)))
-					;(mformat nil "Input: ~M;" (nth 2 tem))
-		     )
-		   (setq $% (meval* (nth 2 tem)))
-		   (setq d-tag (makelabel $outchar))
-		   (unless $nolabels (set d-tag $%))
-		   (if (eq (caar tem) 'displayinput)
-		       (displa `((mlable) ,d-tag ,$%)))
-					;(mformat nil "==> ~M"  (setq $% (meval* (nth 2 tem))))
-		   (go doit) 	
+	 (setq tem (read st nil nil))
+	 (or tem (go notfound))
+	 (setq tem (makealias tem))
+	 (cond ((eql tem example)
+		(go doit))
+	       (t (push tem all)
+		  (go again)))
+	 ;; at this stage we read maxima forms and print and eval
+	 ;; until a peek sees '&' as the first character of next expression.
+	 doit
+	 (setq tem (peek-char nil st nil))
+	 (cond ((or (null tem) (eql tem #\&))
+		(setf *need-prompt* t)
+		(return-from $example '$done)))
+	 (setq tem (dbm-read st nil nil))
+	 (setq $linenum (+ 1 $linenum))
+	 (setq c-tag (makelabel $inchar))
+	 (unless $nolabels (set c-tag (nth 2 tem)))
+	 (let ($display2d)
+	   (displa `((mlable) ,c-tag ,(nth 2 tem))))
+	 (setq $% (meval* (nth 2 tem)))
+	 (setq d-tag (makelabel $outchar))
+	 (unless $nolabels (set d-tag $%))
+	 (if (eq (caar tem) 'displayinput)
+	     (displa `((mlable) ,d-tag ,$%)))
+	 (go doit) 	
 
-		   notfound
-		   (format t "Not Found.  You can look at:")
-		   (setf *need-prompt* t)
-		   (return-from $example
-		     `((mlist) ,@ (nreverse all)))
-		   ))))))
+	 notfound
+	 (format t "Not Found.  You can look at:")
+	 (setf *need-prompt* t)
+	 (return-from $example
+	   `((mlist) ,@ (nreverse all))))))))
 
 (defun mread-noprompt (&rest read-args)
   (let ((*mread-prompt* ""))
