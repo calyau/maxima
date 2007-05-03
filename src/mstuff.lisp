eaa54b24c1b2c66a75df28bc8c9f3ba329c488a2
are_muc <are_muc>
2007-05-03 20:37:35 +0000

    changed maclisp like function calling to use CL's optional argument stuff; rearranged code to make it more legible.

--- src/mstuff.lisp
+++ src/mstuff.lisp
@@ -12,21 +12,16 @@
 
 (macsyma-module mstuff)
 
-(defmfun $sort n
-  (if (or (= n 0) (> n 2)) (merror "`sort' takes 1 or 2 arguments."))
-  (let ((llist (arg 1)) comparfun bfun)
-    (if (not ($listp llist))
-	(merror "The first argument to `sort' must be a list:~%~M" llist))
-    (setq llist (copy-list (cdr llist) )
+(defmfun $sort (l &optional (f 'lessthan))
+  (let ((llist l) comparfun bfun)
+    (unless ($listp llist)
+      (merror "The first argument to `sort' must be a list:~%~M" llist))
+    (setq llist (copy-list (cdr llist))
 	  comparfun 
-	  (mfunction1 (setq bfun (if (= n 2) (getopr (arg 2)) 'lessthan))))
-    (if (member bfun '(lessthan great) :test #'eq)
-	(setq llist (mapcar #'ratdisrep llist)))
+	  (mfunction1 (setq bfun (getopr f))))
+    (when (member bfun '(lessthan great) :test #'eq)
+      (setq llist (mapcar #'ratdisrep llist)))
     (cons '(mlist simp) (sort llist comparfun))))
-
-;; old non closure version
-;;(DEFUN MFUNCTION1 (FUN)
-;;  `(LAMBDA (X Y) (MEVALP `((,',FUN) ((MQUOTE) ,X) ((MQUOTE) ,Y)))))
 
 ;; cmulisp does not like the closure version.  Clisp insists on the
 ;; closure version.  Gcl likes either...  For the moment we will
@@ -38,48 +33,50 @@
   #'(lambda (x y) (mevalp `((,fun) ((mquote) ,x) ((mquote) ,y)))))
 
 (defun lessthan (a b)
-  (if (great b a) t))
+  (great b a))
 
-(defmspec $makelist (x) (setq x (cdr x))
-	  (prog (n form arg a b lv d)
-	     (setq n (length x))
-	     (if (or (< n 3) (> n 4))
-		 (merror "`makelist' takes 3 or 4 arguments."))
-	     (setq form (car x)
-		   arg (cadr x)
-		   a (meval (caddr x))
-		   lv (cond ((= n 3) 
-			     (if ($listp a)
-				 (mapcar #'(lambda (u) (list '(mquote) u)) (cdr a))
-				 (merror "
-If 3 arguments are given to MAKELIST,
+(defmspec $makelist (x)
+  (setq x (cdr x))
+  (prog (n form arg a b lv d)
+     (setq n (length x))
+     (if (or (< n 3) (> n 4))
+	 (merror "`makelist' takes 3 or 4 arguments."))
+     (setq form (car x)
+	   arg (cadr x)
+	   a (meval (caddr x))
+	   lv (cond ((= n 3) 
+		     (if ($listp a)
+			 (mapcar #'(lambda (u) (list '(mquote) u)) (cdr a))
+			 (merror "If 3 arguments are given to MAKELIST, ~
 the 3rd argument should evaluate to a list:~%~M" a)))
-			    (t
-			     (setq b (meval (cadddr x)))
-			     (if (or (not (fixnump (setq d (sub* b a)))) (< d -1))
-				 (merror "
-If 4 arguments are given to MAKELIST, the difference of the 3rd
+		    (t
+		     (setq b (meval (cadddr x)))
+		     (if (or (not (fixnump (setq d (sub* b a)))) (< d -1))
+			 (merror "If 4 arguments are given to MAKELIST, the difference of the 3rd ~
 and 4th arguments should evaluate to a non-negative integer:~%~M" d)
-				 (interval a b)))))
-	     (return 
-	       (do ((lv lv (cdr lv)) (ans))
-		   ((null lv) (cons '(mlist simp) (nreverse ans)))
-		 (setq ans (cons (meval `(($ev)
-					  ,@(list (list '(mquote) form)
-						  (list '(mequal simp) 
-							arg 
-							(car lv)))))
-				 ans))))))
+			 (interval a b)))))
+     (return 
+       (do ((lv lv (cdr lv))
+	    (ans))
+	   ((null lv) (cons '(mlist simp) (nreverse ans)))
+	 (push (meval `(($ev)
+			,@(list (list '(mquote) form)
+				(list '(mequal simp) arg (car lv)))))
+	       ans)))))
 
 (defun interval (i j)
-  (do ((nn i (add2 1 nn)) (m 0 (f1+ m)) (k (sub* j i)) (ans))
+  (do ((nn i (add2 1 nn))
+       (m 0 (1+ m))
+       (k (sub* j i))
+       (ans))
       ((> m k) (nreverse ans))
-    (setq ans (cons nn ans))))
+    (push nn ans)))
 
 (defmfun $sublist (a f)
-  (if ($listp a)
-      (do ((a (cdr a) (cdr a)) (x))
-	  ((null a) (cons '(mlist simp) (nreverse x)))
-	(if (mevalp (list (ncons f) (car a)))
-	    (setq x (cons (car a) x))))
-      (merror "The first argument to `sublist' must be a list:~%~M" a)))
+  (unless ($listp a)
+    (merror "The first argument to `sublist' must be a list:~%~M" a) )
+  (do ((a (cdr a) (cdr a))
+       (x))
+      ((null a) (cons '(mlist simp) (nreverse x)))
+    (if (mevalp (list (ncons f) (car a)))
+	(push (car a) x))))
