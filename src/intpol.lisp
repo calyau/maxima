686475c361198e26f0ee6f1b6e1a44717a9feaab
robert_dodier <robert_dodier>
2007-06-11 14:50:28 +0000

    Change find_root so that it evaluates its arguments in an ordinary way. (1) In src/intpol.lisp, define find_root via DEFUN and cut out the previous macrology. Rename $FIND_ROOT_SUBR to FIND-ROOT-SUBR (so it is no longer visible at user level). (2) In src/plot.lisp, bind $%ENUMER to T in function constructed by COERCE-FLOAT-FUN (which is now called from find_root code). (3) In tests/rtest8.mac, additional find_root examples.

--- src/intpol.lisp
+++ src/intpol.lisp
@@ -30,16 +30,23 @@ Perhaps you meant to enter `~a'.~%"
 	  (print-invert-case (implode (mstring `(($find_root) ,@(cdr form))))))
   '$done)
 
-(defun $find_root_subr (f left right)
-  (bind-tramp1$
-   f f
+(defun find-root-subr (f left right)
    (prog (a b c fa fb fc (lin 0))
       (declare (fixnum lin))
-      (setq a (float left)
-	    b (float right))
+      (let (($numer t) ($%enumer t))
+        (setq
+          ;; Uh-oh. LEFT and RIGHT are evaluated twice !! So burn me at the stake already.
+          ;; $FLOAT has a special case for exponents; float(%pi^%e) => 3.14^%e, believe it or not.
+          ;; But this MEVAL formulation tries harder, so just do it.
+          a (meval `(($float) ,left))
+          b (meval `(($float) ,right))))
+      (if (not (and (numberp a) (numberp b)))
+        (return (values nil a b)))
       (or (> b a) (setq a (prog1 b (setq b a))))
       (setq fa (funcall f a)
 	    fb (funcall f b))
+      (if (not (and (numberp fa) (numberp fb)))
+        (return (values nil a b)))
       (or (> (abs fa) $find_root_abs) (return a))
       (or (> (abs fb) $find_root_abs) (return b))
       (and (plusp (* fa fb))
@@ -55,6 +62,8 @@ Perhaps you meant to enter `~a'.~%"
       binary
       (setq c (* (+ a b) 0.5d0)
 	    fc (funcall f c))
+      (if (not (numberp fc))
+        (return (values nil a b)))
       (and (interpolate-check a c b fc) (return c))
       (cond ((< (abs (- fc (* (+ fa fb) 0.5d0))) (* 1d-1 (- fb fa)))
 	     (incf lin))
@@ -66,42 +75,41 @@ Perhaps you meant to enter `~a'.~%"
 		     (+ a (* (- b a) (/ fa (- fa fb)))))
 		    (t (+ b (* (- a b) (/ fb (- fb fa)))))) 
 	    fc (funcall f c))
+      (if (not (numberp fc))
+        (return (values nil a b)))
       (and (interpolate-check a c b fc) (return c))
       (cond ((plusp fc) (setq fb fc b c)) (t (setq fa fc a c)))
-      (go falsi))))
+      (go falsi)))
 
 (defun interpolate-check (a c b fc)
   (not (and (prog1 (> (abs fc) $find_root_abs) (setq fc (max (abs a) (abs b))))
 	    (> (abs (- b c)) (* $find_root_rel fc))
 	    (> (abs (- c a)) (* $find_root_rel fc)))))
 
-(defun interpolate-macro (form translp)
-  (setq form (cdr form))
-  (cond ((= (length form) 3)
-	 (cond (translp
-		`(($find_root_subr) ,@form))
-	       (t
-		`((mprog) ((mlist) ((msetq) $numer t) ((msetq) $%enumer t))
-		  (($find_root_subr) ,@form)))))
-	((= (length form) 4)
-	 (destructuring-let (((exp var . bnds) form))
-	   (setq exp (sub ($lhs exp) ($rhs exp)))
-	   (cond (translp
-		  `(($find_root_subr)
-		    ((lambda-i) ((mlist) ,var)
-		     (($modedeclare) ,var $float)
-		     ,exp)
-		    ,@bnds))
-		 (t
-		  `((mprog) ((mlist) ((msetq) $numer t) ((msetq) $%enumer t))
-		    (($find_root_subr)
-		     ((lambda) ((mlist) ,var) ,exp)
-		     ,@bnds))))))
-	(t (merror "wrong number of args to `interpolate'"))))
+(defun $find_root (&rest args)
+  (cond
+    ((= (length args) 3)
+     (multiple-value-bind
+       (result left right)
+       (find-root-subr
+         (coerce-float-fun (first args))
+         (second args)
+         (third args))
+       (if (numberp result)
+         result
+         `(($find_root) ,(first args) ,left ,right))))
+    ((= (length args) 4)
+     (multiple-value-bind
+       (result left right)
+       (find-root-subr
+         (coerce-float-fun
+           (sub ($lhs (first args)) ($rhs (first args)))
+           `((mlist) ,(second args)))
+         (third args)
+         (fourth args))
+       (if (numberp result)
+         result
+         `(($find_root) ,(first args) ,(second args) ,left ,right))))
+    (t
+      (wna-err '$find_root))))
 
-(defmspec $find_root (form)
-  (meval (interpolate-macro form nil)))
-
-(def-translate-property $find_root (form)
-  (let (($tr_numer t))
-    (translate (interpolate-macro form t))))
