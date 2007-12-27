2d41b9f28bcff29c5759b876b9c2b11458feafe5
willisbl <willisbl>
2007-12-27 20:20:23 +0000

    o Change to underscore identifiers o  Change least-negative-double-float to least-positive-double-float

--- share/contrib/floatproperties.lisp
+++ share/contrib/floatproperties.lisp
@@ -1,14 +1,14 @@
 ;; Expose some properties of double floating point numbers to Maxima.
 ;; Note: floatbits is one plus the number of bits in the fractional part.
 
-(defmvar $doublefloateps double-float-epsilon)
-(setf (get '$doublefloateps 'assign) 'neverset)
+(defmvar $double_float_eps double-float-epsilon)
+(setf (get '$double_float_eps 'assign) 'neverset)
 
-(defmvar $largestfloat most-positive-double-float)
-(setf (get '$largestfloat 'assign) 'neverset)
+(defmvar $largest_float most-positive-double-float)
+(setf (get '$largest_float 'assign) 'neverset)
 
-(defmvar $leastfloat least-negative-double-float)
-(setf (get '$leastfloat 'assign) 'neverset)
+(defmvar $least_positive_float least-positive-double-float)
+(setf (get '$least_positive_float 'assign) 'neverset)
 
-(defmvar $floatbits (float-digits 0.0))
-(setf (get '$floatbits 'assign) 'neverset)
+(defmvar $float_bits (float-digits 0.0))
+(setf (get '$float_bits 'assign) 'neverset)
