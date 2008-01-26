3d5b72b48ba387404f13aad2aa62608c17c8e133
robert_dodier <robert_dodier>
2008-01-26 20:42:32 +0000

    Minor conditionalization: #\vt is not understood by ABCL.

--- src/mforma.lisp
+++ src/mforma.lisp
@@ -118,7 +118,7 @@
 
 (defmacro white-space-p (x)
   `(member ,x '(#\linefeed #\return #\space #\tab #\page
-		#-(or clisp gcl openmcl) #\vt
+		#-(or clisp gcl openmcl abcl) #\vt
 		#+clisp #\code11)
     :test #'char=))
 
