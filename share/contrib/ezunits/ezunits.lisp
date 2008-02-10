c56df97e3e0e743ae52c4d36399ce12f21242410
robert_dodier <robert_dodier>
2008-02-10 00:15:55 +0000

    (1) New file ezunits/physical_constants.mac w/ data for physical constants. (2) New file ezunits/ezunits.lisp w/ TeX function for backtick operator. (3) Paste copyright and license statement onto existing ezunits files.

---
+++ share/contrib/ezunits/ezunits.lisp
@@ -0,0 +1,40 @@
+;; ezunits: yet another units package for Maxima
+;; This program copyright 2008 by Robert Dodier.
+;; I release this program under the terms of the
+;; GNU General Public License, version 2.
+
+;; Process tex(a`b): throw away the backtick, texify a,
+;; and texify b with all symbols in b output as mathrm.
+
+(defun tex-ezunits (x l r)
+  (append
+    (tex (second x) l r 'mparen 'mparen)
+    (list "\\;")
+    (tex (mathrm-ify (third x)) l r 'mparen 'mparen)))
+
+;; If a symbol has a texword property, preserve it.
+;; Otherwise replace each symbol with a gensym which has a mathrm texword property.
+
+(defun mathrm-ify (e)
+  (let
+    ((v (reverse (cdr ($listofvars e)))))
+    (let
+      ((L (mapcar #'(lambda (s)
+                      (if (get s 'texword)
+                        s
+                        (let
+                          ((g (gensym)))
+                          (putprop
+                            g
+                            (concatenate
+                              'string
+                              "\\mathrm{"
+                              (maybe-invert-string-case (symbol-name (tex-stripdollar0 s)))
+                              "}")
+                            'texword)
+                          g)))
+                  v)))
+      ($substitute `((mlist) ,@(mapcar #'(lambda (a b) `((mequal) ,a ,b)) v L)) e))))
+
+(defprop $\` tex-ezunits tex)
+
