5fe34563766fd3f93aecc4ff0534ade0df023465
dtc <dtc>
2008-02-14 01:53:54 +0000

    o Rework to use the 'flonum type, rather than explicitly using double-float.

--- share/contrib/floatproperties.lisp
+++ share/contrib/floatproperties.lisp
@@ -1,13 +1,13 @@
 ;; Expose some properties of double floating point numbers to Maxima.
 ;; Note: floatbits is one plus the number of bits in the fractional part.
 
-(defmvar $double_float_eps double-float-epsilon)
-(setf (get '$double_float_eps 'assign) 'neverset)
+(defmvar $float_eps flonum-float-epsilon)
+(setf (get '$float_eps 'assign) 'neverset)
 
-(defmvar $largest_float most-positive-double-float)
+(defmvar $largest_float most-positive-flonum)
 (setf (get '$largest_float 'assign) 'neverset)
 
-(defmvar $least_positive_float least-positive-double-float)
+(defmvar $least_positive_float least-positive-flonum)
 (setf (get '$least_positive_float 'assign) 'neverset)
 
 (defmvar $float_bits (float-digits 0.0))
