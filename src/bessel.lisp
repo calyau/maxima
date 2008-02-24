25d5fc8f16da861e5378dc404b3a0ecddb36f99c
billingd <billingd>
2008-02-24 13:51:05 +0000

    Fix for bug 1900843.  Sign of diff(bessel_k(n,x),x) is wrong.  This bug was introduced post maxima-5.14 and is not present in any release.
    A&S 9.6.29 is not too clear for bessel_k. G&R 8.486.11 is clearer and
    agrees with this fix.
    
    Also correct two testsuite cases.

--- src/bessel.lisp
+++ src/bessel.lisp
@@ -1020,6 +1020,7 @@ Perhaps you meant to enter `~a'.~%"
 	 ((%derivative simp) ((%bessel_i simp) n x) n 1)))))
      ;; Derivative wrt to x.  A&S 9.6.29.
      ((mtimes)
+      -1
       ((mplus) ((%bessel_k) ((mplus) -1 n) x)
                ((%bessel_k) ((mplus) 1 n) x))
       ((rat) 1 2)))
