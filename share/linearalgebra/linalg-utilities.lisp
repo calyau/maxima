89b70ed52797d1aec38461b78ac252d640092865
willisbl <willisbl>
2008-03-02 16:34:28 +0000

    o Delete function mytest o fix for blockmatrixp(matrix()) --> true (SF bug 1905668) o fix typo in linearalgebra.mac o faster mat_norm o fix missing quotes in linearalgebra.mac o additional tests for (SF bug 1905668) in test-linalg.mac

--- share/linearalgebra/linalg-utilities.lisp
+++ share/linearalgebra/linalg-utilities.lisp
@@ -13,17 +13,14 @@
   (if (member level (member ($get '$infolevel pck) `($debug $verbose $silent)))
       (apply 'mtell `(,msg ,@arg))))
 
-(defun $mytest (fn)
-  (let ((*collect-errors* nil))
-    (setq fn ($file_search fn))
-    (test-batch fn nil :show-all nil :show-expected nil)))
-
 (defun $require_nonempty_matrix (m pos fun)
   (if (not (and ($matrixp m) (> ($length m) 0) (> ($length ($first m)) 0)))
       (merror "The ~:M argument of the function ~:M must be a nonempty matrix" pos fun)))
 
+;; Why both some and every? Because we want blockmatrixp(matrix()) --> false.
+
 (defun $blockmatrixp (m) 
-  (and ($matrixp m) ($every '$matrixp m)))
+  (and ($matrixp m) ($some '$matrixp m) ($every '$matrixp m)))
 
 (defun $require_matrix (m pos fun)
   (if (not ($matrixp m))
