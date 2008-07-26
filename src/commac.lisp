90a947214bdef74657fdc60aea22936ad51e1043
robert_dodier <robert_dodier>
2008-07-26 17:23:48 +0000

    Revise timedate function to output time/date string in ISO 8601 format. Example: "2008-07-26 11:25:36-06:00" To test a timezone other than the default, I used tzselect (Linux). In default and non-default timezones (US Mountain and Asia/Novosibirsk) CMUCL, Clisp, and SBCL yield correct results. GCL yields correct result for default, but apparently does not observe the TZ variable (returns default result in non-default timezone).

--- src/commac.lisp
+++ src/commac.lisp
@@ -608,10 +608,19 @@ values")
 (defvar ^r nil)
 
 (defun $timedate ()
-  (multiple-value-bind (second minute hour date month year day-of-week dst-p tz)
-      (get-decoded-time)
-    (format nil "~2,'0d:~2,'0d:~2,'0d ~[Mon~;Tue~;Wed~;Thu~;Fri~;Sat~;Sun~], ~d/~2,'0d/~d (GMT~@d)"
-	    hour minute second day-of-week month date year (if dst-p (- 1 tz) (- tz)))))
+  (multiple-value-bind
+    (second minute hour date month year day-of-week dst-p tz)
+    (get-decoded-time)
+    (declare (ignore day-of-week))
+    (let
+      ((tz-offset (if dst-p (- 1 tz) (- tz))))
+      (multiple-value-bind
+        (tz-hours tz-hour-fraction)
+        (floor tz-offset)
+        (let
+          ((tz-sign (if (< 0 tz-hours) #\+ #\-)))
+          (format nil "~4,'0d-~2,'0d-~2,'0d ~2,'0d:~2,'0d:~2,'0d~a~2,'0d:~2,'0d"
+              year month date hour minute second tz-sign (abs tz-hours) (floor (* 60 tz-hour-fraction))))))))
 
 ;;Some systems make everything functionp including macros:
 (defun functionp (x)
