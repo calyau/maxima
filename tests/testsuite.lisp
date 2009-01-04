9bb912c54ba5fb65d7d4f49fb5ca526dfaa6d6d4
crategus <crategus>
2009-01-04 15:33:02 +0000

    Removing tests 66,66 and 72 from the list of expected failures. These tests work after implementation of taylorize for the functions log and exp.
    Tested with GCL 2.44 and CLISP 2.44. No problems.

--- tests/testsuite.lisp
+++ tests/testsuite.lisp
@@ -36,7 +36,7 @@
 	"rtestifactor"
 	((mlist) "rtest_equal" 157 160)
 	((mlist) "rtest_abs" 42 43)
-	((mlist) "rtest_taylor" 66 67 72 88 91 94 96 99 104 118 119 120 121 123 124)
+	((mlist) "rtest_taylor" 88 91 94 96 99 104 118 119 120 121 123 124)
 	((mlist) "rtest_dot")
 	"rtest_mset"
 	((mlist) "rtest_boolean" 100 101)
