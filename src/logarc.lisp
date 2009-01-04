9aefcc3e0ebb56a6b4646b53eda6a42eb78573f9
crategus <crategus>
2009-01-04 13:39:25 +0000

    Implementation of a general factor for the half-angle-transformation of the functions sin, cos, sinh and cosh.
    The transformation simplifies to correct expressions for real and complex arguments of the trigonometrix and hyperbolic functions.
    
    In rtest_trig.mac restriction of the test in Problem 33 to a postive interval (0,3). The results of this test are given for this interval.
    
    Related bug: SF [505443] Halfangle limitation
    
    Tested with CLISP 2.44 and GCL 2.6.8. No problems with the testsuite.

--- src/logarc.lisp
+++ src/logarc.lisp
@@ -64,13 +64,44 @@
 (defun halfangleaux (f a) ;; f=function; a=twice argument
   (let ((sw (member f '(%cos %cot %coth %cosh) :test #'eq)))
     (cond ((member f '(%sin %cos) :test #'eq)
-	   (power (div (add 1 (porm sw (take '(%cos) a))) 2) (1//2)))
-	  ((member f '(%tan %cot) :test #'eq)
-	   (div (add 1 (porm sw (take '(%cos) a))) (take '(%sin) a)))
-	  ((member f '(%sinh %cosh) :test #'eq)
-	   (power (div (add (take '(%cosh) a) (porm sw 1)) 2) (1//2)))
+           (mul (halfangleaux-factor f a)
+                (power (div (add 1 (porm sw (take '(%cos) a))) 2) (1//2))))
+          ((member f '(%tan %cot) :test #'eq)
+           (div (add 1 (porm sw (take '(%cos) a))) (take '(%sin) a)))
+          ((member f '(%sinh %cosh) :test #'eq)
+           (mul (halfangleaux-factor f a)
+                (power (div (add (take '(%cosh) a) (porm sw 1)) 2) (1//2))))
 	  ((member f '(%tanh %coth) :test #'eq)
 	   (div (add (take '(%cosh) a) (porm sw 1)) (take '(%sinh) a)))
 	  ((member f '(%sec %csc %sech %csch) :test #'eq)
 	   (inv (halfangleaux (get f 'recip) a))))))
 
+(defun halfangleaux-factor (f a)
+  (cond 
+    ((member f '(%sin %cos))
+     (let ((arg (div (if (eq f '%sin) 
+                         ($realpart a)
+                         (add ($realpart a) '$%pi)) 
+                     (mul 2 '$%pi))))
+       (mul 
+         (power -1 (simplify (list '($floor) arg)))
+         (sub 1
+           (mul 
+             (add 1
+               (power -1 (add (simplify (list '($floor) arg))
+                              (simplify (list '($floor) (mul -1 arg))))))
+               (simplify (list '($unit_step) (mul -1 ($imagpart a)))))))))
+    ((member f '(%sinh %cosh))
+     (let ((arg (div (add ($imagpart a) '$%pi) (mul 2 '$%pi)))
+           (fac (if (eq f '%sinh)
+                    (div (power (power a 2) (div 1 2)) a)
+                    1)))
+       (mul fac
+         (power -1 (simplify (list '($floor) arg)))
+         (sub 1
+           (mul 
+             (add 1
+               (power -1 (add (simplify (list '($floor) arg))
+                              (simplify (list '($floor) (mul -1 arg))))))
+               (simplify (list '($unit_step) ($realpart a))))))))
+    (t 1)))
