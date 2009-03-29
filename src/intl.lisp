f97c36df262e25786789fb2ee26dfd0d4c319371
andrejv <andrejv>
2009-03-29 08:14:26 +0000

    New upstream version.

--- src/intl.lisp
+++ src/intl.lisp
@@ -1,6 +1,6 @@
 ;;; -*- Mode: LISP; Syntax: ANSI-Common-Lisp; Package: INTL -*-
 
-;;; $Revision: 1.8 $
+;;; $Revision: 1.9 $
 ;;; Copyright 1999 Paul Foley (mycroft@actrix.gen.nz)
 ;;;
 ;;; Permission is hereby granted, free of charge, to any person obtaining
@@ -23,7 +23,13 @@
 ;;; (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
 ;;; USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH
 ;;; DAMAGE.
-#+CMU (ext:file-comment "$Header: /home/lbutler/maxima/sandbox/cvs/maxima/maxima/src/intl.lisp,v 1.8 2009-03-13 14:49:17 are_muc Exp $")
+#+CMU (ext:file-comment "$Header: /home/lbutler/maxima/sandbox/cvs/maxima/maxima/src/intl.lisp,v 1.9 2009-03-29 08:14:26 andrejv Exp $")
+
+(defpackage "INTL"
+  (:use "COMMON-LISP")
+  (:export "SETLOCALE" "TEXTDOMAIN" "GETTEXT" "DGETTEXT" "NGETTEXT" "DNGETTEXT"
+           "*TRANSLATABLE-DUMP-STREAM*" "READ-TRANSLATABLE-STRING"
+	   "*LOCALE-DIRECTORIES*"))
 
 (in-package :intl)
 
@@ -39,6 +45,7 @@
   (domain "" :type simple-base-string)
   (locale "" :type simple-base-string)
   (file #p"" :type pathname)
+  (plurals nil :type (or null function))
   (hash (make-hash-table :test #'equal) :type hash-table)
   (encoding nil))
 
@@ -69,17 +76,216 @@
 		     (and us (probe-file (path (subseq locale 0 us) base)))))))
 	  (when probe (return probe)))))))
 
-(defun guess-locale-encoding (locale)
-  (let ((locale (or (gethash locale *locale-aliases*) locale)))
-    (or (let ((dot (position #\. locale)))
-	  (and dot (intern (string-upcase (subseq locale (1+ dot)))
-			   "KEYWORD")))
-	(gethash locale *locale-encoding*)
-	(let ((at (position #\@ locale)))
-	  (and at (gethash (subseq locale 0 at) *locale-encoding*)))
-	(let ((us (position #\_ locale)))
-	  (and us (gethash (subseq locale 0 us) *locale-encoding*)))
-	:utf-8)))
+(defun find-encoding (domain)
+  (when (null (domain-entry-encoding domain))
+    (setf (domain-entry-encoding domain) :iso-8859-1)
+    (let* ((header (domain-lookup "" domain))
+	   (ctype (search "Content-Type: " header))
+	   (eoln (and ctype (position #\Newline header :start ctype)))
+	   (charset (and ctype (search "; charset=" header
+				       :start2 ctype :end2 eoln))))
+      (when charset
+	(incf charset 10)
+	(loop for i upfrom charset below eoln as c = (char header i)
+            while (or (alphanumericp c) (eql c #\-))
+          finally (setf (domain-entry-encoding domain)
+		      (intern (subseq header charset i) "KEYWORD"))))))
+  domain)
+
+(defun parse-plurals (domain)
+  (let* ((header (domain-lookup "" domain))
+	 (plurals (search "Plural-Forms: " header))
+	 (default (lambda (n) (if (= n 1) 0 1))))
+    (if (and plurals
+	     (> (length header) (+ plurals 36))
+	     (string= header "nplurals="
+		      :start1 (+ plurals 14) :end1 (+ plurals 23)))
+	(let ((nplurals
+	       (parse-integer header :start (+ plurals 23) :junk-allowed t))
+	      (point (+ (position #\; header :start (+ plurals 23)) 2)))
+	  (if (and (> (length header) (+ point 10))
+		   (string= header "plural=" :start1 point :end1 (+ point 7)))
+	      (values (parse-expr header (+ point 7)) nplurals)
+	      (values default 2)))
+	(values default 2))))
+
+(defun parse-expr (string pos)
+  (labels ((next ()
+	     (loop while (member (char string pos) '(#\Space #\Tab #\Newline))
+		   do (incf pos))
+	     (case (char string (1- (incf pos)))
+	       (#\n 'n)
+	       (#\? 'IF)
+	       (#\: 'THEN)
+	       (#\( 'LPAR)
+	       (#\) 'RPAR)
+	       (#\^ 'LOGXOR)
+	       (#\+ 'ADD)
+	       (#\- 'SUB)
+	       (#\* 'MUL)
+	       (#\/ 'FLOOR)
+	       (#\% 'MOD)
+	       (#\~ 'LOGNOT32)
+	       (#\; 'END)
+	       (#\| (if (char= (char string pos) #\|)
+			(progn (incf pos) 'COR)
+			'LOGIOR))
+	       (#\& (if (char= (char string pos) #\&)
+			(progn (incf pos) 'CAND)
+			'LOGAND))
+	       (#\= (if (char= (char string pos) #\=)
+			(progn (incf pos) 'CMP=)
+			(error "Encountered illegal token: =")))
+	       (#\! (if (char= (char string pos) #\=)
+			(progn (incf pos) 'CMP/=)
+			'NOT))
+	       (#\< (case (char string pos)
+		      (#\= (incf pos) 'CMP<=)
+		      (#\< (incf pos) 'SHL)
+		      (otherwise 'CMP<)))
+	       (#\> (case (char string pos)
+		      (#\= (incf pos) 'CMP>=)
+		      (#\> (incf pos) 'SHR)
+		      (otherwise 'CMP>)))
+	       (otherwise (let ((n (digit-char-p (char string (1- pos)))))
+			    (if n
+				(loop for nx = (digit-char-p (char string pos))
+				      while nx
+				   do (setq n (+ (* n 10) nx)) (incf pos)
+				   finally (return n))
+				(error "Encountered illegal token: ~C"
+				       (char string (1- pos))))))))
+	   (conditional (tok &aux tree)
+	     (multiple-value-setq (tree tok) (logical-or tok))
+	     (when (eql tok 'IF)
+	       (multiple-value-bind (right next) (logical-or (next))
+		 (unless (eql next 'THEN)
+		   (error "Expected : in ?: construct"))
+		 (multiple-value-bind (else next) (conditional (next))
+		   (setq tree (list tok (list 'zerop tree) else right)
+			 tok next))))
+	     (values tree tok))
+	   (logical-or (tok &aux tree)
+	     (multiple-value-setq (tree tok) (logical-and tok))
+	     (loop while (eql tok 'COR) do
+		(multiple-value-bind (right next) (logical-and (next))
+		  (setq tree (list tok tree right)
+			tok next)))
+	     (values tree tok))
+	   (logical-and (tok &aux tree)
+	     (multiple-value-setq (tree tok) (inclusive-or tok))
+	     (loop while (eql tok 'CAND) do
+		(multiple-value-bind (right next) (inclusive-or (next))
+		  (setq tree (list tok tree right)
+			tok next)))
+	     (values tree tok))
+	   (inclusive-or (tok &aux tree)
+	     (multiple-value-setq (tree tok) (exclusive-or tok))
+	     (loop while (eql tok 'LOGIOR) do
+		(multiple-value-bind (right next) (exclusive-or (next))
+		  (setq tree (list tok tree right)
+			tok next)))
+	     (values tree tok))
+	   (exclusive-or (tok &aux tree)
+	     (multiple-value-setq (tree tok) (bitwise-and tok))
+	     (loop while (eql tok 'LOGXOR) do
+		(multiple-value-bind (right next) (bitwise-and (next))
+		  (setq tree (list tok tree right)
+			tok next)))
+	     (values tree tok))
+	   (bitwise-and (tok &aux tree)
+	     (multiple-value-setq (tree tok) (equality tok))
+	     (loop while (eql tok 'LOGAND) do
+		(multiple-value-bind (right next) (equality (next))
+		  (setq tree (list tok tree right)
+			tok next)))
+	     (values tree tok))
+	   (equality (tok &aux tree)
+	     (multiple-value-setq (tree tok) (relational tok))
+	     (loop while (member tok '(CMP= CMP/=)) do
+		(multiple-value-bind (right next) (relational (next))
+		  (setq tree (list tok tree right)
+			tok next)))
+	     (values tree tok))
+	   (relational (tok &aux tree)
+	     (multiple-value-setq (tree tok) (shift tok))
+	     (loop while (member tok '(CMP< CMP> CMP<= CMP>=)) do
+		(multiple-value-bind (right next) (shift (next))
+		  (setq tree (list tok tree right)
+			tok next)))
+	     (values tree tok))
+	   (shift (tok &aux tree)
+	     (multiple-value-setq (tree tok) (additive tok))
+	     (loop while (member tok '(SHL SHR)) do
+		(multiple-value-bind (right next) (additive (next))
+		  (setq tree (list tok tree right)
+			tok next)))
+	     (values tree tok))
+	   (additive (tok &aux tree)
+	     (multiple-value-setq (tree tok) (multiplicative tok))
+	     (loop while (member tok '(ADD SUB)) do
+		(multiple-value-bind (right next) (multiplicative (next))
+		  (setq tree (list tok tree right)
+			tok next)))
+	     (values tree tok))
+	   (multiplicative (tok &aux tree)
+	     (multiple-value-setq (tree tok) (unary tok))
+	     (loop while (member tok '(MUL FLOOR MOD)) do
+		(multiple-value-bind (right next) (unary (next))
+		  (setq tree (list tok tree right)
+			tok next)))
+	     (values tree tok))
+	   (unary (tok &aux tree)
+	     (cond ((eq tok 'LPAR)
+		    (multiple-value-setq (tree tok) (conditional (next)))
+		    (unless (eq tok 'RPAR)
+		      (error "Expected close-paren."))
+		    (values tree (next)))
+		   ((numberp tok)
+		    (values tok (next)))
+		   ((eql tok 'n)
+		    (values tok (next)))
+		   ((eql tok 'ADD)
+		    (unary (next)))
+		   ((eql tok 'SUB)
+		    (multiple-value-setq (tree tok) (unary (next)))
+		    (values (list '- tree) tok))
+		   ((eql tok 'LOGNOT32)
+		    (multiple-value-setq (tree tok) (unary (next)))
+		    (values (list 'LOGNOT32 tree) tok))
+		   ((eql tok 'NOT)
+		    (multiple-value-setq (tree tok) (unary (next)))
+		    (values (list 'CNOT tree) tok))
+		   (t
+		    (error "Unexpected token: ~S." tok)))))
+    (multiple-value-bind (tree end) (conditional (next))
+      (unless (eq end 'END)
+	(error "Expecting end of expression.  ~S." end))
+      (let ((*compile-print* nil))
+	(compile nil
+		 `(lambda (n)
+		    (declare (type (unsigned-byte 32) n)
+			     (optimize (space 3)))
+		    (flet ((add   (a b) (ldb (byte 32 0) (+ a b)))
+			   (sub   (a b) (ldb (byte 32 0) (- a b)))
+			   (mul   (a b) (ldb (byte 32 0) (* a b)))
+			   (shl   (a b) (ldb (byte 32 0) (ash a b)))
+			   (shr   (a b) (ash a (- b)))
+			   (cmp=  (a b) (if (= a b) 1 0))
+			   (cmp/= (a b) (if (/= a b) 1 0))
+			   (cmp<  (a b) (if (< a b) 1 0))
+			   (cmp<= (a b) (if (<= a b) 1 0))
+			   (cmp>  (a b) (if (> a b) 1 0))
+			   (cmp>= (a b) (if (>= a b) 1 0))
+			   (cand  (a b) (if (or (zerop a) (zerop b)) 0 1))
+			   (cor   (a b) (if (and (zerop a) (zerop b)) 0 1))
+			   (cnot  (a)   (if a 0 1))
+			   (lognot32 (a) (ldb (byte 32 0) (lognot a))))
+		      (declare (ignorable #'add #'sub #'mul #'shr #'shl
+					  #'cmp= #'cmp/=
+					  #'cmp< #'cmp<= #'cmp> #'cmp>=
+					  #'cand #'cor #'cnot #'lognot32))
+		      ,tree)))))))
 
 (defun load-domain (domain locale &optional (locale-dir *locale-directories*))
   (let ((file (locate-domain-file domain locale locale-dir)))
@@ -105,8 +311,8 @@
 	    (setf (gethash length (domain-entry-hash entry))
 		  (acons start (+ translation (* 8 msg))
 			 (gethash length (domain-entry-hash entry))))))
-	(setf (domain-entry-encoding entry) (guess-locale-encoding locale))
-	(setf (gethash domain *loaded-domains*) entry)))))
+	(setf (gethash domain *loaded-domains*) entry)
+	(find-encoding entry)))))
 
 (defun find-domain (domain locale &optional (locale-dir *locale-directories*))
   (let ((found (gethash domain *loaded-domains*)))
@@ -140,6 +346,41 @@
     (unless (= (aref a start1) (aref b start2)) (return nil))
     (when (or (= (incf start1) end1) (= (incf start2) end2)) (return t))))
 
+(defun search-domain (octets domain pos)
+  (declare (type (simple-array (unsigned-byte 8) (*)) octets)
+	   (type domain-entry domain)
+	   (type list pos)
+	   (optimize (speed 3) (space 2) (safety 0) (debug 0)))
+  (when pos
+    (let ((temp (make-array 120 :element-type '(unsigned-byte 8)))
+	  (length (length octets)))
+      (with-open-file (stream (domain-entry-file domain)
+			      :direction :input
+			      :element-type '(unsigned-byte 8))
+	(dolist (entry pos)
+	  (file-position stream (car entry))
+	  (let ((off 0)
+		(end (read-sequence temp stream
+				    :end (min 120 length))))
+	    (declare (type (integer 0 #.array-dimension-limit) off end))
+	    (loop while (octets= octets temp
+			  :start1 off
+			  :end1 (min (+ off 120) length)
+			  :end2 end)
+	      do
+		(incf off end)
+		(when (< off length)
+		  (setf end (read-sequence temp stream
+					   :end (min 120 (- length off))))))
+	    (when (= off length)
+	      (file-position stream (cdr entry))
+	      (let* ((len (read-lelong stream))
+		     (off (read-lelong stream))
+		     (tmp (make-array len :element-type '(unsigned-byte 8))))
+		(file-position stream off)
+		(read-sequence tmp stream)
+		(return (values tmp entry))))))))))
+
 (defun domain-lookup (string domain)
   (declare (type string string) (type domain-entry domain)
 	   (optimize (speed 3) (space 2) (safety 0) (debug 0)))
@@ -148,51 +389,56 @@
       (let* ((octets (string-to-octets string
 				       (domain-entry-encoding domain)))
 	     (length (length octets))
-	     (pos (gethash length (domain-entry-hash domain)))
-	     (temp (make-array 120 :element-type '(unsigned-byte 8))))
-	(declare (type (simple-array (unsigned-byte 8) (*)) octets)
-		 (type list pos))
-	(when pos
-	  (with-open-file (stream (domain-entry-file domain)
-				  :direction :input
-				  :element-type '(unsigned-byte 8))
-	    (dolist (entry pos)
-	      (file-position stream (car entry))
-	      (let ((off 0)
-		    (end (read-sequence temp stream
-					:end (min 120 length))))
-		(loop while (octets= octets temp
-				     :start1 off
-				     :end1 (min (+ off 120) length)
-				     :end2 end)
-		  do
-		  (incf off end)
-		  (when (< off length)
-		    (setf end (read-sequence temp stream
-					     :end (min 120 (- length off))))))
-		(when (= off length)
-		  (file-position stream (cdr entry))
-		  (let* ((len (read-lelong stream))
-			 (off (read-lelong stream))
-			 (tmp (make-array len
-					  :element-type '(unsigned-byte 8)))
-			 str)
-		    (file-position stream off)
-		    (read-sequence tmp stream)
-		    (setq str (octets-to-string tmp
-						(domain-entry-encoding
-						 domain)))
-		    (setf (gethash (copy-seq string)
-				   (domain-entry-hash domain))
-			  str)
-		    (let ((temp (delete entry pos :test #'eq)))
-		      (if temp
-			  (setf (gethash (length string)
-					 (domain-entry-hash domain))
-				temp)
-			  (remhash (length string)
-				   (domain-entry-hash domain))))
-		    (return-from domain-lookup str))))))))))
+	     (pos (gethash length (domain-entry-hash domain))))
+	(declare (type vector octets))
+	(multiple-value-bind (tmp entry) (search-domain octets domain pos)
+	  (declare (type (or null (simple-array (unsigned-byte 8) (*))) tmp))
+	  (when tmp
+	    (let ((temp (delete entry pos :test #'eq)))
+	      (if temp
+		  (setf (gethash length (domain-entry-hash domain)) temp)
+		  (remhash length (domain-entry-hash domain))))
+	    (setf (gethash (copy-seq string) (domain-entry-hash domain))
+		(octets-to-string tmp (domain-entry-encoding domain))))))))
+
+(defun domain-lookup-plural (singular plural domain)
+  (declare (type string singular plural) (type domain-entry domain)
+	   (optimize (speed 3) (space 2) (safety 0) (debug 0)))
+  (or (if (null (domain-entry-encoding domain)) nil)
+      (gethash (cons singular plural) (domain-entry-hash domain))
+      (let* ((octets (let* ((a (string-to-octets singular
+					       (domain-entry-encoding domain)))
+			    (b (string-to-octets plural
+					       (domain-entry-encoding domain)))
+			    (c (make-array (+ (length a) (length b) 1)
+					   :element-type '(unsigned-byte 8))))
+		       (declare (type (simple-array (unsigned-byte 8) (*))
+				      a b c))
+		       (replace c a)
+		       (setf (aref c (length a)) 0)
+		       (replace c b :start1 (+ (length a) 1))
+		       c))
+	     (length (length octets))
+	     (pos (gethash length (domain-entry-hash domain))))
+	(declare (type vector octets) (type list pos))
+	(multiple-value-bind (tmp entry) (search-domain octets domain pos)
+	  (declare (type (or null (simple-array (unsigned-byte 8) (*))) tmp))
+	  (when tmp
+	    (prog1
+		(setf (gethash (cons (copy-seq singular) (copy-seq plural))
+			       (domain-entry-hash domain))
+		    (loop for i = 0 then (1+ j)
+			   as j = (position 0 tmp :start i)
+		      collect (octets-to-string (subseq tmp i j)
+						(domain-entry-encoding domain))
+		      while j))
+	      (let ((temp (delete entry pos :test #'eq)))
+		(if temp
+		    (setf (gethash length (domain-entry-hash domain)) temp)
+		    (remhash length (domain-entry-hash domain))))
+	      (when (null (domain-entry-plurals domain))
+		(setf (domain-entry-plurals domain)
+		    (parse-plurals domain)))))))))
 
 (declaim (inline getenv)
 	 (ftype (function (string) (or null string)) getenv))
@@ -215,15 +461,15 @@
 		     (getenv "LANG")
 		     *locale*)))
 
-;; This is a macro so that it can take effect at compile-time, in order
-;; to allow the _" reader macro to define the correct domain.
 (defmacro textdomain (domain)
   `(eval-when (:compile-toplevel :execute)
-    (setf *default-domain* ,domain)))
+     (setf *default-domain* ,domain)))
 
-;; This is a macro so that it can capture the correct value of *default-domain*
 (defmacro gettext (string)
   `(dgettext ,*default-domain* ,string))
+
+(defmacro ngettext (singular plural n)
+  `(dngettext ,*default-domain* ,singular ,plural ,n))
 
 (declaim (inline dgettext))
 (defun dgettext (domain string)
@@ -232,42 +478,17 @@
   (let ((domain (find-domain domain *locale*)))
     (or (and domain (domain-lookup string domain)) string)))
 
-#+(or)
-(define-compiler-macro dgettext (domain string)
-  (let ((cache (gensym))
-	(clean (gensym))
-	(dom (gensym))
-	(str (gensym)))
-    `(let ((,cache (load-time-value (vector nil nil nil)))
-	   (,clean t)
-	   (,dom ,domain)
-	   (,str ,string))
-       (declare (optimize (speed 3) (space 2) (safety 0) (debug 0))
-		(type (simple-array t (3)) ,cache)
-		(ignorable ,str))
-       (unless (and (aref ,cache 0)
-		    (string= (domain-entry-domain
-			      (the domain-entry (aref ,cache 0)))
-			     ,dom)
-		    (string= (domain-entry-locale
-			      (the domain-entry (aref ,cache 0)))
-			     *locale*))
-	 (setf (aref ,cache 0) (find-domain ,dom *locale*)
-	       ,clean nil))
-       ,(if (stringp string)
-	    `(if ,clean
-		 (aref ,cache 1)
-		 (setf (aref ,cache 1)
-		     (or (and (aref ,cache 0)
-			      (domain-lookup ,string (aref ,cache 0)))
-		         ,string)))
-	    `(if (and ,clean (string= (aref ,cache 2) ,str))
-		 (aref ,cache 1)
-		 (setf (aref ,cache 1) (or (and (aref ,cache 0)
-						(domain-lookup ,str
-							      (aref ,cache 0)))
-					   ,str)
-		       (aref ,cache 2) ,str))))))
+(defun dngettext (domain singular plural n)
+  "Look up the singular or plural form of a message in the specified domain."
+  (declare (type integer n)
+	   (optimize (speed 3) (space 2) (safety 0) (debug 0)))
+  (let* ((domain (find-domain domain *locale*))
+	 (list (and domain (domain-lookup-plural singular plural domain))))
+    (if list
+	(nth (the integer
+	       (funcall (the function (domain-entry-plurals domain)) n))
+	     list)
+	(if (= n 1) singular plural))))
 
 (defun po-output-string (stream string)
   (format stream "~&msgid ~S~%msgstr \"\"~2%" string))
