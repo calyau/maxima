d44b9188bfbd68fa3708e0d65aee4425607edd02
are_muc <are_muc>
2009-09-21 16:57:15 +0000

    replaced boundp hackery with defmvar; tried to increase readability

--- src/intpol.lisp
+++ src/intpol.lisp
@@ -16,11 +16,9 @@
 
 (load-macsyma-macros transm)
 
-(declare-top (special $find_root_rel $find_root_abs $find_root_error)) 
-
-(or (boundp '$find_root_abs) (setq $find_root_abs 0.0)) 
-(or (boundp '$find_root_rel) (setq $find_root_rel 0.0))
-(or (boundp '$find_root_error) (setq $find_root_error t))
+(defmvar $find_root_abs 0.0)
+(defmvar $find_root_rel 0.0)
+(defmvar $find_root_error t)
 
 (defmspec $interpolate (form)
   (format t "NOTE: The interpolate function has been renamed to find_root.
@@ -31,81 +29,82 @@ Perhaps you meant to enter `~a'.~%"
   '$done)
 
 (defun find-root-subr (f left right)
-   (prog (a b c fa fb fc (lin 0))
-      (declare (fixnum lin))
-      (let (($numer t) ($%enumer t))
-        (setq
-          a ($float left)
-          b ($float right)))
-      (if (not (and (numberp a) (numberp b)))
-        (return (values nil a b)))
-      (or (> b a) (setq a (prog1 b (setq b a))))
-      (setq fa (funcall f a)
-	    fb (funcall f b))
-      (if (not (and (numberp fa) (numberp fb)))
-        (return (values nil a b)))
-      (or (> (abs fa) $find_root_abs) (return a))
-      (or (> (abs fb) $find_root_abs) (return b))
-      (and (plusp (* fa fb))
-	   (cond ((eq $find_root_error t)
-              (merror (intl:gettext "find_root: function has same sign at endpoints: ~M, ~M")
-                      `((mequal) ((f) ,a) ,fa)
-                      `((mequal) ((f) ,b) ,fb)))
-		 (t (return $find_root_error))))
-      (and (> fa 0.0)
-	   (setq fa (prog2 nil fb (setq fb fa)) a (prog2 nil b (setq b a))))
-      (setq lin 0)
-      binary
-      (setq c (* (+ a b) 0.5)
-	    fc (funcall f c))
-      (if (not (numberp fc))
-        (return (values nil a b)))
-      (and (interpolate-check a c b fc) (return c))
-      (cond ((< (abs (- fc (* (+ fa fb) 0.5))) (* 1e-1 (- fb fa)))
-	     (incf lin))
-	    (t (setq lin 0)))
-      (cond ((> fc 0.0) (setq fb fc b c)) (t (setq fa fc a c)))
-      (or (= lin 3) (go binary))
-      falsi
-      (setq c (cond ((plusp (+ fb fa))
-		     (+ a (* (- b a) (/ fa (- fa fb)))))
-		    (t (+ b (* (- a b) (/ fb (- fb fa)))))) 
-	    fc (funcall f c))
-      (if (not (numberp fc))
-        (return (values nil a b)))
-      (and (interpolate-check a c b fc) (return c))
-      (cond ((plusp fc) (setq fb fc b c)) (t (setq fa fc a c)))
-      (go falsi)))
+  (let (($numer t) ($%enumer t))
+    (setq left ($float left)
+	  right ($float right)))
+  (unless (and (numberp left) (numberp right)) ; the interval boundaries must have
+    (return-from find-root-subr (values nil left right))) ;numerical values
+  (when (< right left) ;make left the lower and right the upper bound of the interval
+    (psetq left right right left))
+  (let ((lin 0) (a left) (b right) (fa (funcall f left)) (fb (funcall f right)) c fc)
+    (unless (and (numberp fa) (numberp fb))
+      (return-from find-root-subr (values nil a b)))
+    (when (<= (abs fa) $find_root_abs) ;if a or be is already small enough
+      (return-from find-root-subr a))  ;return it as the root
+    (when (<= (abs fb) $find_root_abs)
+      (return-from find-root-subr b))
+    (when (plusp (* fa fb))
+      (if (eq $find_root_error t)
+	  (merror (intl:gettext "find_root: function has same sign at endpoints: ~M, ~M")
+		  `((mequal) ((f) ,a) ,fa)
+		  `((mequal) ((f) ,b) ,fb))
+	  (return-from find-root-subr $find_root_error)))
+    (when (plusp fa)
+      (psetq fa fb
+	     fb fa
+	     a b
+	     b a))
+    (loop while (< lin 3) do		; use binary search to close in on the root
+	 (setq c (* 0.5 (+ a b))
+	       fc (funcall f c))
+	 (unless (numberp fc)
+	   (return-from find-root-subr (values nil a b)))
+	 (when (interpolate-check a c b fc)
+	   (return-from find-root-subr c))
+	 (if (< (abs (- fc (* 0.5 (+ fa fb)))) (* 0.1 (- fb fa)))
+	     (incf lin)
+	     (setq lin 0))
+	 (if (plusp fc)
+	     (setq fb fc b c)
+	     (setq fa fc a c)))
+    (loop				; now use the regula falsi
+       (setq c (if (plusp (+ fb fa))
+		   (+ a (* (- b a) (/ fa (- fa fb))))
+		   (+ b (* (- a b) (/ fb (- fb fa)))))
+	     fc (funcall f c))
+       (unless (numberp fc)
+	 (return-from find-root-subr (values nil a b)))
+       (when (interpolate-check a c b fc)
+	 (return-from find-root-subr c))
+       (if (plusp fc)
+	   (setq fb fc b c)
+	   (setq fa fc a c)))))
 
 (defun interpolate-check (a c b fc)
-  (not (and (prog1 (> (abs fc) $find_root_abs) (setq fc (max (abs a) (abs b))))
+  (not (and (prog1
+		(> (abs fc) $find_root_abs)
+	      (setq fc (max (abs a) (abs b))))
 	    (> (abs (- b c)) (* $find_root_rel fc))
 	    (> (abs (- c a)) (* $find_root_rel fc)))))
 
-(defun $find_root (&rest args)
-  (cond
-    ((= (length args) 3)
-     (multiple-value-bind
-       (result left right)
-       (find-root-subr
-         (coerce-float-fun (first args))
-         (second args)
-         (third args))
+(defun $find_root (fun-or-expr &rest args)
+  (case (length args)
+    (2					; function case: f, lo, hi
+     (multiple-value-bind (result left right)
+	 (find-root-subr (coerce-float-fun fun-or-expr)
+			 (first args)
+			 (second args))
        (if (numberp result)
-         result
-         `(($find_root) ,(first args) ,left ,right))))
-    ((= (length args) 4)
-     (multiple-value-bind
-       (result left right)
-       (find-root-subr
-         (coerce-float-fun
-           (sub ($lhs (first args)) ($rhs (first args)))
-           `((mlist) ,(second args)))
-         (third args)
-         (fourth args))
+	   result
+	   `(($find_root) ,fun-or-expr ,left ,right))))
+    (3					;expr case: expr, var, lo, hi
+     (multiple-value-bind (result left right)
+	 (find-root-subr (coerce-float-fun (sub ($lhs fun-or-expr) ($rhs fun-or-expr))
+					   `((mlist) ,(first args)))
+			 (second args)
+			 (third args))
        (if (numberp result)
-         result
-         `(($find_root) ,(first args) ,(second args) ,left ,right))))
-    (t
-      (wna-err '$find_root))))
-
+	   result
+	   `(($find_root) ,fun-or-expr ,(first args) ,left ,right))))
+    (t					;wrong number of args
+     (wna-err '$find_root))))
