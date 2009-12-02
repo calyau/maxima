ef589311fc0a51dcdf4b162321c2245103b96b08
dtc <dtc>
2009-12-02 12:50:17 +0000

    * The Scieneer CL needs to use the 'cl-user package not the 'user package.

--- src/maxima-build.lisp
+++ src/maxima-build.lisp
@@ -32,7 +32,7 @@
   #+sbcl (sb-ext:save-lisp-and-die "binary-sbcl/maxima.core" :toplevel #'cl-user::run)
   #+gcl (si:save-system "binary-gcl/maxima")
   #+cmu (extensions:save-lisp "binary-cmucl/maxima.core" :init-function 'cl-user::run)
-  #+scl (extensions:save-lisp "binary-scl/maxima.core" :init-function 'user::run)
+  #+scl (extensions:save-lisp "binary-scl/maxima.core" :init-function 'cl-user::run)
   #+allegro (excl:dumplisp :name "binary-acl/maxima.dxl")
   #+lispworks (hcl:save-image "binary-lispworks/maxima" :restart-function 'cl-user::run)
   #+ccl (ccl:save-application "binary-openmcl/maxima" :toplevel-function 'cl-user::run))
