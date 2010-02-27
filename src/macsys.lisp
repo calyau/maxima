f7122e9e760de37f9c9cddc53e07bfd46cf33bd5
rtoy <rtoy>
2010-02-27 17:15:16 +0000

    Get rid of some compiler warnings (from clisp) about *maxima-initlisp* and *maxima-initmac* being unknown variables.

--- src/macsys.lisp
+++ src/macsys.lisp
@@ -366,6 +366,11 @@
 (defvar *maxima-prolog* "")
 (defvar *maxima-epilog* "")
 
+;; These 2 are actually defined in init-cl.lisp.  This is to get rid
+;; of some compiler warnings (clisp).
+(defvar *maxima-initlisp*)
+(defvar *maxima-initmac*)
+
 (defvar *maxima-quiet* nil)
 
 (defun macsyma-top-level (&optional (input-stream *standard-input*) batch-flag)
