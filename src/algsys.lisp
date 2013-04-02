4d8a27dace4488b4958fac591612729323b99735
Rupert Swarbrick <rswarbrick@gmail.com>
2013-04-02 14:37:41 +0100

    Switch the ALGSYS fix to a more sensible one; fix lambert_w on GCL
    It occurred to me that ($float '((mexpt) 10 -6)) was absolutely
    ridiculous and (float 1/1000000) was a bit more sensible...
    
    With this patch, the test suite passes on GCL.

--- src/algsys.lisp
+++ src/algsys.lisp
@@ -79,7 +79,7 @@
         ;; GCL seems to read 1e-7 as zero, but only when compiling. Incantations
         ;; based on 1d-7, 1l-7 etc. don't seem to make any difference.
 	($ratepsilon #-gcl 1e-7
-	             #+gcl ($float '((mexpt) 10 -7)))
+	             #+gcl (float 1/10000000))
 	($keepfloat nil)
 	(varlist (reverse (cdr varxlist)))
 	(genvar nil) ($ratfac nil) ($breakup nil)
