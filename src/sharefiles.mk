f8aa55c229467447d76033a766b83d5cc17be627
Leo Butler <l_butler@users.sourceforge.net>
2013-11-07 10:35:01 -0500

    SMath Studio plugin. Provides a 1d-display mode that allows Maxima expressions to be read correctly by SMath. See feature request 109.

--- src/sharefiles.mk
+++ src/sharefiles.mk
@@ -224,6 +224,7 @@ contrib/diffequations/tests/rtest_sym2.mac \
 contrib/diffequations/tests/rtest_sym.mac \
 contrib/diffequations/tests/testsuite.lisp \
 contrib/f90.lisp \
+contrib/fad/fad.lisp \
 contrib/floatproperties.lisp \
 contrib/format/autoload-m.macsyma \
 contrib/format/autoload-mp.macsyma \
@@ -453,6 +454,8 @@ contrib/sarag/topology_examples.mac \
 contrib/sarag/topology.mac \
 contrib/sarag/whatsnew.txt \
 contrib/simplifying.lisp \
+contrib/smath/smath.lisp \
+contrib/smath/smath.mac \
 contrib/state/a.cir \
 contrib/state/b.cir \
 contrib/state/c.cir \
