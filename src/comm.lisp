calyau/maxima

commit 9bf2c507c8875e26ea9ce9d206cbf97e0b5ae8f8

Bug #3484414. (false) precedes the display output if display2d is set to false. Fix by Robert Dodier.

src/comm.lisp
@@ -753,7 +753,9 @@
 	(setq ans (list '(mequal simp) (disp2 l) ans)))
     (if lablist (nconc lablist (cons (elabel ans) nil)))
     (setq tim (get-internal-run-time))
-    (displa (list '(mlable) (if lablist linelable) ans))
+    (let ((*display-labels-p* nil))
+      (declare (special *display-labels-p*))
+      (displa (list '(mlable) (if lablist linelable) ans)))
     (mterpri)
     (timeorg tim)))
 

commit cc35700ed008ffae04223160c1b1f0bd362ee014

Fix bug in ldisplay and ldisp: need to display %t label. (Undoing a bug introduced by commit 9bf2c507c8.)

src/comm.lisp
@@ -744,7 +744,7 @@
 	(setq ans (list '(mequal simp) (disp2 l) ans)))
     (if lablist (nconc lablist (cons (elabel ans) nil)))
     (setq tim (get-internal-run-time))
-    (let ((*display-labels-p* nil))
+    (let ((*display-labels-p* (not (null lablist))))
       (declare (special *display-labels-p*))
       (displa (list '(mlabel) (if lablist *linelabel*) ans)))
     (mterpri)