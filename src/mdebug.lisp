calyau/maxima

commit d8dfa05d1942a38d0cbc45fac8311ee4dd16e082

Localise *prompt-prefix* and *prompt-suffix* use
This has all been rationalised into a function called FORMAT-PROMPT. The
idea is that the next commit will add further ways in which a user can
customise what appears on the prompt and I want to make sure that it
works correctly.

This commit also pretty much rewrites AFORMAT to just punt to MFORMAT,
possibly wrapping the output in a string. As far as I can tell, that's
basically what it did already, except it worked by including the guts of
the function via the MFORMAT-LOOP macro. And it errored with destination
equal to 'T', which seemed less than helpful...

src/mdebug.lisp
@@ -405,10 +405,9 @@
       (catch *quit-tag*
 	(unwind-protect
 	     (do () (())
-	       (format *debug-io*
-		       "~a~&~@[(~a:~a) ~]~a"  *prompt-prefix* 
-		       (unless (stringp at) "dbm")
-		       (length *quit-tags*) *prompt-suffix*)
+	       (format-prompt *debug-io* "~&~@[(~a:~a) ~]"
+                              (unless (stringp at) "dbm")
+                              (length *quit-tags*))
 	       (finish-output *debug-io*)
 	       (setq val
 		     (catch 'macsyma-quit

src/cl-info.lisp
@@ -2,16 +2,13 @@
 
 (defvar *info-tables* (make-hash-table :test 'equal))
 
-(defvar *prompt-prefix* "")
-(defvar *prompt-suffix* "")
-
 (defun print-prompt (prompt-count)
-  (format t "~&~a~a~a"
-	  *prompt-prefix*
-	  (if (zerop prompt-count)
+  (fresh-line)
+  (maxima::format-prompt
+   t "~a"
+   (if (zerop prompt-count)
 	      (intl:gettext "Enter space-separated numbers, `all' or `none': ")
-	      (intl:gettext "Still waiting: "))
-	  *prompt-suffix*))
+	      (intl:gettext "Still waiting: "))))
 
 (defvar +select-by-keyword-alist+
   '((noop "") (all "a" "al" "all") (none "n" "no" "non" "none")))

src/init-cl.lisp
@@ -611,7 +611,7 @@ When one changes, the other does too."
   (setf *debugger-hook* #'maxima-lisp-debugger-repl)
   (loop
      (catch 'to-maxima-repl
-       (format t "~a~%~a> ~a" *prompt-prefix* (package-name *package*) *prompt-suffix*)
+       (format-prompt t "~%~A> " (package-name *package*))
        (finish-output)
        (format t "~{~&~S~}" (multiple-value-list (eval (read)))))))
 

src/macdes.lisp
@@ -117,9 +117,7 @@
 
 (defmspec $describe (x)
   (let ((topic ($sconcat (cadr x)))
-	(exact-p (or (null (caddr x)) (eq (caddr x) '$exact)))
-	(cl-info::*prompt-prefix* *prompt-prefix*)
-	(cl-info::*prompt-suffix* *prompt-suffix*))
+	(exact-p (or (null (caddr x)) (eq (caddr x) '$exact))))
     (if exact-p
 	(cl-info::info-exact topic)
 	(cl-info::info-inexact topic))))

src/macsys.lisp
@@ -22,32 +22,48 @@
 (defmvar $prompt '_
   "Prompt symbol of the demo function, playback, and the Maxima break loop.")
 
+;; A prefix and suffix that are wrapped around every prompt that Maxima
+;; emits. This is designed for use with text-based interfaces that drive Maxima
+;; through standard input and output and need to decorate prompts to make the
+;; output easier to parse. There are some more notes in
+;; doc/implementation/external-interface.txt.
 (defvar *prompt-prefix* "")
 (defvar *prompt-suffix* "")
+
 (defvar *general-display-prefix* "")
 
+(defun format-prompt (destination control-string &rest arguments)
+  "Like AFORMAT, but add the prefix and suffix configured for a prompt. This
+function deals correctly with the ~M control character, but only when
+DESTINATION is an actual stream (rather than nil for a string)."
+  (let ((*print-circle* nil))
+    (concatenate 'string
+                 *prompt-prefix*
+                 (apply 'aformat destination control-string arguments)
+                 *prompt-suffix*)))
+
+;;  "When time began" (or at least the start of version control history),
+;;  the following comment was made at this point:
+;;
+;;     instead of using this STRIPDOLLAR hackery, the
+;;     MREAD function should call MFORMAT to print the prompt,
+;;     and take a format string and format arguments.
+;;     Even easier and more general is for MREAD to take
+;;     a FUNARG as the prompt. -gjc
+;;
+;;  I guess we're still failing miserably, but unfortunately MFORMAT/AFORMAT
+;;  don't deal correctly with ~M plus a string output stream.
 (defun main-prompt ()
-  ;; instead off using this STRIPDOLLAR hackery, the
-  ;; MREAD function should call MFORMAT to print the prompt,
-  ;; and take a format string and format arguments.
-  ;; Even easier and more general is for MREAD to take
-  ;; a FUNARG as the prompt. -gjc
   (declare (special *display-labels-p*))
   (if *display-labels-p*
-      (let ((*print-circle* nil))
-	(format nil "~A(~A~D) ~A"
-		*prompt-prefix*
-		(print-invert-case (stripdollar $inchar))
-		$linenum
-		*prompt-suffix*))
-    ""))
+      (format-prompt nil "(~A~A) "
+                     (print-invert-case (stripdollar $inchar))
+                     $linenum)
+      ""))
 
 (defun break-prompt ()
-  (let ((*print-circle* nil))
-    (format nil "~A~A~A"
-	    *prompt-prefix*
-	    (print-invert-case (stripdollar $prompt))
-	    *prompt-suffix*)))
+  (format-prompt nil "~A"
+                 (print-invert-case (stripdollar $prompt))))
 
 (defun toplevel-macsyma-eval (x)
   ;; Catch rat-err's here.
@@ -287,23 +303,17 @@
   (or (eq flag 'noprint) (setq print? t))
   (cond ((not print?)
 	 (setq print? t)
-	 (princ *prompt-prefix*)
-	 (princ *prompt-suffix*))
+         (format-prompt t ""))
 	((null msg)
-	 (princ *prompt-prefix*)
-	 (princ *prompt-suffix*))
+         (format-prompt t ""))
 	((atom msg)
-	 (format t "~a~a~a" *prompt-prefix* msg *prompt-suffix*)
-	 (mterpri))
+         (format-prompt t "~A" msg)
+	 (terpri))
 	((eq flag t)
-	 (princ *prompt-prefix*)
-	 (mapc #'princ (cdr msg))
-	 (princ *prompt-suffix*)
+         (format-prompt t "~{~A~}" (cdr msg))
 	 (mterpri))
 	(t
-	 (princ *prompt-prefix*)
-	 (displa msg)
-	 (princ *prompt-suffix*)
+         (format-prompt t "~M" msg)
 	 (mterpri)))
   (let ((res (mread-noprompt *query-io* nil)))
     (princ *general-display-prefix*)
@@ -318,7 +328,7 @@
 	     (string-right-trim '(#\n)
 				(with-output-to-string (*standard-output*) (apply #'$print l)))
 	     "")))
-    (setf *mread-prompt* (format nil "~a~a~a" *prompt-prefix* *mread-prompt* *prompt-suffix*))
+    (setf *mread-prompt* (format-prompt nil "~A" *mread-prompt*))
     (third (mread *query-io*))))
 
 ;; FUNCTION BATCH APPARENTLY NEVER CALLED. OMIT FROM GETTEXT SWEEP AND DELETE IT EVENTUALLY

src/mformt.lisp
@@ -103,19 +103,16 @@
 ;; standard output not a string  
 ;;note: compile whole file, incremental compiling will not work.
 
-(defmfun aformat n
-  (unless (> n 1)
-      ;; make error message without new symbols.
-      ;; This error should not happen in compiled code because
-      ;; this check is done at compile time too.
-      (maxima-error "MFORMAT: expected two or more arguments."))
-  (let ((stream (arg 1))
-	(sstring (exploden (arg 2)))
-	(arg-index 2))
-    (if (null stream)
-	(with-output-to-string (stream) (mformat-loop (output-text)))
-	(mformat-loop (output-text)))))
-
+;; AFORMAT
+;;
+;; Basically the same as MFORMAT, which is a "souped-up" FORMAT implementation
+;; with support for the ~M control string. However, unlike MFORMAT, when
+;; DESTINATION is NIL, the function writes its result to a string.
+(defmfun aformat (destination control-string &rest arguments)
+  (if destination
+      (apply 'mformat destination control-string arguments)
+      (with-output-to-string (stream)
+        (apply 'mformat stream control-string arguments))))
 
 (defun output-text* (stream text displa-p pre-%-p post-%-p)
   (setq text (nreverse text))

commit 1db75dd57df153972b36d994ae647708da6f8676

Fix debug prompt in break-dbm-loop. The breakage occurred in commit d8dfa05d19 because the the control-string is a FORMAT control-string, not an MFORMAT one. The fix is to have FORMAT process the control-string and args, and pass the result plus the trivial control string "~a" to FORMAT-PROMPT.

src/mdebug.lisp
@@ -405,9 +405,10 @@
       (catch *quit-tag*
 	(unwind-protect
 	     (do () (())
-	       (format-prompt *debug-io* "~&~@[(~a:~a) ~]"
-                              (unless (stringp at) "dbm")
-                              (length *quit-tags*))
+	       (format-prompt *debug-io* "~a"
+			      (format nil "~&~@[(~a:~a) ~]"
+				      (unless (stringp at) "dbm")
+				      (length *quit-tags*)))
 	       (finish-output *debug-io*)
 	       (setq val
 		     (catch 'macsyma-quit