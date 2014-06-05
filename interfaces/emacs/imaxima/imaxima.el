873f253bae9d55be7241d690a48ea4baa9676885
Robert Dodier <robert_dodier@users.sourceforge.net>
2014-06-04 19:34:25 -0700

    Commit patch from David Ronis to put package and command stuff into LaTeX boilerplate. Fixes bug # 2741: imaxima preamble broken for display in emacs

--- interfaces/emacs/imaxima/imaxima.el
+++ interfaces/emacs/imaxima/imaxima.el
@@ -609,6 +609,7 @@ This command does not work in XEmacs."
     (insert "\\documentclass[leqno]{article}
 \\usepackage{verbatim}
 \\usepackage[cmbase]{flexisym}
+\\usepackage{color}
 \\usepackage{breqn}
 \\setkeys{breqn}{compact}
 
@@ -860,6 +861,20 @@ temporary files.  Use linearized form if LINEAR is non-nil."
      ;;"\\batchmode\n"
      (format "\\documentclass[%dpt,leqno]{article}\n" imaxima-pt-size)
      "\n% mylatex\n"
+     "\\usepackage{color}\n"
+     "\\usepackage{exscale}\n"
+     "\\usepackage[cmbase]{flexisym}\n"
+     "\\usepackage{breqn}\n"
+     "\\setkeys{breqn}{compact}\n"
+     "\\setlength{\\textheight}{200cm}\n"
+     "\\newcommand{\\boxed}[1]{\\fbox{\\m@th$\\displaystyle#1$}}\n"
+      "\\newcommand{\\ifrac}[2]{\\frac{#1}{#2}}\n"
+      "\\newcommand{\\ifracd}[2]{\\frac{#1}{#2}}\n"
+      "\\newcommand{\\ifracn}[2]{\\frac{#1}{#2}}\n"
+      "\\newcommand{\\isubscript}[2]{{#1}_{#2}}\n"
+      "\\newcommand{\\iexpt}[2]{{#1}^{#2}}\n"
+      "\\newcommand{\\isqrt}[1]{\\sqrt{#1}}\n"
+ 
      (format "\\setlength{\\textwidth}{%dmm}\n"
 	     (round (/ (imaxima-get-window-width)
 		       imaxima-scale-factor)))
