4f0c0267cb7d1485c4a38012e4e79ca7fc814722
Jaime Villate <villate@fe.up.pt>
2014-12-03 13:08:17 +0000

    Fixes the name of the menubar that still had its old name (mb) in some places.

--- interfaces/xmaxima/Tkmaxima/scene.tcl
+++ interfaces/xmaxima/Tkmaxima/scene.tcl
@@ -35,7 +35,7 @@ proc PlayStep {frame} {
       }
     if {$iteration < $maxiter } {
         incr iteration
-        uplevel $frame.mb.play configure -image ::img::pause
+        uplevel $frame.menubar.play configure -image ::img::pause
         foreach anim_obj $animate {
             set name [lindex $anim_obj 0]
             set prop [lindex $anim_obj 1]
@@ -104,7 +104,7 @@ proc stopAnimation {frame} {
     linkLocal $frame iteration playing
     after cancel PlayStep $frame
     set playing 0
-    uplevel $frame.mb.play configure -image ::img::play
+    uplevel $frame.menubar.play configure -image ::img::play
 }
 proc startAnimation {frame} {
     linkLocal $frame iteration prev_iteration playing
@@ -113,7 +113,7 @@ proc startAnimation {frame} {
     set iteration -1
     set prev_iteration 0
     uplevel PlayStep $frame
-    uplevel $frame.mb.play configure -image ::img::play
+    uplevel $frame.menubar.play configure -image ::img::play
 }
 proc endAnimation {frame} {
     linkLocal $frame maxiter iteration prev_iteration
