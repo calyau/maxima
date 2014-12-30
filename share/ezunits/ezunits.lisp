cb39816b86f3601ee0d5af56c097c41c5d01378d
Rupert Swarbrick <rswarbrick@gmail.com>
2014-12-30 11:57:25 +0000

    Add a hook mechanism to PrMathML.lisp
    This means that other code can register a hook to be called when the
    MathML code is loaded (this is basically the same as Emacs's
    eval-after-load, but without the syntactic sugar)
    
    Using the hook, we can then get rid of the undefined function calls when
    loading ezunits (which warn on at least SBCL). This also cuts the
    apparent sort-of-dependency of ezunits on something in contrib.

--- share/ezunits/ezunits.lisp
+++ share/ezunits/ezunits.lisp
@@ -42,8 +42,34 @@
 
 (defun $evens (a) (cons '(mlist) (odds (cdr a) 0)))
 
-(defun mathml-presentation-ezunits (mexpress)
-  (mPr_engine `((mtimes) ,(second mexpress) ,(third mexpress)) 'mparen 'mparen))
+;; MathML presentation code integration.
+;;
+;; The MATHML-PRESENTATION-EZUNITS function is used to display an ezunits
+;; expression. It should only be called after the MathML presentation library
+;; has been loaded (and, as such, MPR_ENGINE will be fbound)
 
-(when (fboundp 'setup)
-  (setup '($\` (mPrprocess mathml-presentation-ezunits))))
+(defun mathml-presentation-ezunits (mexpress)
+  (funcall (symbol-function 'mPr_engine)
+           `((mtimes) ,(second mexpress) ,(third mexpress)) 'mparen 'mparen))
+
+;; The ezunits-setup-mathml thunk gets installed into *MATHML-PRESENTATION-HOOK*
+;; and is an idempotent setup function that registers the mathml output for
+;; ezunits expressions.
+
+(defun ezunits-setup-mathml ()
+  (funcall (symbol-function 'setup)
+           '($\` (mPrprocess mathml-presentation-ezunits))))
+
+;; This is a re-implementation of Emacs's WITH-EVAL-AFTER-LOAD. If and when we
+;; put a more general version of that into Maxima, use it here!
+;;
+;; Register a hook so we set up properly if the MathML code hasn't yet been
+;; loaded.
+(setf (symbol-value '*mathml-presentation-hook*)
+      (cons 'ezunits-setup-mathml
+            (remove 'ezunits-setup-mathml
+                    (when (boundp '*mathml-presentation-hook*)
+                      (symbol-value '*mathml-presentation-hook*)))))
+;; But make sure we do the right thing if it's been loaded already
+(when (fboundp 'mPr_engine)
+  (ezunits-setup-mathml))
