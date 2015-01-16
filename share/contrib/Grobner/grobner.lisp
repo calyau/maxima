a30b3843b4b7f0e0f5579a51698084a93ba5d824
Robert Dodier <robert_dodier@users.sourceforge.net>
2015-01-15 17:29:56 -0800

    In package grobner, cut out "Loading maxima-grobner" message, as it is not useful.

--- share/contrib/Grobner/grobner.lisp
+++ share/contrib/Grobner/grobner.lisp
@@ -24,12 +24,6 @@
 
 (macsyma-module cgb-maxima)
 
-(eval-when
-    #+gcl (load eval)
-    #-gcl (:load-toplevel :execute)
-    (format t "~&Loading maxima-grobner ~a ~a~%"
-	    "$Revision: 1.6 $" "$Date: 2009-06-02 07:49:49 $"))
-
 ;; Macros for making lists with iterators - an exammple of GENSYM
 ;; MAKELIST-1 makes a list with one iterator, while MAKELIST accepts an
 ;; arbitrary number of iterators
