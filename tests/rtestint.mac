calyau/maxima

commit b0776c24bf74d0cf909055600cd1f6594c303c2c

Fix #2776 Error when integrate sqrt
src/defint.lisp:
o poles-in-interval: test sign of denom.  This handles cases where we
  can't find the exact roots, but we know that they occur outside the
  interval of integration.
  example: integrate((1+exp(t))/sqrt(t+exp(t)), t, 0, 1);

tests/rtestint.mac:
o add integrate((1+exp(t))/sqrt(t+exp(t)), t, 0, 1);

tests/rtest15.lisp:
o kill assumption cos(x)>0 that interferes with
  integrate(1/(sin(x)^2+1),x,1,1+%pi);

tests/rtest16.mac:
o syntactic change in one result,
  can now correctly identity divergent integratl in one other result.

src/defint.lisp
@@ -3337,7 +3337,12 @@ in the interval of integration.")
 	 (roots (real-roots denom var))
 	 (ll-pole (limit-pole exp var ll '$plus))
 	 (ul-pole (limit-pole exp var ul '$minus)))
-    (cond ((or (eq roots '$failure)
+    (cond ((member ($csign denom) '($pos $neg $pz))
+	   ;; this clause handles cases where we can't find the exact roots,
+	   ;; but we know that they occur outside the interval of integration.
+	   ;;  example: integrate ((1+exp(t))/sqrt(t+exp(t)), t, 0, 1);
+	   '$no)
+	  ((or (eq roots '$failure)
 	       (null ll-pole)
 	       (null ul-pole))   '$unknown)
 	  ((and (eq roots '$no)

tests/rtestint.mac
@@ -1453,6 +1453,10 @@ integrate( erf(x+a)-erf(x-a), x, minf, inf);
 integrate(cos(3*x)/(5-4*cos(x)),x,0,2*%pi);
 %pi/12;
 
+/* #2776 Error when integrate sqrt */
+integrate ((1+exp(t))/sqrt(t+exp(t)), t, 0, 1);
+2*sqrt(%e+1)-2;
+
 /* SF # 2633: ev(integrate,numer) gives strange result */
 (foo : ev(integrate(sin(2*%pi*x),x,0,1),numer), 0);
 0;

tests/rtest15.mac
@@ -428,6 +428,8 @@ float(0.0b0);
 0;
 integrate(sin(x)/cos(x)^2,x,0,%pi/3);
 1$
+kill(all);
+done;
 
 /*
  * Test compiled array access

tests/rtest16.mac
@@ -473,7 +473,7 @@ ratsimp(hgfred([-3/2,1],[-1/2], t));
 integrate((g32475^n*(g32475*n-n-1)/(g32475-1)^2+1/(g32475-1)^2)/(1-g32475)
  -(g32475^(2*n+1)*(g32475*n-n-1)/(g32475-1)^2+g32475^(n+1)/(g32475-1)^2)
   /(1-g32475),g32475,0,1);
-n^2/2+n;
+(n+1)^2/2-1/2;
 
 kill(all);
 done;
@@ -1418,15 +1418,15 @@ block([V:0.75, a:24, b:68, e],
  defint(f(x)=g(x),x,0,1);
  'integrate(f(x),x,0,1)='integrate(g(x),x,0,1);
  
-/* Bug report ID: 2796194 - error doing a Fourier transform
- * The following integral is divergent, but gives a noun form.
- * The Lisp error is no longer present.
- */
+/* Bug report ID: 2796194 - error doing a Fourier transform */
 (assume(equal(x,0)),done);
 done;
 
-integrate(%pi*exp(-2*%pi*t)*exp(2*%pi*x*t*%i),t,minf,inf);
-%pi*'integrate(%e^(2*%i*%pi*t*x-2*%pi*t),t,minf,inf);
+errcatch(integrate(%pi*exp(-2*%pi*t)*exp(2*%pi*x*t*%i),t,minf,inf));
+[];
+
+error;
+["defint: integral is divergent."];
 
 (forget(equal(x,0)),done);
 done;

commit d0a8769f5d1ea9a0ce6b2f05ec162d98b48fd91d

Fix typo: $pz can be zero
Fixes #2919 Definite integration broken: integrate(1/(x^2), x, -inf, inf) gives zero

src/defint.lisp:
o poles-in-interval: fix typo introduced in commit
  [b0776c24bf74d0cf909055600cd1f6594c303c2c]

tests/rtestint.mac:
o add integrate(1/(x^2), x, -inf, inf)
o add integrate(1/(x+1), x,0,inf)

src/defint.lisp
@@ -3339,7 +3339,7 @@ in the interval of integration.")
 	       (null ll-pole)
 	       (null ul-pole))   '$unknown)
 	  ((and (or (eq roots '$no)
-		    (member ($csign denom) '($pos $neg $pz)))
+		    (member ($csign denom) '($pos $neg $pn)))
 		    ;; this clause handles cases where we can't find the exact roots,
 		    ;; but we know that they occur outside the interval of integration.
 		    ;;  example: integrate ((1+exp(t))/sqrt(t+exp(t)), t, 0, 1);

tests/rtestint.mac
@@ -1469,6 +1469,22 @@ error;
 ["defint: integral is divergent."];
 
 
+/* #2903 Wrong integration over 1/(x+1) with bounds 0 to inf, since version 5.34.0 */
+errcatch(integrate(1/(x+1), x,0,inf));
+[];
+
+error;
+["defint: integral is divergent."];
+
+
+/* #2919 Definite integration broken: integrate(1/(x^2), x, -inf, inf) gives zero  */
+errcatch(integrate(1/(x^2), x, -inf, inf));
+[];
+
+error;
+["defint: integral is divergent."];
+
+
 /* SF # 2633: ev(integrate,numer) gives strange result */
 (foo : ev(integrate(sin(2*%pi*x),x,0,1),numer), 0);
 0;