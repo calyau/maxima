c63d1b844980a74a15c22e04cab539883a7c34c2
Wolfgang Dautermann <dauti@users.sourceforge.net>
2015-05-19 19:06:27 +0200

    Forgot to 'git add' to files in my last commit.

--- crosscompile-windows/xmaxima.bat
+++ crosscompile-windows/xmaxima.bat
@@ -4,5 +4,5 @@ rem startscript for xmaxima
 rem get directory, where xmaxima.bat is installed (including final "\")
 set xmpath=%~dp0
 
-start /b "%xmpath%wish86" %xmpath%xmaxima" %*
+"%xmpath%wish86.exe" "%xmpath%\xmaxima" %*
 
