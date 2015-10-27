5cc41da5d2dcb8cc40af552f73e57da61466cebd
Gunter Königsmann <gunter@peterpall.de>
2015-10-27 22:38:18 +0100

    Allow sbcl to use enough memory to compile Lapack. Unfortunately this patch doesn't affect windows since the 32-bit sbcl for windows doesn't allow to use enough memory.

--- share/draw/draw-index.lisp
+++ share/draw/draw-index.lisp
@@ -2,8 +2,9 @@
 (let (
 (deffn-defvr-pairs '(
 ; CONTENT: (<INDEX TOPIC> . (<FILENAME> <BYTE OFFSET> <LENGTH IN CHARACTERS> <NODE NAME>))
-("plot_vector_field" . ("drawutils.info" 768 766 "Vector fields"))
-("plot_vector_field3d" . ("drawutils.info" 1535 858 "Vector fields"))
+("plot_vector_field" . ("drawutils.info" 786 766 "Vector fields"))
+("plot_vector_field3d" . ("drawutils.info" 1553 858 "Vector fields"))
+("vennplot" . ("drawutils.info" 2913 314 "Venn diagrams"))
 ))
 (section-pairs '(
 ; CONTENT: (<NODE NAME> . (<FILENAME> <BYTE OFFSET> <LENGTH IN CHARACTERS>))
