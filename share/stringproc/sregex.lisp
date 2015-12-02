ccb60be981e61a3cffb393c682ac79e283193b8a
Volker van Nek <volkervannek@gmail.com>
2015-12-02 19:43:43 +0100

    revision of the interface to pregexp, new function: regex_compile

--- share/stringproc/sregex.lisp
+++ share/stringproc/sregex.lisp
@@ -1,92 +1,191 @@
-;;;;
-;;;;                                   ~*~  sregex  ~*~
-;;;;
-;;;;  Maxima interface to pregexp.lisp (portable regex parser, (c) Dorai Sitaram, see copyright in pregexp.lisp)
-;;;;
-;;;;  Version       : 1.0 (march 2008)
-;;;;  Copyright     : 2008 Volker van Nek
-;;;;  Licence       : GPL2
-;;;;
+#|
+                              ~*~  sregex  ~*~
+  
+    Maxima interface to pregexp.lisp (a portable regex parser by Dorai Sitaram)
+  
+    Copyright : 2008 - 2015 Volker van Nek
 
+--------------------------------------------------------------------------------
 
-;; regex_match_pos("needle", "hay needle stack");
-;;                                                [[5, 11]]
-;; regex_match("needle", "hay needle stack");
-;;                                                 [needle]
-;; regex_split("", "smithereens");
-;;                                    [s, m, i, t, h, e, r, e, e, n, s]
-;; regex_split("[,;]+", "split,pea;;;soup");
-;;                                            [split, pea, soup]
-;; regex_subst_first("ty", "te", "liberte");
-;;                                                 liberty
-;; regex_subst("ty", "te", "liberte egalite fraternite");
-;;                                        liberty egality fratyrnity
-;; string_to_regex("list?");
-;;                                                  list\?
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+    MA 02110-1301, USA.
 
-;; for a small tutorial and more examples see http://www.ccs.neu.edu/home/dorai/pregexp/pregexp.html
+--------------------------------------------------------------------------------
 
+   Functions: 
+   
+   regex_compile - returns a Lisp structure containing a compiled regex.
+   
+   regex_match_pos, regex_match, regex_split, regex_subst_first, regex_subst -
+      the regular expression used as an argument may be a string or 
+      compiled by regex_compile.
+   
+   string_to_regex - masking special characters.
+   
+   Examples:
+   
+   str : "his hay needle stack -- my hay needle stack -- her hay needle stack"$
+   
+   regex : regex_compile("ne{2}dle")$
+   
+   regex_match_pos(regex, str);
+                                               [[9, 15]]
+   regex_match_pos("ne{2}dle", str);
+                                               [[9, 15]]
+   regex_match_pos("ne{2}dle", str, 25, 44);
+                                               [[32, 38]]
+   regex_match("ne{2}dle", "hay needle stack");
+                                               ["needle"]
+   regex_match("ne{2}dle", "hay needle stack", 10);
+                                                 false
+   regex_split("[,;]+", "split,pea;;;soup");
+                                        ["split", "pea", "soup"]
+   regex_subst_first("ty", "t.", "liberte egalite fraternite");
+                                      "liberty egalite fraternite"
+   regex_subst("ty", "t.\\b", "liberte egalite fraternite");
+                                      "liberty egality fraternity"
+   string_to_regex(". :");
+                                                 "\. :"
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+--------------------------------------------------------------------------------
+
+   The pregexp manual by Dorai Sitaram is at
+   http://ds26gte.github.io/pregexp/index.html.
+
+--------------------------------------------------------------------------------
+
+   Like in stringproc.lisp we use 1-indexed position specifications. 
+   
+   When GCL is the underlying Lisp the positions are counted in octets at Lisp 
+   level and in Maxima characters at Maxima level. See remark in stringproc.lisp.
+   Non-us-ascii Maxima characters are not recognized in GCL, e.g. the regex "." 
+   doesn't match to an umlaut.
+
+|#
 
 (in-package :maxima)
 
 
-($load "pregexp")
-
-
-(defun $regex_match_pos (pattern str &optional (start 1) (end nil)) ;; 1-based indexing!
-  (if (or (not (stringp pattern)) (not (stringp str)))
-    (merror "regex_match_pos: first two arguments must be strings."))
-  (or (ignore-errors ;; suppresses Lisp error outputs with internal 0-based indexing 
-        (let ((pos-list 
-                (if end 
-                  (pregexp-match-positions pattern str (1- start) (1- end))
-                  (pregexp-match-positions pattern str (1- start))))
-              (pos-mlist nil))
-          (if pos-list 
-            (dolist (pos pos-list (cons '(mlist) (reverse pos-mlist)))
-              (setq pos-mlist (cons (list '(mlist) (1+ (car pos)) (1+ (cdr pos))) pos-mlist)))
-            (return-from $regex_match_pos nil))))
-      (merror "regex_match_pos: improper start or end index.")))
+#+ (and (or cmucl gcl) unix) 
+(declare-top (special *read-utf-8*))
 
 
 
-(defun $regex_match (pattern str &optional (start 1) (end nil)) ;; 1-based indexing!
-  (if (or (not (stringp pattern)) (not (stringp str)))
-    (merror "regex_match_pos: first two arguments must be strings."))
-  (or (ignore-errors ;; suppresses Lisp error outputs with internal 0-based indexing 
+(defstruct (compiled-regex (:print-function compiled-regex-print))
+  parse-tree )
+
+(defun compiled-regex-print (struct stream i) 
+  (declare (ignore struct i))
+  (format stream "Structure [COMPILED-REGEX]") ) ;; wxMaxima prints this
+                                                 ;; terminal should print this too
+
+(defun $regex_compile (regex)
+  (make-compiled-regex
+    :parse-tree (pregexp regex) ))
+
+
+(defun regex-check-and-maybe-coerce (name regex &rest args)
+  (cond
+    ((compiled-regex-p regex)
+      (setq regex (compiled-regex-parse-tree regex)) )
+    ((not (stringp regex))
+      (gf-merror (intl:gettext "`~m': first arg must be a compiled regex or a string.") name) ))
+  (unless (every #'stringp args)
+    (gf-merror (intl:gettext "Unsuitable arguments to `~m'.") name) )
+  regex )
+
+
+(defun regex-index-error (name)
+  (gf-merror (intl:gettext "`~m': improper start or end index.") name) )
+
+
+;; With GCL/utf-8 positions are counted in octets.
+;; We want them in numbers of characters to find the right position in a string.
+;; utf-8-pos-dec returns the decrement we need to adjust.
+;;   (string position = octet position - decrement)
+#+ (and (or cmucl gcl) unix)
+(defun regex-utf-8-pos-dec (ov off pos) ;; begin to count at a given offset
+  (do ((i off (1+ i))
+       (n 0)) 
+     ((= i pos) n)
+    (when (= (logand (aref ov i) 192.) 128.)
+      (incf n) )))
+
+
+(defun $regex_match_pos (regex str &optional (start 1) (end nil)) ;; 1-based indexing!
+  (setq regex (regex-check-and-maybe-coerce "regex_match_pos" regex str))
+  (decf start)
+  (when end (decf end))
+  (let (#+ (and (or cmucl gcl) unix) ov)
+    (or (ignore-errors 
+          #+ (and (or cmucl gcl) unix) 
+            (unless *read-utf-8*
+              (setq ov (intl::string-to-octets str :iso8859-1))
+              (let ((args (utf-8-fix-start-end ov (list nil start end))))
+                (setq start (cadr args)
+                      end (caddr args) )))
+          (let ((pos-list (pregexp-match-positions regex str start end))
+                (pos-mlist nil) )
+            (if pos-list 
+              (dolist (pos pos-list (cons '(mlist simp) (nreverse pos-mlist)))
+                #+ (and (or cmucl gcl) unix) 
+                  (unless *read-utf-8*
+                    (let ((dec (regex-utf-8-pos-dec ov 0 (car pos))))
+                      (decf (cdr pos) (+ dec (regex-utf-8-pos-dec ov (car pos) (cdr pos))))
+                      (decf (car pos) dec) ))
+                (push `((mlist simp) ,(1+ (car pos)) ,(1+ (cdr pos))) pos-mlist) )
+              (return-from $regex_match_pos nil) )))
+        (regex-index-error "regex_match_pos") )))
+
+
+(defun $regex_match (regex str &optional (start 1) (end nil))
+  (setq regex (regex-check-and-maybe-coerce "regex_match" regex str))
+  (or (ignore-errors 
+        #+ (and (or cmucl gcl) unix) 
+          (unless *read-utf-8*
+            (let* ((ov (intl::string-to-octets str :iso8859-1))
+                   (args (utf-8-fix-start-end ov (list nil start end))) )
+              (setq start (cadr args)
+                    end (caddr args) )))
         (let ((match 
-                (if end 
-                  (pregexp-match pattern str (1- start) (1- end))
-                  (pregexp-match pattern str (1- start)))))
+                (pregexp-match regex str (1- start) (if end (1- end) nil)) ))
           (if match 
-            (cons '(mlist) match)
-            (return-from $regex_match nil))))
-      (merror "regex_match: improper start or end index.")))
+            (cons '(mlist simp) match)
+            (return-from $regex_match nil) )))
+      (regex-index-error "regex_match") ))
 
 
-
-(defun $regex_split (pattern str)
-  (if (or (not (stringp pattern)) (not (stringp str)))
-    (merror "regex_split: first two arguments must be strings."))
-  (cons '(mlist) (pregexp-split pattern str)))
+(defun $regex_split (regex str)
+  (setq regex (regex-check-and-maybe-coerce "regex_split" regex str))
+  (cons '(mlist simp) (pregexp-split regex str)) )
 
 
-(defun $regex_subst_first (new old str)
-  (if (not (and (stringp new) (stringp old) (stringp str)))
-    (merror "regex_subst_first: arguments must be strings."))
-  (pregexp-replace old str new))
-
-
-(defun $regex_subst (new old str)
-  (if (not (and (stringp new) (stringp old) (stringp str)))
-    (merror "regex_subst: arguments must be strings."))
-  (pregexp-replace* old str new))
+(defun $regex_subst_first (replacement regex str)
+  (setq regex (regex-check-and-maybe-coerce "regex_subst_first" regex str replacement))
+  (pregexp-replace regex str replacement) )
+;;
+;; Argument order different to the order of pregexp-replace.
+;; Use order like in $ssubst or substitute: new, old, str.
+;;
+(defun $regex_subst (replacement regex str)
+  (setq regex (regex-check-and-maybe-coerce "regex_subst" regex str replacement))
+  (pregexp-replace* regex str replacement) )
 
 
 (defun $string_to_regex (str)
-  (if (not (stringp str))
-    (merror "string_to_regex: argument must be a string."))
-  (pregexp-quote str))
+  (unless (stringp str)
+    (gf-merror (intl:gettext "`string_to_regex': Argument must be a string.")) )
+  (pregexp-quote str) )
 
