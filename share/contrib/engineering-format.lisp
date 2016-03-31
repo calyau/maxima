b7ddd62cdc0e3e84b6dbeaf347506f0ff7687176
Gunter Königsmann <gunter@peterpall.de>
2016-03-31 18:43:23 +0200

    My commit 2b0f8c43231ba5e96eb0d6129f690aee69dd0c0e did change one file more than intended.
    Reverting the superfluous change.

--- share/contrib/engineering-format.lisp
+++ share/contrib/engineering-format.lisp
@@ -48,5 +48,3 @@
         (setq s1 (if *exploden-strip-float-zeros* (or (strip-float-zeros s) s) s))
         (funcall foo s1))
       (funcall foo x))))
-
-(putprop 'engineering_format_floats t 'evflag)
