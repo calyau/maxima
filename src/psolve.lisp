b433655348990d27befd7e32a9010b1e0f0daa1c
Robert Dodier <robert_dodier@users.sourceforge.net>
2016-05-03 12:47:51 -0700

    Avoid trouble with ratvars when solving cubics and quartics.
    This fixes SF bug #3145: "solve sometimes gives wrong solution when using ratvars"
    
    Commit changes to src/psolve.lisp by Richard Fateman. He writes:
    
    <quote>
    the formulas for irreducible cubic and quartic polynomials
    were making assumptions about the values of global variables
    (assuming that varlist did not change during the evaluation
    of their internal forms.  However, calls to the solve program
    on subproblems could sometimes change the varlist.
    
    I have changed the programs  solvecubic and solvequartic
    in the file psolve.lisp
    [...]
    
    I ran the testsuite -- no errors.  In general  the results should be
    equivalent but not necessarily in exactly the same form, since rational
    (CRE) form is not used as much in the new version.,
    </quote>
    
    This patch was prepared by running diff -w (i.e. ignore changes
    in whitespace) and applying the resulting diff, which substantially
    reduced the number of lines changed, making it easier to see what
    actually changed in the code.

--- src/psolve.lisp
+++ src/psolve.lisp
@@ -6,232 +6,172 @@
 ;;;     All rights reserved                                            ;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; ** (c) Copyright 1980 Massachusetts Institute of Technology **
+;; whole file revised to avoid conflict with CRE forms. 4/27/2016 Richard Fateman
 
 (in-package :maxima)
 
 (macsyma-module psolve)
 
 (declare-top (special mult *roots *failures $solvefactors))
+(declare-top (special expsumsplit $dispflag checkfactors *g
+		      $algebraic equations ;List of E-labels
+		      *power *varb *flg $derivsubst
+		      $%emode genvar genpairs varlist broken-not-freeof
+		      mult    ;Some crock which tracks multiplicities.
+		      *roots ;alternating list of solutions and multiplicities
+		      *failures	;alternating list of equations and multiplicities
+		      *myvar $listconstvars
+		      *has*var *var $dontfactor
+		      $keepfloat $ratfac
+		      xm* xn* mul*))
 
 (defmvar flag4 nil)
 
 (defmfun solvecubic (x) 
   (prog (s1 a0 a1 a2 discr lcoef adiv3 omega^2 pdiv3 qdiv-2
 	 omega y1 u y2) 
+    
      (setq x (cdr x))
      (setq lcoef (cadr x))
      (setq adiv3
-	   (list '(mtimes)
+	 (mul
 		 '((rat) -1 3)
 		 (rdis (setq a2 (ratreduce (ptterm x 2)
 					   lcoef)))))
      (setq a1 (ratreduce (ptterm x 1) lcoef))
      (setq a0 (ratreduce (ptterm x 0) lcoef))
-     (setq s1 '((mtimes)
-		((rat) 1 2)
-		$%i
-		((mexpt) 3 ((rat) 1 2))))
-     (setq omega (list '(mplus)
-		       '((rat) -1 2)
-		       s1) 
-	   omega^2 (list '(mplus)
-			 '((rat) -1 2)
-			 (list '(mtimes) -1 s1)))
+      
+    ;;      coefficients now a0,a1,a2,  and leading coef 1.
+      
+   (setf a2 (rdis a2) a1 (rdis a1) a0 (rdis a0))
+    
+    
+      
+   (setq s1 (mul' ((rat) 1 2) '$%i (power 3 '((rat) 1 2))))
+   (setq omega (add '((rat) -1 2)  s1) 
+	 omega^2 (add '((rat) -1 2) (mul -1 s1)))
      (setq pdiv3
-	   (meval* (rdis (ratplus (rattimes a1 '(1 . 3) t)
-				  (rattimes (ratexpt a2 2)
-					    '(-1 . 9)
-					    t)))))
+	 (add (mul a1 '( (rat) 1  3))
+	      (mul  (power	a2 2) '((rat) -1 9))))
      (and (not (equal pdiv3 0)) (go harder))
      (setq y1
-	   (simptimes
-	    (list
-	     '(mtimes)
+	 (mul
 	     '((rat) 1 3)
-	     (list '(mplus)
-		   (simpnrt (rdis (setq y2 (ratplus (ratexpt a2 3)
-						    (rattimes '(-27 . 1) a0 t))))
-			    3)
-		   (list '(mtimes) -1 (rdis a2))))
-	    1
-	    nil))
+	  (add
+	   (simpnrt  (setq y2 (add (power a2 3)
+				   (mul -27 a0)))
+		     3)			; cube root
+	   (mul -1 a2 ))))
+    
      (and flag4 (return (solve3 y1 mult)))
-     (setq y2 (simpnrt (rdis (rattimes  y2 '(1 . 27) t)) 3))
+   (setq y2 (simpnrt  (mul  y2 '((rat) 1  27)) 3))
      (return (mapc #'(lambda (j) (solve3 j mult))
 		   (list y1
-			 (list '(mplus)
-			       (list '(mtimes) omega y2)
-			       adiv3)
-			 (list '(mplus)
-			       (list '(mtimes) omega^2 y2)
-			       adiv3))))
+			(add (mul omega y2) adiv3)
+			(add (mul omega^2 y2) adiv3))))
      harder
      (setq qdiv-2
-	   (rdis (ratplus (rattimes (ratplus (rattimes a1 a2 t)
-					     (rattimes '(-3 . 1) a0 t))
-				    '(1 . 6)
-				    t)
-			  (rattimes (ratexpt a2 3)
-				    '(-1 . 27)
-				    t))))
+	 (add (mul (add (mul a1 a2) (mul -3  a0))
+		   '((rat) 1 6))
+	      (mul (power a2 3) '((rat) -1  27) )))
+    
      (cond ((equal qdiv-2 0)
 	    (setq u (simpnrt pdiv3 2))
 	    (setq y1 adiv3))
-	   (t (setq discr (simplus (list '(mplus)
-					 (list '(mexpt) pdiv3 3)
-					 (list '(mexpt) qdiv-2 2))
-				   1
-				   nil))
+	 (t (setq discr (add 
+			 (power pdiv3 3)
+			 (power qdiv-2 2)))
+				   
 	      (cond ((equal discr 0)
 		     (setq u (simpnrt qdiv-2 3)))
 		    (t (setq discr (simpnrt discr 2))
 		       (and (complicated discr)
 			    (setq discr (adispline discr)))
-		       (setq u (simpexpt (list '(mexpt)
-					       (list '(mplus)
+		     (setq u (power
+			      (add
 						     qdiv-2
 						     discr)
-					       '((rat) 1 3)) 1 nil))
+			      '((rat) 1 3)))
 		       (and (complicated u)
 			    (setq u (adispline u)))))))
      (if (equal u 0) (merror (intl:gettext "SOLVECUBIC: arithmetic overflow.")))
      (or y1
-	 (setq y1 (simplus (list '(mplus)
-				 adiv3
-				 u
-				 (list '(mtimes)
-				       -1
-				       pdiv3
-				       (list '(mexpt) u -1)))
-			   1
-			   nil)))
+       (setq y1 (add adiv3  u  (mul  -1 pdiv3 (power u -1)))))
      (return
        (cond (flag4 (solve3 y1 mult))
 	     (t (mapc 
 		 #'(lambda (j) (solve3 j mult))
 		 (list y1
-		       (list '(mplus)
-			     adiv3
-			     (list '(mtimes) omega u)
-			     (list '(mtimes)
-				   -1
-				   pdiv3
-				   omega^2
-				   (list '(mexpt) u -1)))
-		       (list '(mplus)
-			     adiv3
-			     (list '(mtimes) omega^2 u)
-			     (list '(mtimes)
-				   -1
-				   pdiv3
-				   omega
-				   (list '(mexpt) u -1))))))))))
+		    (add  adiv3 (mul omega u)  (mul -1 pdiv3 omega^2 (power u -1)))
+		    (add  adiv3 (mul omega^2 u)(mul -1 pdiv3 omega   (power u -1))))))))))
 
 (defmfun solvequartic (x) 
   (prog (a0 a1 a2 b1 b2 b3 b0 lcoef z1 r tr1 tr2 d d1 e sqb3) 
      (setq x (cdr x) lcoef (cadr x))
-     (setq b3 (ratreduce (ptterm x 3) lcoef))
-     (setq b2 (ratreduce (ptterm x 2) lcoef))
-     (setq b1 (ratreduce (ptterm x 1) lcoef))
-     (setq b0 (ratreduce (ptterm x 0) lcoef))
-     (setq a2 (ratminus b2))
-     (setq a1 (ratdif (rattimes b1 b3 t)
-		      (setq a0 (rattimes b0 '(4 . 1) t))))
+   (setq b3 (rdis(ratreduce (ptterm x 3) lcoef)))
+   (setq b2 (rdis(ratreduce (ptterm x 2) lcoef)))
+   (setq b1 (rdis(ratreduce (ptterm x 1) lcoef)))
+   (setq b0 (rdis(ratreduce (ptterm x 0) lcoef)))
+   (setq a2 (mul -1 b2))
+   (setq a1 (sub (mul b1 b3) (setq a0 (mul b0 4))))
      (setq a0
-	   (ratdif (ratdif (rattimes b2 a0 t)
-			   (rattimes (setq sqb3
-					   (ratexpt b3 2))
-				     b0
-				     t))
-		   (ratexpt b1 2.)))
-     (setq tr2
-	   (simplify (rdis
-		      (rattimes
-		       '(1 . 4)
-		       (ratdif (ratdif (rattimes b3
-						 (rattimes b2 '(4 . 1) t)
-						 t)
-				       (rattimes '(8 . 1) b1 t))
-			       (rattimes sqb3 b3 nil))
-		       t))))
+	 (sub (sub (mul b2 a0) (mul (setq sqb3(power b3 2)) b0 )) (power b1 2)))
+   (setq tr2   (mul'((rat) 1 4)
+		(sub (sub (mul b3  b2 4)
+			  (mul 8 b1))
+		     (mul sqb3 b3 )) ))
      (setq z1 (resolvent a2 a1 a0))
      (setq r
-	   (simplus (list '(mplus)
+	 (add
 			  z1
-			  (rdis (ratdif (rattimes sqb3
-						  '(1. . 4.)
-						  t)
+	  (sub (mul sqb3 '((rat) 1 4))
 					b2)))
-		    1
-		    nil))
      (setq r (simpnrt r 2))
      (and (equal r 0) (go l0))
      (and (complicated r) (setq r (adispline r)))
      (and (complicated tr2) (setq tr2 (adispline tr2)))
      (setq tr1
-	   (simplus (list '(mplus)
-			  (rdis (ratdif (rattimes sqb3 '(1 . 2) t)
-					b2))
-			  (list '(mtimes) -1 z1))
-		    1
-		    nil))
+	 (sub
+	  (sub (mul sqb3 '((rat) 1  2))
+	       b2)
+	   z1))
      (and (complicated tr1) (setq tr1 (adispline tr1)))
      (setq tr2 (div* tr2 r))
      (go lb1)
-     l0   (setq d1
-		(simpnrt (simplify (list '(mplus)
-					 (list '(mexpt) z1 2)
-					 (list '(mtimes)
-					       -4
-					       (rdis b0))))
-			 2))
-     (setq tr2 (simplify (list '(mtimes) 2. d1)))
+   l0   (setq d1 (simpnrt (add (power z1 2) (mul -4 b0)) 2))
+   (setq tr2 (mul 2 d1))
      (and (complicated tr2) (setq tr2 (adispline tr2)))
-     (setq tr1
-	   (simplify (rdis (ratdif (rattimes sqb3 '(3 . 4) t)
-				   (rattimes b2 '(2 . 1) t)))))
+   (setq tr1 (sub (mul sqb3 '((rat) 3 4))  (mul b2 2)))
      (and (complicated tr1) (setq tr1 (adispline tr1)))
   lb1
      (setq d (div (power (add tr1 tr2) '((rat simp) 1 2)) 2))
      (setq e (div (power (sub tr1 tr2) '((rat simp) 1 2)) 2))
      (and (complicated d) (setq d (adispline d)))
      (and (complicated e) (setq e (adispline e)))
-     (setq a2 (rdis (rattimes b3 '(-1 . 4) t)))
+   (setq a2 (mul b3 '((rat) -1 4)))
      (setq a1 (div* r 2))
+    
      (setq z1
-	   (list (list '(mplus) a2 a1 d)
-		 (list '(mplus)
-		       a2
-		       a1
-		       (list '(mtimes) -1 d))
-		 (list '(mplus)
-		       a2
-		       (list '(mtimes) -1 a1)
-		       e)
-		 (list '(mplus)
-		       a2
-		       (list '(mtimes) -1 a1)
-		       (list '(mtimes) -1 e))))
+     (list (add a2 a1 d)		;1
+	   (add a2 a1 (mul -1 d))	;2
+	   (add a2 (mul -1 a1) e)	;3
+	   (add	a2 (mul -1 a1) (mul -1 e)) ;4
+	       ))
      (return (mapc #'(lambda (j) (solve3 j mult)) z1))))
 
 ;;; SOLVES RESOLVENT CUBIC EQUATION
 ;;; GENERATED FROM QUARTIC
 
 (defun resolvent (a2 a1 a0) 
-  (prog (*roots flag4 *failures $solvefactors) ;undoes binding in
+  (prog (*roots flag4 *failures $solvefactors yy) ;undoes binding in
      (setq flag4 t $solvefactors t)	       ;algsys
-     (solve (simplus (list '(mplus)
-			   (list '(mexpt) 'yy 3)
-			   (list '(mtimes)
-				 (rdis a2)
-				 (list '(mexpt) 'yy 2))
-			   (list '(mtimes)
-				 (rdis a1)
-				 'yy)
-			   (rdis a0))
-		     1
-		     nil)
-	    'yy
+    (setf yy (gensym))
+     (solve (add
+	     (power yy 3)
+	     (mul a2   (power yy 2))
+	     (mul a1  yy)
+	     a0)
+	    yy
 	    1)
      (when (member 0 *roots :test #'equal) (return 0))
      (return (caddar (cdr (reverse *roots))))))
