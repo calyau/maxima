calyau/maxima

commit a158b1547118ac74da546ec1b0e16c0cc6b44a77

Document and slightly refactor the guts of DISTREP
This patch deletes two functions called CONDENSEY and CONDENSESUBLIST,
which were only used by DISTREP. Squinting a bit, you see that the
combination was just

  (defun condensesublist (lol)
    (mapcar (lambda (lst)
              (delete-duplicates (reverse lst) :test #'alike1))
            lol))

CONDENSESUBLIST was only called on the result of COMBINEY, which
contains code to make sure that its results contain no duplicates (as
compared by #'EQUAL, rather than #'ALIKE1). Since COMBINEY is also only
called from DISTREP, I've changed that to discard duplicates using
ALIKE1 and thus can get rid of CONDENSEY and CONDENSESUBLIST completely,
replacing them with just a call to REVERSE.

I don't really understand the reversal stage here, but things break
if I remove it, so I'll leave it in for now and hopefully things will
become more clear when I've read some more of the code...

src/algsys.lisp
@@ -329,12 +329,16 @@
 ;;; (COMBINEY LISTOFL)
 ;;;
 ;;; Combine "independent" lists in LISTOFL. If all the lists have empty pairwise
-;;; intersections, this returns all selections of items, one from each list.
+;;; intersections, this returns all selections of items, one from each
+;;; list. Destructively modifies LISTOFL.
 ;;;
 ;;; Selections are built up starting at the last list. When building, if there
 ;;; would be a repeated element because the list we're about to select from has
 ;;; nonempty intersection with an existing partial selections then elements from
 ;;; the current list aren't added to this selection.
+;;;
+;;; COMBINEY guarantees that no list in the result has two elements that are
+;;; ALIKE1 each other.
 ;;;
 ;;; This is used to enumerate combinations of solutions from multiple
 ;;; equations. Each entry in LISTOFL is a list of possible solutions for an
@@ -343,13 +347,13 @@
 ;;;
 ;;; (I don't know why the non-disjoint behaviour works like this. RJS 1/2015)
 (defun combiney (listofl)
-  (cond ((member nil listofl :test #'eq) nil)
-	(t (combiney1 (delete '(0) listofl :test #'equal)))))
+  (unless (member nil listofl)
+    (combiney1 (delete '(0) listofl :test #'equal))))
 
 (defun combiney1 (listofl)
   (cond ((null listofl) (list nil))
 	(t (mapcan #'(lambda (r)
-		       (if (intersection (car listofl) r :test #'equal)
+		       (if (intersection (car listofl) r :test #'alike1)
 			   (list r)
 			   (mapcar #'(lambda (q) (cons q r)) (car listofl))))
 		   (combiney1 (cdr listofl))))))
@@ -542,18 +546,21 @@
 (defmfun mycabs (x)
   (and (complexnump x) (cabs x)))
 
+;;; (DISTREP LOL)
+;;;
+;;; Take selections from LOL, a list of lists, using COMBINEY. When used by
+;;; ALGSYS, the elements of the lists are per-equation solutions for some system
+;;; of equations. COMBINEY combines the per-equation solutions into prospective
+;;; solutions for the entire system.
+;;;
+;;; These prospective solutions are then filtered with CONDENSESOLNL, which
+;;; discards special cases of more general solutions.
+;;;
+;;; (I don't understand why this reversal has to be here, but we get properly
+;;;  wrong solutions to some of the testsuite functions without it. Come back to
+;;;  this... RJS 1/2015)
 (defun distrep (lol)
-  (condensesolnl (condensesublist (combiney lol))))
-
-(defun condensey (l)
-  (let ((result nil))
-    (mapl #'(lambda (q)
-	      (or (memalike (car q) (cdr q)) (push (car q) result)))
-	  l)
-    result))
-
-(defun condensesublist (lol)
-  (mapcar #'condensey lol))
+  (condensesolnl (mapcar #'reverse (combiney lol))))
 
 (defun exclude (l1 l2)
   (cond ((null l2)

commit a5873ba00e901e22f8c51af5bb8964270fef235b

SF bugs 3210 and 3212.  Fix algsys regressions.
Revert part of commit a158b1547, which broke algsys.  I suspect there
is a deeper problem, as the logic of the original commit seems sound
at first glance, and works for many cases.

Change test in combiney1 back to #'equal from #'alike1.

I don't understand algsys internals, but reverting this fixed approx
40 regressions in the share/contrib_ode testsuite, (and didn't introduce
any new ones).

Two test cases from bug reports added to rtest8.mac.

src/algsys.lisp
@@ -419,10 +419,15 @@
   (unless (member nil listofl)
     (combiney1 (delete '(0) listofl :test #'equal))))
 
+;;; DB (2016-09-13) Commit a158b1547 introduced a regression (SF bug 3210)
+;;; It: - restructured combiney
+;;;     - used ":test #'alike1" in place of "test #'equal" in combiney1
+;;; Reverting the change to combiney1 restores previous behaviour.
+;;; I don't understand algsys internals and haven't analysed this further.
 (defun combiney1 (listofl)
   (cond ((null listofl) (list nil))
 	(t (mapcan #'(lambda (r)
-		       (if (intersection (car listofl) r :test #'alike1)
+		       (if (intersection (car listofl) r :test #'equal)
 			   (list r)
 			   (mapcar #'(lambda (q) (cons q r)) (car listofl))))
 		   (combiney1 (cdr listofl))))))

tests/rtest8.mac
@@ -151,6 +151,62 @@ solve(asin(cos(3*x))*(f(x)-1),x);
 ev(solve(5^f(x) = 125,f(x)),solveradcan:true);
 [f(x) = 3]$
 
+/* SF bug #3210 ALGSYS does not return (regression)
+ *   also #3212 ALGSYS throws error
+ *
+ * Note: %nrum is the counter for the %r variables introduced
+ *       by algsys.  Set it to 0 for reproducable results
+ */
+block([%rnum:0], algsys([
+ 4*y*(y^2-28),
+ 8*y*(y^2-28)*z,
+ -16*y*(y^2-23)*z,
+ 8*y*(y^2-10)*z,
+ 32*y*(z-y),
+ 32*y*(z+y),
+ 4*y*(y^2-28),
+ 8*y*(y^2-28)*z,
+ -16*y*(y^2-23)*z,
+ 8*y*(y^2-10)*z,
+ y*((4*y^2-112)*z^2-8*y^4+(185-4*x^2)*y^2),
+ -2*y*((4*y^2-92)*z^2-2*y^4+(21-4*x^2)*y^2),
+ y*((4*y^2-40)*z^2+(1-4*x^2)*y^2)],
+[x,y,z]));
+[[x=%r1,y=0,z=%r2]];
+
+block([%rnum:0], algsys([
+  -8*a*z,
+  -3*(y-1)*y*z,-3*z*(z+y-1)*(z+y),
+  2*(y-1)*y*z*(6*y*z-3*z-4*b*y^2-2*y^2+4*b*y+2*y),
+  -8*a*z,
+  -3*(y-1)*y*z,
+  -4*z*((w^2-2*v*w+v^2+2*b-2*a)*z+8*a*y-4*a),
+  -2*(y-1)*y*z*((6*y-3)*z+((-4*b)-2)*y^2+(4*b+2)*y),
+  4*z
+   *((2*w^2-4*v*w+2*v^2+2*b-1)*z^2
+    +(((-2*w^2)+4*v*w-2*v^2-8*b+8*a-2)*y+(2*w+2*v-2)*x-4*v*w+4*b-4*a+2)
+     *z-12*a*y^2+12*a*y-2*a),
+  -z*(((4*w^2-8*v*w+4*v^2+14)*y^2+(((-8*w)-8*v+8)*x+16*v*w-18)*y+4*x^2
+                                 -8*x+3)
+     *z^2
+     +(((-32*b)-16)*y^3+(48*b+24)*y^2+((-16*b)-8)*y)*z+(8*b-8*a+4)*y^4
+     +((-16*b)+16*a-8)*y^3+(8*b-8*a+4)*y^2),
+  -z*((4*w^2-8*v*w+4*v^2-1)*z^3+(((-16*w^2)+32*v*w-16*v^2-32*b)*y
+                                +(16*w+16*v-16)*x-32*v*w+16*b+8)
+                                *z^2
+                               +((4*w^2-8*v*w+4*v^2+48*b-48*a+20)*y^2
+                                +(((-8*w)-8*v+8)*x+16*v*w-48*b+48*a-24)
+                                 *y+4*x^2-8*x+8*b-8*a+4)
+                                *z+32*a*y^3-48*a*y^2+16*a*y),
+  -2*z
+    *(((4*w^2-8*v*w+4*v^2+2)*y+((-4*w)-4*v+4)*x+8*v*w-3)*z^3
+     +(((-4*w^2)+8*v*w-4*v^2-24*b-8)*y^2
+      +((8*w+8*v-8)*x-16*v*w+24*b+12)*y-4*x^2+8*x-4*b-2)
+      *z^2+((16*b-16*a+8)*y^3+((-24*b)+24*a-12)*y^2+(8*b-8*a+4)*y)*z
+     +4*a*y^4-8*a*y^3+4*a*y^2)],
+[v,w,x,y,z]));
+[[v=%r1,w=%r2,x=%r3,y=%r4,z=0]];
+
 (float_approx_equal_tolerance : 1e-12, 0);
 0$
 