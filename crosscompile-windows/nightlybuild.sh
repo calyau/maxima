13e5cdc3cef65e6e226f36597c2245c00bcfbdfa
Wolfgang Dautermann <dauti@users.sourceforge.net>
2017-02-14 17:04:33 +0100

    Crosscompiling: Build with wxmaxima-git works again, improved the nightlybuild-script

--- crosscompile-windows/nightlybuild.sh
+++ crosscompile-windows/nightlybuild.sh
@@ -6,24 +6,45 @@
 LANG=C
 export LANG
 
-CMAKE=/opt/cmake-3.4.2-Linux-x86_64/bin/cmake
+CMAKE=/opt/cmake-3.7.2-Linux-x86_64/bin/cmake
+BUILDLOG=buildlog-win32
 
 cd build
 rm -rf *
-WINEPREFIX=$(pwd)/wine
-export WINEPREFIX
-$CMAKE -DBUILD_CURRENT=YES .. >buildlog 2>&1
-xvfb-run -a -s "-screen 0 1024x768x24" make 2>&1 | tee -a buildlog
-make package >> buildlog
-echo >>buildlog
-echo >>buildlog
-echo "Build Information:">>buildlog
-lsb_release  -d >>buildlog
-echo -n "Maxima GIT Version: ">>buildlog
-git describe >>buildlog
-echo -n "Wxmaxima GIT Version: ">>buildlog
-(cd wxMaxima-git-prefix/src/wxMaxima-git/ ; git describe) >>buildlog
-echo -n "Build date and time (UTC): " >>buildlog
-date --utc >>buildlog
-
+echo "32 Bit Build Information:">$BUILDLOG
+$CMAKE -DBUILD_CURRENT=YES .. >>$BUILDLOG 2>&1
+make 2>&1 | tee -a $BUILDLOG
+make package >> $BUILDLOG
+echo >>$BUILDLOG
+echo >>$BUILDLOG
+echo "Build Information:">>$BUILDLOG
+lsb_release  -d >>$BUILDLOG
+echo -n "Maxima GIT Version: ">>$BUILDLOG
+git describe >>$BUILDLOG
+echo -n "Wxmaxima GIT Version: ">>$BUILDLOG
+(cd wxmaxima/wxmaxima-git-prefix/src/wxmaxima-git ; git describe) >>$BUILDLOG
+echo -n "Build date and time (UTC): " >>$BUILDLOG
+date --utc >>$BUILDLOG
+cp $BUILDLOG maxima-current-win32.exe /tmp
 cd ..
+
+BUILDLOG=buildlog-win64
+cd build
+rm -rf *
+echo "64 Bit Build Information:">$BUILDLOG
+$CMAKE -DBUILD_CURRENT=YES -DBUILD_64BIT=YES .. >>$BUILDLOG 2>&1
+make 2>&1 | tee -a $BUILDLOG
+make package >> $BUILDLOG
+echo >>$BUILDLOG
+echo >>$BUILDLOG
+echo "Build Information:">>$BUILDLOG
+lsb_release  -d >>$BUILDLOG
+echo -n "Maxima GIT Version: ">>$BUILDLOG
+git describe >>$BUILDLOG
+echo -n "Wxmaxima GIT Version: ">>$BUILDLOG
+(cd wxmaxima/wxmaxima-git-prefix/src/wxmaxima-git ; git describe) >>$BUILDLOG
+echo -n "Build date and time (UTC): " >>$BUILDLOG
+date --utc >>$BUILDLOG
+cp $BUILDLOG maxima-current-win64.exe /tmp
+cd ..
+
