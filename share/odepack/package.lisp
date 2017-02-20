a927d47b509b55a99fa7a4cb6e1979b43476298a
Raymond Toy <toy.raymond@gmail.com>
2017-02-20 11:24:14 -0800

    Remove the definition of the common blocks.
    This is now generated and saved in the file dls001.cmn.

--- share/odepack/package.lisp
+++ share/odepack/package.lisp
@@ -1,19 +1,3 @@
 (defpackage #:odepack
   (:use :cl)
   (:export #:dlsode))
-
-(in-package #:odepack)
-
-(defstruct (dls001
-             (:predicate is-dls001-p))
-  (part-0 (make-array 218 :element-type 'double-float)
-          :type (simple-array double-float (218)))
-  (part-1 (make-array 37 :element-type '(f2cl-lib:integer4))
-          :type (simple-array (f2cl-lib:integer4) (37))))
-
-
-(defparameter *dls001-common-block*
-  (let* ()
-    (declare (ignorable))
-    (make-dls001)))
-
