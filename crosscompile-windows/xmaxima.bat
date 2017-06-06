a67cf7230231d9267b4bf743b98dc710caf134b1
Wolfgang Dautermann <dauti@users.sourceforge.net>
2017-06-06 22:02:06 +0200

    maxima.bat/xmaxima.bat: Use "/" as a path separator.
    That works too and solve some issues (especially with xmaxima)

--- crosscompile-windows/xmaxima.bat
+++ crosscompile-windows/xmaxima.bat
@@ -3,9 +3,10 @@ rem startscript for xmaxima
 
 rem get directory, where xmaxima.bat is installed (including final "\")
 set xmpath=%~dp0
+rem change "\" to "/" in that path. Works too and causes less troubles.
+set "xmpath=%xmpath:\=/%"
 
+set "MAXIMA_PREFIX=%xmpath%/.."
 
-set MAXIMA_PREFIX=%xmpath%/..
-
-start "Xmaxima" /b "%xmpath%wish86.exe" "%xmpath%\xmaxima" %*
+start "Xmaxima" /b "%xmpath%wish86.exe" "%xmpath%xmaxima" %*
 
