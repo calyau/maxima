8439a4bb8469465176d79eca2b3e44c990a50906
Gunter Königsmann <gunter@peterpall.de>
2018-05-05 14:57:11 +0200

    Documentation what winkill and winkill_lib are there for.

--- interfaces/xmaxima/win32/winkill.c
+++ interfaces/xmaxima/win32/winkill.c
@@ -1,3 +1,28 @@
+//  Written by William Schelter.
+//  Additional work from Davind Billinghurst and Andrej Vopodivec.
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; if not, write to the Free Software
+//  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+//
+//  SPDX-License-Identifier: GPL-2.0+
+//
+//
+//  Winkill is a program that tries to find the shared memory location maxima
+//  offers on MS Windows in order to receive interrupt signals from a GUI.
+// 
+
 #include <signal.h>
 #include <stdio.h>
 #include <stdlib.h>
