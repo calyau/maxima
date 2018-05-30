126a3759a3253877fbf8b13ba449ee2fbf24ae4e
Kris Katterjohn <katterjohn@gmail.com>
2018-05-30 18:39:27 -0500

    Add support for multiple return values to the ERRSET macro
    The previous definition of the ERRSET macro (which is like the ERRSET
    special form in Maclisp) was only returning the primary return value
    of the evaluated form in the non-error case.
    
    Now a list of all return values is returned in the non-error case (but
    there is one ambiguous case which I describe in a new comment).
    
    The test suite runs fine.

--- src/generr.lisp
+++ src/generr.lisp
@@ -2,20 +2,33 @@
 
 (defvar errset nil)
 
-;;here is the  desired behavior of errset
-;;(let ((errset t)) (errset (+ 2 'a))) ;==> signals error
-;;(let ((errset nil)) (errset (+ 2 'a))) ;==> nil
-;;(let ((errset nil)) (errset (+ 2 3))) ;==> (5)
+;; Here is the desired behavior of errset:
+;;
+;; (let ((errset t)) (errset (+ 2 'a)))      ;==> signals error
+;; (let ((errset nil)) (errset (+ 2 'a)))    ;==> nil
+;; (let ((errset nil)) (errset (values)))    ;==> (nil)
+;; (let ((errset nil)) (errset (+ 2 3)))     ;==> (5)
+;; (let ((errset nil)) (errset (floor 4/3))) ;==> (1 1/3)
+;;
+;; Notice that there is one ambiguous case: a singleton list
+;; containing only nil could mean either the form really did
+;; return nil, or that the form returned no values at all.
+;; The original "single value" errset had this ambiguous case,
+;; and it was preserved explicitly in this "multiple value"
+;; errset for backward-compatibility and to avoid the ambiguous
+;; case where nil could be returned if an error occurred or if
+;; the form returned no values.
 
 #-ecldebug
 (defmacro errset (&rest l)
-  `(handler-case (list ,(car l))
+  `(handler-case (or (multiple-value-list ,(car l)) (list nil))
     (error (e) (when errset (error e)))))
 
 #+ecldebug
 (defmacro errset (&rest l)
-  `(handler-case (list ,(car l))
+  `(handler-case (or (multiple-value-list ,(car l)) (list nil))
     (error (e)
       (format *error-output* "~S~%~A~%" (type-of e) e)
       (when errset
 	(let ((*debugger-hook* nil)) (si::default-debugger e))))))
+
