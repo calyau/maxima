9df67591afb48ac774ae10477496ccfc977223cd
Gunter Königsmann <gunter@peterpall.de>
2018-06-29 06:41:37 +0200

    The expected testbench failures for ACL.
    Didn't add the current share testbench failures as "expected", though,
    as many of them look like low-hanging fruits.

--- tests/testsuite.lisp
+++ tests/testsuite.lisp
@@ -7,7 +7,8 @@
 
 (setf $testsuite_files
       `((mlist simp)
-        "rtest_rules"
+#-acl        "rtest_rules"
+#+acl   ((mlist) "rtest_rules" 11 13)
         "rtestnset" 
         ((mlist) "rtest1" 183 185 186)
         ((mlist) "rtest1a" 33)
@@ -22,9 +23,10 @@
         ((mlist) "rtest6" #+ecl 44 #+ecl 46) ;; 46 = ECL bug #437: mishandling float format
         "rtest6a" "rtest6b" "rtest7"
         "rtest9" 
-        "rtest9a"
+#-acl   "rtest9a"
+#+acl   ((mlist simp) "rtest9a" 24 27 30 31 35 36 47 48 51 52 55 56 59 60 63 64 67 68 71 72)
         ((mlist simp) "rtest10" 24 25)
-        ((mlist) "rtest11" #+(or gcl cmucl ccl64) 158 #+(or gcl cmucl ccl64) 174 #+gcl 175)
+        ((mlist) "rtest11" #+acl 136 #+acl 137 #+(or gcl cmucl ccl64 acl) 158 #+(or gcl cmucl ccl64 acl) 174 #+gcl 175)
         "rtest13" "rtest13s"
 #-ecl   "rtest14"
 #+ecl   ((mlist simp) "rtest14" 201 233 234 249 250 251 252 267 297 298 307 310 312 315 319)
@@ -32,7 +34,7 @@
 	;; ccl versions 1.11 and earlier fail test 50.  Mark it as a
 	;; known failure.  Presumably 1.12 will have this fixed.
     ;; Test 561 tickles bug in ECL if signed zero enabled; see ECL bug #329. Fixed post-16.1.3.
-        ((mlist simp) "rtest16" #+ccl 50 #+ecl 50 #+ecl 561)
+        ((mlist simp) "rtest16" #+ccl 50 #+ecl 50 #+acl 50 #+acl 241 #+ecl 561)
         "rtestode" "rtestode_zp"
         "rtest3"
 #-ecl	"rtest8"
@@ -68,12 +70,13 @@
 	;; i64.
 	;; Unfortunately many gcl versions have the same version number so we cannot
 	;; test for the buggy version in order to get full support for the lts version.
-	#-gcl   "rtest_gamma"	
+	#-(or gcl acl)   "rtest_gamma"	
 	#+gcl   ((mlist simp) "rtest_gamma" 307)
+	#+acl   ((mlist simp) "rtest_gamma" 48 198 663 745)
         "rtest_expintegral"
         "rtest_signum"
         "rtest_lambert_w"
-        ((mlist) "rtest_elliptic" 129 143)
+        ((mlist) "rtest_elliptic" #+acl 92 129 143)
         "rtest_integrate"
         "rtest_integrate_special"
         ((mlist simp) "rtest_sqrt" 89)
@@ -81,7 +84,8 @@
         ((mlist simp) "rtest_log")
         ((mlist simp) "rtest_power" 19 20 26 58 65)
         "rtestdefstruct"
-        "rtest_limit"
+#-acl   "rtest_limit"
+#+acl   ((mlist simp) "rtest_limit" 184)
         "rtest_powerseries"
         ((mlist) "rtest_laplace" 29 49 50 51 54 59 60 61 62 78 80)
         "rtest_plotoptions"
