calyau/maxima

commit 08e69d477c26bceb1466ec9e3ef1ba7b8fb2994c

Another place where we claimed errors to automatically be caused by maxima.

src/mload.lisp
@@ -349,7 +349,7 @@
 		    ((and (not correct) expected-error)
 		     (if (or show-all show-expected)
 			 (progn
-			   (format t (intl:gettext "~%This is a known error in Maxima. The correct result is:~%"))
+			   (format t (intl:gettext "~%This is a known error in Maxima or in the lisp is was compiled with. The correct result is:~%"))
 			   (displa next-result))))
 		    (t (format t (intl:gettext "~%This differed from the expected result:~%"))
 		       (push i all-differences)

commit 564233255e6f21238873be20ced94b12a3407b71

Fix typos introduced in commit 08e69d47
Change "lisp" to "Lisp" for consistency with a similar message
introduced in commit cf4e2fda.

Change "is" to "it" because it was the wrong word.

src/mload.lisp
@@ -349,7 +349,7 @@
 		    ((and (not correct) expected-error)
 		     (if (or show-all show-expected)
 			 (progn
-			   (format t (intl:gettext "~%This is a known error in Maxima or in the lisp is was compiled with. The correct result is:~%"))
+			   (format t (intl:gettext "~%This is a known error in Maxima or in the Lisp it was compiled with. The correct result is:~%"))
 			   (displa next-result))))
 		    (t (format t (intl:gettext "~%This differed from the expected result:~%"))
 		       (push i all-differences)