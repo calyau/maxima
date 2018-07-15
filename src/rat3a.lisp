calyau/maxima

commit 99d0c62d7fd36faf603cf9b68e4496f0323a7bd1

Rename SETQMODULUS to SET-MODULUS and document
The rename is because I spent a while pondering what "qmodulus" was
before I realised it's "setq modulus". The other change here is to
replace the MERROR call with a hard lisp error: looking at the code,
this isn't called when a user binds modulus at the repl, so a bogus
value here means a coding error in lisp code. As such, it should throw
a proper lisp error, I think.

src/rat3a.lisp
@@ -209,12 +209,18 @@
 (defun ctimes      (a b) (cmod (* a b)))
 (defun cdifference (a b) (cmod (- a b)))
 
-(defun setqmodulus (m)
-  (cond ((numberp m)
-	 (cond ((> m 0)
-		(setq modulus m))
-	       (t (merror (intl:gettext "assignment: modulus must be a positive number; found: ~M") m))))
-	(t (setq modulus nil))))
+;; SET-MODULUS
+;;
+;; Set the base in which the rational function package works. This does
+;; sanity-checking on the value chosen and is probably the way you should set
+;; the global value.
+;;
+;; Valid values for M are either a positive integer or NULL.
+(defun set-modulus (m)
+  (if (or (null m) (typep m '(integer 0)))
+      (setq modulus m)
+      (error "modulus must be a positive number or nil"))
+  (values))
 
 (defmfun pcoefadd (e c x)
   (cond ((pzerop c) x)

src/factor.lisp
@@ -291,7 +291,7 @@
 (defun z1 (poly fact1 fact2)
   (prog (res hsteps steps kterm a b c d *ab* m df1 df2 dlr step *sharpa *sharpb)
      (let ((modulus))
-       (setqmodulus *prime)
+       (set-modulus *prime)
        (setq *sharpb (fact20 fact1 fact2 limk)))
      (setq *sharpa (car *sharpb))
      (setq *sharpb (cadr *sharpb))
@@ -353,7 +353,7 @@
      (setq b (cadr reml))
      sharp   (cond ((> k limk) (return (list a b))))
      (setq pk modulus)
-     (setqmodulus (* modulus modulus))
+     (set-modulus (* modulus modulus))
      (setq f(pmod f1) g (pmod g1))
      (setq h (pquo (pmod (pdifference (pplus (ptimes a f) (ptimes b g))
 				      1))
@@ -537,7 +537,7 @@
 (defun zfact (u fl limk many*)
   (prog (fcs* prodl)
      (when many*
-       (setqmodulus plim)
+       (set-modulus plim)
        (setq dlp (reduce #'max (mapcar #'multideg (cdr (oddelm u))))))
      (when (= (length fl) 1) (return (list u)))
      (setq prodl (fsplit fl 'v))
@@ -770,7 +770,7 @@
 (defun trufac (v lp olfact many* modulus)
   (prog (ans olc lc af qnt factor lfunct)
      (setq lc 1 olc 1)
-     (setqmodulus modulus)
+     (set-modulus modulus)
      (setq lfunct (setq olfact (cons nil olfact)))
      test (cond
 	    ((equal v 1) (setq ans factor) (go out))
@@ -890,12 +890,12 @@
      (cond ((and (not (member l tuple :test #'equal))
 		 (not (> (+ (cadr f) (cadr funct)) d2))
 		 (not (member (setq l (orde l tuple)) ltemp :test #'equal)))
-	    (setqmodulus plim)
+	    (set-modulus plim)
 	    (setq af0 (setq af (ptimes(pmod f) (pmod funct))))
 	    (cond (llc (setq af (ptimes (pmod (lchk llc lcindex l)) af))))
 	    (cond (many* (setq af (dropterms af)))
 		  ((and algfac* (not (equal intbs* 1)))(setq af (intbasehk af))))
-	    (setqmodulus nil)
+	    (set-modulus nil)
 	    (cond ((setq qnt (testdivide v af))
 		   (cond (llc (setq af (oldcontent af))
 			      (setq v (ptimes (car af) qnt)af (cadr af))
@@ -1036,7 +1036,7 @@
 (defun fact5mod (u)
   (prog (lc poly)
      (setq poly (copy-list u))
-     (setqmodulus modulu*)
+     (set-modulus modulu*)
      (setq poly (pmod poly))
      (setq lc (caddr poly))
      (pmonicize (cdr poly))
@@ -1123,7 +1123,7 @@
      (setq b (pplus b (npctimes mpk b1)))
      (setq h nil b1 nil qlp nil)
      (go sharp)
-     on   (setqmodulus p)
+     on   (set-modulus p)
      (return (list f g))))
 
 

src/rat3c.lisp
@@ -512,7 +512,7 @@
 ;;**THE CHINESE REMAINDER ALGORITHM IS A SPECIAL CASE OF LAGRANGE INTERPOLATION
 
 (defun lagrange3 (u uk p qk)
-  (setqmodulus p)
+  (set-modulus p)
   (setq uk (pdifference uk (pmod u)))
   (cond ((pzerop uk) (setq modulus nil) u)
 	(t (setq uk (pctimes (crecip (cmod qk)) uk))

src/ratout.lisp
@@ -26,7 +26,7 @@
 ;;	(GCD, X/GCD, Y/GCD)
 
 (defun newgcd (x y modulus)
-  (setqmodulus modulus)
+  (set-modulus modulus)
   (let ((a (cond ((pcoefp x)
 		  (cond ((zerop x) y)
 			((pcoefp y) (cgcd x y))
@@ -139,7 +139,7 @@
 	 gbar		nubar		nu1bar		nu2bar
 	 gtilde		f1tilde		f2tilde		biggtilde
 	 degree		f1		f1f2)
-     (setqmodulus modulus)
+     (set-modulus modulus)
      (cond ((and (univar (cdr bigf1)) (univar (cdr bigf2)))
 	    (setq q (pgcdu bigf1 bigf2))
 	    (return (list q (pquotient bigf1 q) (pquotient bigf2 q)))))
@@ -252,7 +252,7 @@
      step6a
      (cond ((or (zerop (rem f1 p)) (zerop (rem f2 p)))
 	    (go step6)))
-     (setqmodulus p)
+     (set-modulus p)
      ;; Step 7
      (setq gtilde (pmod gbar))
      ;; Step 8
@@ -276,7 +276,7 @@
 	   ((vgreat e degree) (setq n 0) (setq e degree)))
      (setq n (1+ n))
      ;; Step 11
-     (setqmodulus nil)
+     (set-modulus nil)
      (cond ((equal n 1) (setq q p)
 	    (setq gstar biggtilde)
 	    (setq h1star h1tilde)
@@ -291,7 +291,7 @@
 			 (* gtilde (maxcoefficient h2star))))
 	       q)
 	    (go step6)))
-     (setqmodulus nil)
+     (set-modulus nil)
      (setq gstar (cadr (pcontent gstar)))
      step15
      (setq last-good-prime p)

src/result.lisp
@@ -152,12 +152,12 @@
        (setq p *alpha)
        (go step3)
        step2	(setq p (newprime p))
-       step3	(setqmodulus p)
+       step3	(set-modulus p)
        (setq a* (pmod a))
        (setq b* (pmod b))
        (cond ((or (reject a* m xr1) (reject b* n xr1)) (go step2)))
        (setq c* (cpres a* b* xr1 varl))
-       (setqmodulus nil)
+       (set-modulus nil)
        (setq c (lagrange3 c c* p q))
        (setq q (* p q))
        (cond ((> q f) (return c))

commit 520893dea7a5b622dbe684791c1d81106d319346

Bug fix: SET-MODULUS allowed setting modulus to 0
Having modulus set to 0 causes massive breakage.

SET-MODULUS is supposed to be a safe way to set modulus (with checks
to validate the new value), but there has been an off-by-one error
since it was introduced in commit 99d0c62d as a rewrite of
SETQMODULUS.

(It wasn't mentioned in the commit message for the aforementioned
commit, but it appears that SETQMODULUS was broken because it
allowed any positive number.  SET-MODULUS fixed that part to only
allow integers, but it was broken because of the incorrect lower
bound.)

After a cursory glance it doesn't appear that a user could get
modulus set to 0 (without using lisp directly), but I didn't bother
doing any in-depth analysis.

src/rat3a.lisp
@@ -210,7 +210,7 @@
 ;;
 ;; Valid values for M are either a positive integer or NULL.
 (defun set-modulus (m)
-  (if (or (null m) (typep m '(integer 0)))
+  (if (or (null m) (typep m '(integer 1)))
       (setq modulus m)
       (error "modulus must be a positive number or nil"))
   (values))