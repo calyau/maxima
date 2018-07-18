calyau/maxima

commit 3fb19813c1f5709e93d9a3731ea7ce644caa6ffd

Make Maxima comfortable with partially-evaluated Boolean expressions by merging code from share/contrib/boolsimp.lisp into src. (Only the stuff for Boolean expressions has been brought over; the stuff for partially-evaluated conditional expressions was not.)
The global flag prederror is still honored. When prederror=true,
Boolean expressions which evaluate (under "is", "if",
or one of the operators "and", "or", or "not") to something
other than true or false trigger an error.
The default value of prederror is now false (was true).

With these modifications, run_testsuite() (including new tests for
Boolean expressions) succeeds without unexpected errors.

Residual strangeness: "and", "or", and "not" cause evaluation of
their operands via MEVALP. So (foo or bar) is equivalent to
(?mevalp(foo) or ?mevalp(bar)). This is mostly OK but for operands
which are a=b or a#b, MEVALP always returns T or NIL so "and", "or",
and "not" turn anything with = or # into true or false. I'm inclined
to think that's a bug. (The structure of the previous code was
pretty much entirely preserved, which is the source of this bug;
there may be other strangeness present for the same reason.)

  * doc/info/Operators.texi:
    Update descriptions of "and", "or", and "not"
    Update descriptions of "equal" and "notequal"

  * doc/info/Program.texi:
    Update description of "if"

  * share/contrib/boolsimp/boolsimp.lisp:
    Cut out code which was merged into various src files

  * share/contrib/boolsimp/rtest_boolsimp.mac:
    Cut out tests which were moved to tests/rtest_boolean.mac

  * src/acall.lisp:
    Cut $MAYBE (redefined elsewhere)
    Cut special declaration for PATEVALLED
    Modify MEVALP_TR and MEVALP1_TR

  * src/asum.lisp:
    Modify NARY1 to better flatten Boolean expressions

  * src/compar.lisp:
    Change default value of $PREDERROR to NIL
    Cut out obsolete special PATEVALLED (now a lexical variable)
    Paste in lengthy comments about evaluation and simplification

src/acall.lisp
@@ -279,32 +279,24 @@
 (defmfun maybe-boole-check (form)
   (mevalp_tr form nil nil))
 
-;; The following entry point is for querying the database without
-;; the dubious side effects of using PREDERROR:FALSE.
-
-(defmspec $maybe (form)
-  (mevalp_tr (fexprcheck form) nil t))
-
-(declare-top (special patevalled))
-
 (defun mevalp_tr (pat error? meval?)
   (let (patevalled ans)
     (setq ans (mevalp1_tr pat error? meval?))
     (cond ((member ans '(t nil) :test #'eq) ans)
-	  (error? (pre-err patevalled))
-	  (t '$unknown))))
+	  (error?
+	   (pre-err patevalled))
+	  ('else '$unknown))))
 
 (defun mevalp1_tr (pat error? meval?)
   (cond ((and (not (atom pat)) (member (caar pat) '(mnot mand mor) :test #'eq))
 	 (cond ((eq 'mnot (caar pat)) (is-mnot_tr (cadr pat) error? meval?))
 	       ((eq 'mand (caar pat)) (is-mand_tr (cdr pat) error? meval?))
 	       (t (is-mor_tr (cdr pat) error? meval?))))
-	((atom (setq patevalled (if meval? (meval pat) pat)))
-	 patevalled)
-	((member (caar patevalled) '(mnot mand mor) :test #'eq)
-	 (mevalp1_tr patevalled error? meval?))
-	(t
-	 (mevalp2 (caar patevalled) (cadr patevalled) (caddr patevalled)))))
+	((atom (setq patevalled (if meval? (meval pat) pat))) patevalled)
+	((member (caar patevalled) '(mnot mand mor) :test #'eq) (mevalp1_tr patevalled
+							       error?
+							       meval?))
+	(t (mevalp2 patevalled (caar patevalled) (cadr patevalled) (caddr patevalled)))))
 
 (defun is-mnot_tr (pred error? meval?)
   (setq pred (mevalp_tr pred error? meval?))

doc/info/Operators.texi
@@ -899,7 +899,8 @@ the result is @code{false} and no further operands are evaluated.
 The global flag @code{prederror} governs the behavior of @code{and}
 when an evaluated operand cannot be determined to be @code{true} or @code{false}.
 @code{and} prints an error message when @code{prederror} is @code{true}.
-Otherwise, @code{and} returns @code{unknown}.
+Otherwise, operands which do not evaluate to @code{true} or @code{false} are accepted,
+and the result is a Boolean expression.
 
 @code{and} is not commutative:
 @code{a and b} might not be equal to @code{b and a} due to the treatment of indeterminate operands.
@@ -926,7 +927,8 @@ the result is @code{true} and no further operands are evaluated.
 The global flag @code{prederror} governs the behavior of @code{or}
 when an evaluated operand cannot be determined to be @code{true} or @code{false}.
 @code{or} prints an error message when @code{prederror} is @code{true}.
-Otherwise, @code{or} returns @code{unknown}.
+Otherwise, operands which do not evaluate to @code{true} or @code{false} are accepted,
+and the result is a Boolean expression.
 
 @code{or} is not commutative:
 @code{a or b} might not be equal to @code{b or a} due to the treatment of indeterminate operands.
@@ -947,7 +949,8 @@ its operand is a Boolean expression, and its result is a Boolean value.
 The global flag @code{prederror} governs the behavior of @code{not}
 when its operand cannot be determined to be @code{true} or @code{false}.
 @code{not} prints an error message when @code{prederror} is @code{true}.
-Otherwise, @code{not} returns @code{unknown}.
+Otherwise, operands which do not evaluate to @code{true} or @code{false} are accepted,
+and the result is a Boolean expression.
 
 @end deffn
 
@@ -1222,11 +1225,6 @@ namely @code{if}, @code{and}, @code{or}, and @code{not}.
 @c All variables are effectively assumed to be real-valued.
 
 The negation of @code{equal} is @code{notequal}.
-Note that because of the rules for evaluation of predicate expressions
-(in particular because @code{not @var{expr}} causes evaluation of @var{expr}),
-@code{not equal(@var{a}, @var{b})}
-is equivalent to @code{is(notequal(@var{a}, @var{b}))}
-instead of @code{notequal(@var{a}, @var{b})}.
 
 Examples:
 
@@ -1278,9 +1276,7 @@ Two expressions may be equivalent even if they are not syntactically equal (i.e.
 (%i7) ratsimp (x - y);
 (%o7)                         x - y
 (%i8) is (equal (x, y));
-Maxima was unable to evaluate the predicate:
-equal(x, y)
- -- an error.  Quitting.  To debug this try debugmode(true);
+(%o8)                        unknown
 (%i9) is (x = y);
 (%o9)                         false
 @end example
@@ -1318,16 +1314,14 @@ equal(x  + 2 x + 1, x  - 2 x - 1)
 Some operators evaluate @code{equal} and @code{notequal} to @code{true} or @code{false}.
 
 @c ===beg===
-@c if equal (a, b) then FOO else BAR;
+@c if equal (y, y - 1) then FOO else BAR;
 @c eq_1 : equal (x, x + 1);
 @c eq_2 : equal (y^2 + 2*y + 1, (y + 1)^2);
 @c [eq_1 and eq_2, eq_1 or eq_2, not eq_1];
 @c ===end===
 @example
-(%i1) if equal (a, b) then FOO else BAR;
-Maxima was unable to evaluate the predicate:
-equal(a, b)
- -- an error.  Quitting.  To debug this try debugmode(true);
+(%i4) if equal (y, y - 1) then FOO else BAR;
+(%o4)                          BAR
 (%i2) eq_1 : equal (x, x + 1);
 (%o2)                    equal(x, x + 1)
 (%i3) eq_2 : equal (y^2 + 2*y + 1, (y + 1)^2);
@@ -1421,12 +1415,6 @@ can use this information; for example:
 @deffn {Function} notequal (@var{a}, @var{b})
 Represents the negation of @code{equal(@var{a}, @var{b})}.
 
-Note that because of the rules for evaluation of predicate expressions
-(in particular because @code{not @var{expr}} causes evaluation of @var{expr}),
-@code{not equal(@var{a}, @var{b})}
-is equivalent to @code{is(notequal(@var{a}, @var{b}))}
-instead of @code{notequal(@var{a}, @var{b})}.
-
 Examples:
 
 @c ===beg===
@@ -1435,14 +1423,11 @@ Examples:
 @c notequal (a, b);
 @c not equal (a, b);
 @c maybe (notequal (a, b));
-@c maybe (not equal (a, b));
 @c assume (a > b);
 @c equal (a, b);
 @c maybe (equal (a, b));
 @c notequal (a, b);
-@c not equal (a, b);
 @c maybe (notequal (a, b));
-@c maybe (not equal (a, b));
 @c ===end===
 @example
 (%i1) equal (a, b);
@@ -1452,27 +1437,19 @@ Examples:
 (%i3) notequal (a, b);
 (%o3)                    notequal(a, b)
 (%i4) not equal (a, b);
-Maxima was unable to evaluate the predicate:
-equal(a, b)
- -- an error.  Quitting.  To debug this try debugmode(true);
+(%o4)                    notequal(a, b)
 (%i5) maybe (notequal (a, b));
 (%o5)                        unknown
-(%i6) maybe (not equal (a, b));
-(%o6)                        unknown
-(%i7) assume (a > b);
-(%o7)                        [a > b]
-(%i8) equal (a, b);
-(%o8)                      equal(a, b)
-(%i9) maybe (equal (a, b));
-(%o9)                         false
-(%i10) notequal (a, b);
-(%o10)                   notequal(a, b)
-(%i11) not equal (a, b);
-(%o11)                        true
-(%i12) maybe (notequal (a, b));
-(%o12)                        true
-(%i13) maybe (not equal (a, b));
-(%o13)                        true
+(%i6) assume (a > b);
+(%o6)                        [a > b]
+(%i7) equal (a, b);
+(%o7)                      equal(a, b)
+(%i8) maybe (equal (a, b));
+(%o8)                         false
+(%i9) notequal (a, b);
+(%o9)                    notequal(a, b)
+(%i10) maybe (notequal (a, b));
+(%o10)                        true
 @end example
 
 @end deffn

doc/info/Program.texi
@@ -369,9 +369,15 @@ The alternatives @var{expr_0}, ..., @var{expr_n} may be any Maxima expressions,
 including nested @code{if} expressions.
 The alternatives are neither simplified nor evaluated unless the corresponding condition is @code{true}.
 
-The conditions @var{cond_1}, ..., @var{cond_n} are expressions such that @code{is(@var{cond_k})} evaluates
-to @code{true} or @code{false};
-it is an error otherwise.
+The conditions @var{cond_1}, ..., @var{cond_n} are expressions which potentially or actually
+evaluate to @code{true} or @code{false}.
+When a condition does not actually evaluate to @code{true} or @code{false},
+the behavior of @code{if} is governed by the global flag @code{prederror}.
+When @code{prederror} is @code{true},
+it is an error if any evaluated condition does not evaluate to @code{true} or @code{false}.
+Otherwise, conditions which do not evaluate to @code{true} or @code{false} are accepted,
+and the result is a conditional expression.
+
 Among other elements, conditions may comprise relational and logical operators as follows.
 
 @c - SEEMS LIKE THIS TABLE WANTS TO BE IN A DISCUSSION OF PREDICATE FUNCTIONS; PRESENT LOCATION IS OK I GUESS

share/contrib/boolsimp/boolsimp.lisp
@@ -39,53 +39,6 @@
 ; is true or false. Therefore arguments which potentially have side effects
 ; (print, kill, save, quit, etc) may or may not actually have those side effects.
 ;
-; Simplification of boolean expressions:
-;
-; and and or are declared nary. The sole effect of this is to allow Maxima to
-; flatten nested expressions, e.g., a and (b and c) => a and b and c
-; (The nary declaration does not make and and or commutative, and and and or
-; are not otherwise declared commutative.)
-;
-; and: if any argument simplifies to false, return false
-;  otherwise omit arguments which simplify to true and simplify others
-;  if only one argument remains, return it
-;  if none remain, return true
-;
-; or: if any argument simplifies to true, return true
-;  otherwise omit arguments which simplify to false and simplify others
-;  if only one argument remains, return it
-;  if none remain, return false
-;
-; not: if argument simplifies to true / false, return false / true
-;  otherwise reverse sense of comparisons (if argument is a comparison)
-;  otherwise return not <simplified argument>
-;
-; Evaluation (MEVAL) of boolean expressions:
-; same as simplification except evaluating (MEVALP) arguments instead of simplifying
-; When prederror = true, complain if expression evaluates to something other than T / NIL
-; (otherwise return unevaluated boolean expression)
-;
-; Evaluation (MEVALP) of boolean expressions:
-; same as simplification except evaluating (MEVALP) arguments instead of simplifying
-; When prederror = true, complain if expression evaluates to something other than T / NIL
-; (otherwise return unevaluated boolean expression)
-;
-; Simplification of "is" expressions:
-; if argument simplifies to true/false, return true/false
-; otherwise return is (<simplified argument>)
-;
-; Evaluation of "is" expressions:
-; if argument evaluates to true/false, return true/false
-; otherwise return unknown if prederror = false, else trigger an error
-;
-; Simplification of "maybe" expressions:
-; if argument simplifies to true/false, return true/false
-; otherwise return maybe (<simplified expression>)
-;
-; Evaluation of "maybe" expressions:
-; if argument evaluates to true/false, return true/false
-; otherwise return unknown
-;
 ; Simplification of "if" expressions:
 ;
 ; Let the expression be if P[1] then E[1] elseif P[2] then E[2] ... elseif P[n] then E[n]
@@ -125,132 +78,10 @@
 ; We're not there yet.
 
 (remprop 'mcond 'translate)
-(remprop 'mand 'translate)
-(remprop 'mor 'translate)
-(remprop 'mnot 'translate)
 
 ; It's OK for MEVALATOMS to evaluate the arguments of MCOND.
 ; %MCOND already has this property.
 (defprop mcond t evok)
-
-; Change default value of $PREDERROR to NIL.
-; Binding $PREDERROR to T banishes unevaluated conditionals.
-(setq $prederror nil)
-
-(remprop '$is 'mfexpr*)
-(remprop '$maybe 'mfexpr*)
-
-(defprop $is simp-$is operators)
-(defprop %is simp-$is operators)
-(defprop $maybe simp-$is operators)
-(defprop %maybe simp-$is operators)
-
-; I'VE ASSUMED (NULL Z) => SIMPLIFIY ARGUMENTS
-; SAME WITH SIMPCHECK (SRC/SIMP.LISP)
-; SAME WITH TELLSIMP-GENERATED SIMPLIFICATION FUNCTIONS
-; SAME WITH SIMPLIFICATION OF %SIN
-; PRETTY SURE I'VE SEEN OTHER EXAMPLES AS WELL
-; Z SEEMS TO SIGNIFY "ARE THE ARGUMENTS SIMPLIFIED YET"
-
-(defun maybe-simplifya (x z) (if z x (simplifya x z)))
-
-(defun maybe-simplifya-protected (x z)
-  (let ((errcatch t) ($errormsg nil))
-    (declare (special errcatch $errormsg))
-    (ignore-errors (maybe-simplifya x z) x)))
-
-(defun simp-$is (x y z)
-  (declare (ignore y))
-  (let ((a (maybe-simplifya (cadr x) z)))
-    (if (or (eq a t) (eq a nil))
-      a
-      `((,(caar x) simp) ,a))))
-
-(defmfun $is (pat)
-  (multiple-value-bind (ans patevalled) (mevalp1 pat)
-    (cond
-      ((member ans '(t nil) :test #'eq) ans)
-      ; I'D RATHER HAVE ($PREDERROR ($THROW `(($PREDERROR) ,PATEVALLED))) HERE
-      ($prederror (pre-err patevalled))
-      (t '$unknown))))
-
-(defmfun $maybe (pat)
-  (multiple-value-bind (ans patevalled) (let (($prederror nil)) (mevalp1 pat))
-    (cond
-      ((member ans '(t nil) :test #'eq) ans)
-      (t '$unknown))))
-
-(defun mevalp1 (pat)
-  (let (patevalled ans)
-    (setq ans 
-      (cond ((and (not (atom pat)) (member (caar pat) '(mnot mand mor) :test #'eq))
-	   (cond ((eq 'mnot (caar pat)) (is-mnot (cadr pat)))
-	         ((eq 'mand (caar pat)) (is-mand (cdr pat)))
-	         (t (is-mor (cdr pat)))))
-	  ((atom (setq patevalled (meval pat))) patevalled)
-	  ((member (caar patevalled) '(mnot mand mor) :test #'eq) (mevalp1 patevalled))
-	  (t (mevalp2 patevalled (caar patevalled) (cadr patevalled) (caddr patevalled)))))
-    (values ans patevalled)))
-
-(defmfun mevalp2 (patevalled pred arg1 arg2)
-  (cond ((eq 'mequal pred) (like arg1 arg2))
-	((eq '$equal pred) (meqp arg1 arg2))
-	((eq 'mnotequal pred) (not (like arg1 arg2)))
-	((eq '$notequal pred) (mnqp arg1 arg2))
-	((eq 'mgreaterp pred) (mgrp arg1 arg2))
-	((eq 'mlessp pred) (mgrp arg2 arg1))
-	((eq 'mgeqp pred) (mgqp arg1 arg2))
-	((eq 'mleqp pred) (mgqp arg2 arg1))
-	(t (isp (munformat patevalled)))))
-
-(defmfun mevalp (pat)
-  (multiple-value-bind (ans patevalled) (mevalp1 pat)
-    (cond ((member ans '(#.(not ()) ()) :test #'eq)
-       ans)
-      ; I'D RATHER HAVE ($PREDERROR ($THROW `(($PREDERROR) ,PATEVALLED))) HERE
-      ($prederror (pre-err patevalled))
-      (t (or patevalled ans)))))
-
-(defmfun assume (pat)
-  (if (and (not (atom pat))
-	   (eq (caar pat) 'mnot)
-	   (eq (caaadr pat) '$equal))
-      (setq pat `(($notequal) ,@(cdadr pat))))
-  (let ((dummy (let ($assume_pos) (mevalp1 pat))))
-    (cond ((eq dummy t) '$redundant)
-	  ((null dummy) '$inconsistent)
-	  ((atom dummy) '$meaningless)
-	  (t (learn pat t)))))
-
-(defmspec mand (form) (setq form (cdr form))
-  (do ((l form (cdr l)) (x) (unevaluated))
-    ((null l)
-    (cond
-      ((= (length unevaluated) 0) t)
-      ((= (length unevaluated) 1) (car unevaluated))
-      (t (cons '(mand) (reverse unevaluated)))))
-  (setq x (mevalp (car l)))
-  (cond
-    ((null x) (return nil))
-    ((not (member x '(t nil) :test #'eq)) (push x unevaluated)))))
-
-(defmspec mor (form) (setq form (cdr form))
-  (do ((l form (cdr l)) (x) (unevaluated))
-  ((null l)
-    (cond
-      ((= (length unevaluated) 0) nil)
-      ((= (length unevaluated) 1) (car unevaluated))
-      (t (cons '(mor) (reverse unevaluated)))))
-  (setq x (mevalp (car l)))
-  (cond
-    ((eq x t) (return t))
-    ((not (member x '(t nil) :test #'eq)) (push x unevaluated)))))
-
-(defmspec mnot (form) (setq form (cdr form))
-  (let ((x (mevalp (car form))))
-    (cond
-      ((member x '(t nil) :test #'eq) (not x))
-      (t `((mnot) ,x)))))
 
 ; X is an expression of the form ((OP) B1 G1 B2 G2 B3 G3 ...)
 ; where OP is MCOND or %MCOND,
@@ -335,123 +166,6 @@
 (putprop 'mcond 'simp-mcond 'operators)
 (putprop '%mcond 'simp-mcond 'operators)
 
-; Redefine REQUIRE-BOOLEAN from nset.lisp --
-; current version assumes MEVALP returns only T, NIL, or $UNKNOWN,
-; which is no longer the case.
-
-(defun require-boolean (x) (cond ((or (not x) (eq x t)) x) (t nil)))
-
-; The presence of OPERS tells SIMPLIFYA to call OPER-APPLY,
-; which calls NARY1 to flatten nested "and" and "or" expressions
-; (due to $NARY property of MAND and MOR, declared elsewhere).
-
-(put 'mand t 'opers)
-(put 'mor t 'opers)
-
-; NARY EXPRESSIONS NOT FLATTENED COMPLETELY AT PRESENT
-; BUG IS DUE TO STRANGENESS OF SPECIAL VARIABLE OPERS-LIST
-; WHICH IS MAINTAINED BY OPER-APPLY AND SUBSIDIARIES INCLUDING NARY1
-; DO NOT ATTEMPT TO MODIFY OPER-APPLY -- CHANGE ONLY NARY1
-; EVENTUALLY MOVE THIS CODE INTO SRC/ASUM.LISP
-
-(defun nary1 (e z)
-  (do
-    ((l (cdr e) (cdr l)) (ans) (some-change))
-
-    ((null l)
-     (if some-change
-       (nary1 (cons (car e) (nreverse ans)) z)
-       (let ((w (get (caar e) 'operators)))
-         (if w (funcall w e 1 z) (simpargs e z)))))
-
-    (setq
-      ans (if (and (not (atom (car l))) (eq (caaar l) (caar e)))
-            (progn
-              (setq some-change t)
-              (nconc (reverse (cdar l)) ans))
-            (cons (car l) ans)))))
-
-(putprop 'mnot 'simp-mnot 'operators)
-(putprop 'mand 'simp-mand 'operators)
-(putprop 'mor 'simp-mor 'operators)
-
-(defun simp-mand (x y z)
-  (declare (ignore y))
-  (do ((l (cdr x) (cdr l)) (a) (simplified))
-    ((null l)
-    (cond
-      ((= (length simplified) 0) t)
-      ((= (length simplified) 1) (car simplified))
-      (t (cons '(mand simp) (reverse simplified)))))
-  (setq a (maybe-simplifya (car l) z))
-  (cond
-    ((null a) (return nil))
-    ((eq a '$unknown) (if (not (member '$unknown simplified :test #'eq)) (push a simplified)))
-    ((not (member a '(t nil) :test #'eq)) (push a simplified)))))
-
-(defun simp-mor (x y z)
-  (declare (ignore y))
-  (do ((l (cdr x) (cdr l)) (a) (simplified))
-    ((null l)
-    (cond
-      ((= (length simplified) 0) nil)
-      ((= (length simplified) 1) (car simplified))
-      (t (cons '(mor simp) (reverse simplified)))))
-  (setq a (maybe-simplifya (car l) z))
-  (cond
-    ((eq a t) (return t))
-    ((eq a '$unknown) (if (not (member '$unknown simplified :test #'eq)) (push a simplified)))
-    ((not (member a '(t nil) :test #'eq)) (push a simplified)))))
-
-; ALSO CUT STUFF ABOUT NOT EQUAL => NOTEQUAL AT TOP OF ASSUME
-
-(defun simp-mnot (x y z)
-  (declare (ignore y))
-  (let ((arg (maybe-simplifya (cadr x) z)))
-    (if (atom arg)
-      (cond
-        ((or (eq arg t) (eq arg '$true))
-         nil)
-        ((or (eq arg nil) (eq arg '$false))
-         t)
-        ((eq arg '$unknown)
-         '$unknown)
-        (t `((mnot simp) ,arg)))
-      (let ((arg-op (caar arg)) (arg-arg (cdr arg)))
-        (setq arg-arg (mapcar #'(lambda (a) (maybe-simplifya a z)) arg-arg))
-        (cond
-          ((eq arg-op 'mlessp)
-           `((mgeqp simp) ,@arg-arg))
-          ((eq arg-op 'mleqp)
-           `((mgreaterp simp) ,@arg-arg))
-          ((eq arg-op 'mequal)
-           `((mnotequal simp) ,@arg-arg))
-          ((eq arg-op '$equal)
-           `(($notequal simp) ,@arg-arg))
-          ((eq arg-op 'mnotequal)
-           `((mequal simp) ,@arg-arg))
-          ((eq arg-op '$notequal)
-           `(($equal simp) ,@arg-arg))
-          ((eq arg-op 'mgeqp)
-           `((mlessp simp) ,@arg-arg))
-          ((eq arg-op 'mgreaterp)
-           `((mleqp simp) ,@arg-arg))
-          ((eq arg-op 'mnot)
-           (car arg-arg))
-
-          ; Distribute negation over conjunction and disjunction;
-          ; analogous to '(- (a + b)) --> - a - b.
-
-          ((eq arg-op 'mand)
-           (let ((L (mapcar #'(lambda (e) `((mnot) ,e)) arg-arg)))
-             (simplifya `((mor) ,@L) nil)))
-
-          ((eq arg-op 'mor)
-           (let ((L (mapcar #'(lambda (e) `((mnot) ,e)) arg-arg)))
-             (simplifya `((mand) ,@L) nil)))
-
-          (t `((mnot simp) ,arg)))))))
-
 ; WTF IS THIS ??
 (let ((save-intext (symbol-function 'intext)))
   (defun intext (rel body)
@@ -509,79 +223,6 @@
 
  (t (merror "Improper arguments to function 'some'"))))
 |#
-
-; DON'T FORGET TO KILL OFF (DEFVAR PATEVALLED), IS-MNOT, IS-MAND, IS-MOR, AND TRANSLATION STUFF !!
-
-; HEY SPEAKING OF WHICH HERE IS THE TRANSLATION STUFF FROM ACALL.LISP
-
-; LOOKS LIKE SOME OF THIS STUFF NEED NOT BE REVISED
-; JUST COMMENT OUT IN CASE I CHANGE MY MIND
-
-#|
-(defmfun is-boole-check (form)
-  (cond ((null form) nil)
-	((eq form t) t)
-	('else
-	 ;; We check for T and NIL quickly, otherwise go for the database.
-	 (mevalp_tr form $prederror nil))))
-
-(defmfun maybe-boole-check (form)
-  (mevalp_tr form nil nil))
-
-;; The following entry point is for querying the database without
-;; the dubious side effects of using PREDERROR:FALSE.
-
-(defmspec $maybe (form) (mevalp_tr (fexprcheck form) nil t))
-
-(declare-top(special patevalled))
-|#
-
-(defun mevalp_tr (pat error? meval?)
-  (let (patevalled ans)
-    (setq ans (mevalp1_tr pat error? meval?))
-    (cond ((member ans '(t nil) :test #'eq) ans)
-	  (error?
-	   (pre-err patevalled))
-	  ('else '$unknown))))
-
-(defun mevalp1_tr (pat error? meval?)
-  (cond ((and (not (atom pat)) (member (caar pat) '(mnot mand mor) :test #'eq))
-	 (cond ((eq 'mnot (caar pat)) (is-mnot_tr (cadr pat) error? meval?))
-	       ((eq 'mand (caar pat)) (is-mand_tr (cdr pat) error? meval?))
-	       (t (is-mor_tr (cdr pat) error? meval?))))
-	((atom (setq patevalled (if meval? (meval pat) pat))) patevalled)
-	((member (caar patevalled) '(mnot mand mor) :test #'eq) (mevalp1_tr patevalled
-							       error?
-							       meval?))
-	(t (mevalp2 patevalled (caar patevalled) (cadr patevalled) (caddr patevalled)))))
-
-(defun is-mnot_tr (pred error? meval?)
-  (setq pred (mevalp_tr pred error? meval?))
-  (cond ((eq t pred) nil)
-	((not pred))
-	(t (pred-reverse pred))))
-
-(defun is-mand_tr (pl error? meval?)
-  (do ((dummy) (npl))
-      ((null pl) (cond ((null npl))
-		       ((null (cdr npl)) (car npl))
-		       (t (cons '(mand) (nreverse npl)))))
-    (setq dummy (mevalp_tr (car pl) error? meval?)
-	  pl (cdr pl))
-    (cond ((eq t dummy))
-	  ((null dummy) (return nil))
-	  (t (setq npl (cons dummy npl))))))
-
-(defun is-mor_tr (pl error? meval?)
-  (do ((dummy) (npl))
-      ((null pl) (cond ((null npl) nil)
-		       ((null (cdr npl)) (car npl))
-		       (t (cons '(mor) (nreverse npl)))))
-    (setq dummy (mevalp_tr (car pl) error? meval?)
-	  pl (cdr pl))
-    (cond ((eq t dummy) (return t))
-	  ((null dummy))
-	  (t (setq npl (cons dummy npl))))))
 
 ; REDEFINE PARSE-CONDITION (IN SRC/NPARSE.LISP) TO APPEND NIL INSTEAD OF $FALSE
 ; WHEN INPUT IS LIKE "IF FOO THEN BAR" (WITHOUT ELSE)

commit cc344abc6daaa5791a80e8a90a70381228350e98

Bug fix: MEVALP_TR error messages always said the predicate was false
If MEVALP_TR could not evaluate the predicate and it was asked to
signal an error, the error message was always

  Unable to evaluate predicate false

It looks like this has been broken since commit 3fb19813 (11 years
ago) when PATEVALLED was no longer declared special.  Prior to that
commit, MEVALP_TR used the value of PATEVALLED as set in MEVALP1_TR
for error messages.  A few weeks after that commit it was noticed
that PATEVALLED was not bound in MEVALP1_TR, so a lexical binding
was added there in commit 1eec31de.

This commit reverts 1eec31de and declares the binding and references
to PATEVALLED to be dynamic in MEVALP_TR and MEVALP1_TR.

The test suite and share test suite both run fine.

src/acall.lisp
@@ -282,6 +282,7 @@
 
 (defun mevalp_tr (pat error? meval?)
   (let (patevalled ans)
+    (declare (special patevalled))
     (setq ans (mevalp1_tr pat error? meval?))
     (cond ((member ans '(t nil) :test #'eq) ans)
 	  (error?
@@ -289,16 +290,16 @@
 	  ('else '$unknown))))
 
 (defun mevalp1_tr (pat error? meval?)
-  (let (patevalled)
-    (cond ((and (not (atom pat)) (member (caar pat) '(mnot mand mor) :test #'eq))
-	   (cond ((eq 'mnot (caar pat)) (is-mnot_tr (cadr pat) error? meval?))
-		 ((eq 'mand (caar pat)) (is-mand_tr (cdr pat) error? meval?))
-		 (t (is-mor_tr (cdr pat) error? meval?))))
-	  ((atom (setq patevalled (if meval? (meval pat) pat))) patevalled)
-	  ((member (caar patevalled) '(mnot mand mor) :test #'eq) (mevalp1_tr patevalled
-									      error?
-									      meval?))
-	  (t (mevalp2 patevalled (caar patevalled) (cadr patevalled) (caddr patevalled))))))
+  (declare (special patevalled))
+  (cond ((and (not (atom pat)) (member (caar pat) '(mnot mand mor) :test #'eq))
+	 (cond ((eq 'mnot (caar pat)) (is-mnot_tr (cadr pat) error? meval?))
+	       ((eq 'mand (caar pat)) (is-mand_tr (cdr pat) error? meval?))
+	       (t (is-mor_tr (cdr pat) error? meval?))))
+	((atom (setq patevalled (if meval? (meval pat) pat))) patevalled)
+	((member (caar patevalled) '(mnot mand mor) :test #'eq) (mevalp1_tr patevalled
+									    error?
+									    meval?))
+	(t (mevalp2 patevalled (caar patevalled) (cadr patevalled) (caddr patevalled)))))
 
 (defun is-mnot_tr (pred error? meval?)
   (setq pred (mevalp_tr pred error? meval?))