384248b292a7dca2d08e1d5bff230c489709c130
Gunter Königsmann <gunter@peterpall.de>
2018-07-18 09:11:43 +0200

    Seems like tweaking the gc made more test suites pass on launchpad => making the gc more aggressive.

--- src/init-cl.lisp
+++ src/init-cl.lisp
@@ -64,7 +64,7 @@ When one changes, the other does too."
     (t
       (merror (intl:gettext "assignment: must assign a string to ~:M; found: ~M") var value))))
 
-#+sbcl (setf (sb-ext:bytes-consed-between-gcs) (* 1 1024 1024))
+#+sbcl (setf (sb-ext:bytes-consed-between-gcs) (* 512 1024))
 
 (defun print-directories ()
   (format t "maxima-prefix=~a~%" *maxima-prefix*)
