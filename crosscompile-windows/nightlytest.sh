4ce7574b67571c64c6d7b9b95546cb239e4cdc64
Wolfgang Dautermann <dauti@users.sourceforge.net>
2018-11-14 23:28:20 +0100

    Add timing statistics to the nightly tests.

--- crosscompile-windows/nightlytest.sh
+++ crosscompile-windows/nightlytest.sh
@@ -39,21 +39,30 @@ export GCL_MEM_MULTIPLE=0.2
 ~/maxima-test/installroot/bin/maxima --batch-string="build_info();" >logfile-buildinfo.txt
 commands=$(
 for lisp in abcl clisp ecl sbcl gcl ccl64 cmucl acl ; do
-      echo "echo Running Maxima testsuite with $lisp ; ~/maxima-test/installroot/bin/maxima --lisp=$lisp --batch-string='run_testsuite();' >logfile-testsuite-$lisp.txt 2>&1"
-      echo "echo Running Maxima share testsuite with $lisp ; ~/maxima-test/installroot/bin/maxima --lisp=$lisp --batch-string='run_testsuite(share_tests=only);' >logfile-share-testsuite-$lisp.txt 2>&1"
+      echo "echo Running Maxima testsuite with $lisp ; /usr/bin/time --portability --output=logfile-timing-testsuite-$lisp.txt ~/maxima-test/installroot/bin/maxima --lisp=$lisp --batch-string='run_testsuite();' >logfile-testsuite-$lisp.txt 2>&1"
+      echo "echo Running Maxima share testsuite with $lisp ; /usr/bin/time --portability --output=logfile-timing-share-testsuite-$lisp.txt ~/maxima-test/installroot/bin/maxima --lisp=$lisp --batch-string='run_testsuite(share_tests=only);' >logfile-share-testsuite-$lisp.txt 2>&1"
 done
 )
 echo "$commands" | parallel --no-notice
 
 
-rm -f logfile-summary.txt logfile-share-summary.txt
+rm -f logfile-summary.txt logfile-share-summary.txt logfile-timings-summary.txt
 for lisp in clisp ecl sbcl gcl ccl64 cmucl acl abcl ; do
       echo "$lisp summary" >>logfile-summary.txt
       echo "$lisp summary" >>logfile-share-summary.txt
+      echo -e "$lisp summary" >>logfile-timings-summary.txt
+      echo -e "\nMain testsuite"  >>logfile-timings-summary.txt
+      cat logfile-timing-testsuite-$lisp.txt >>logfile-timings-summary.txt
+      echo -e "\nShare testsuite"  >>logfile-timings-summary.txt
+      cat logfile-timing-share-testsuite-$lisp.txt >>logfile-timings-summary.txt
       sed -n -e '/^Error summary\|^No unexpected errors/,$p' logfile-testsuite-$lisp.txt >>logfile-summary.txt
       sed -n -e '/^Error summary\|^No unexpected errors/,$p' logfile-share-testsuite-$lisp.txt >>logfile-share-summary.txt
       echo -e "\n\n" >>logfile-summary.txt
       echo -e "\n\n" >>logfile-share-summary.txt
+      echo -e "\n\n-----------------------------------\n" >>logfile-timings-summary.txt
 done
 
+# remove the single timing files before copying to the server
+# every information is in the logfile-timings-summary.txt file.
+rm logfile-timing-*
 scp -i ~/.ssh/maximakopierkey ~/maxima-test/logfile-*.txt maxima@ns1.dautermann.at:/var/www/wolfgang.dautermann.at/maxima/nightlybuild/
