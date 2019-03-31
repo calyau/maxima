9c7084c0a2f4fa3332fd67998dc53b024f98ab72
Gunter Königsmann <gunter@peterpall.de>
2019-03-31 15:20:48 +0200

    ECL/Testsuite: One resolved errors still fails for old ECL versions
    => Marked it as "known bad" again.

--- src/testsuite.lisp
+++ src/testsuite.lisp
@@ -33,8 +33,9 @@
                         (list* '(mlist simp) (list 78 80))))
 	;; 46 = ECL bug #437: mishandling float format
 	;; 45 sporadically fails in all tested ECL versions (15.3.7-16.1.3)
+	;; 43 fails in ECL up to version 15.3.7
         ((mlist simp) "rtest6"
-	 #+ecl ((mlist simp) 45 46))
+	 #+ecl ((mlist simp) 43 45 46))
         "rtest6a"
 	"rtest6b"
 	"rtest7"
