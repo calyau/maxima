calyau/maxima

commit a7e99d4f67d02e619b0008bca15a582fa08f1590

In CONTINUE, ensure that output label is unbound. Fixes bug reported to mailing list 2019-03-08: "batch overwrites last output line"

src/macsys.lisp
@@ -191,8 +191,7 @@ DESTINATION is an actual stream (rather than nil for a string)."
         (finish nil)
       (declare (ignorable area-before area-after))
       (catch 'return-from-debugger
-	(when (or (not (checklabel $inchar))
-		  (not (checklabel $outchar)))
+	(unless (checklabel $inchar)
 	  (incf $linenum))
 	(setq c-tag (makelabel $inchar))
 	(let ((*mread-prompt* (if batch-or-demo-flag nil (main-prompt)))
@@ -256,6 +255,8 @@ DESTINATION is an actual stream (rather than nil for a string)."
 			  (float (- etime-after etime-before))
 			  internal-time-units-per-second))
 	(incf accumulated-time time-used)
+	(unless (checklabel $outchar)
+	  (incf $linenum))
 	(setq d-tag (makelabel $outchar))
 	(unless $nolabels (setf (symbol-value d-tag) $%))
 	(setq $_ $__)

commit 71af208203429e3e9ebcfd78c1ddac3fb5607b09

Second attempt to fix bug in which batch clobbers the last output line. Revert commit a7e99d4, which caused problems of its own when linenum was reset via kill(all), kill(labels), or linenum: something. Instead fix the bug by incrementing linenum at the end of batch. I hope this really fixes the bug reported to mailing list 2019-03-08: "batch overwrites last output line"

src/macsys.lisp
@@ -192,7 +192,8 @@ DESTINATION is an actual stream (rather than nil for a string)."
         (finish nil)
       (declare (ignorable area-before area-after))
       (catch 'return-from-debugger
-	(unless (checklabel $inchar)
+	(when (or (not (checklabel $inchar))
+		  (not (checklabel $outchar)))
 	  (incf $linenum))
 	(setq c-tag (makelabel $inchar))
 	(let ((*mread-prompt* (if batch-or-demo-flag nil (main-prompt)))
@@ -256,8 +257,6 @@ DESTINATION is an actual stream (rather than nil for a string)."
 			  (float (- etime-after etime-before))
 			  internal-time-units-per-second))
 	(incf accumulated-time time-used)
-	(unless (checklabel $outchar)
-	  (incf $linenum))
 	(setq d-tag (makelabel $outchar))
 	(unless $nolabels (setf (symbol-value d-tag) $%))
 	(setq $_ $__)

src/mload.lisp
@@ -201,6 +201,7 @@
           (format nil "~A" in-stream))))
       (format t (intl:gettext "~%read and interpret ~A~%") in-stream-string-rep)
       (catch 'macsyma-quit (continue :stream in-stream :batch-or-demo-flag demo))
+      (incf $linenum)
       in-stream-string-rep)))
 
 ;; Return true if $float converts both a and b to floats and 