d2fe6e8e75a18095352a80178802dd5e9ca61c40
Leo Butler <l_butler@users.sourceforge.net>
2020-04-06 22:21:33 -0500

    Fix bug 3613, bug 3618
    Define new customization variables:
    
    imaxima-latex-buffer-name
    imaxima-latex-document-class
    imaxima-latex-use-packages (to make imaxima-latex-preamble obsolete eventually)
    imaxima-latex-document-dimensions
    imaxima-latex-macros
    imaxima-latex-macros-linear
    imaxima-latex-macros-format-file
    
    New functions:
    imaxima-latex-document-class
    imaxima-latex-set-textwidth
    imaxima-latex-set-pagecolor
    imaxima-latex-set-labelcolor
    imaxima-latex-format-label
    imaxima-latex-format-output
    
    The functions imaxima-latex, imaxima-tex-to-dvi, imaxima-dump-tex,
    imaxima-latex have been re-written, without a change in function, to
    use the various custom variables in place of hard-coded strings.
    
    The restructured code includes Jinsong Zhao's patch in
    imaxima-latex-use-packages, which loads flexisym before breqn.

--- interfaces/emacs/imaxima/imaxima.el
+++ interfaces/emacs/imaxima/imaxima.el
@@ -332,6 +332,93 @@ This hook is called after imaxima has started Maxima."
   :group 'imaxima
   :type '(string))
 
+(defcustom imaxima-latex-buffer-name
+  "*imaxima-latex*"
+  "Default name of buffer created by `imaxima-latex'."
+  :group 'imaxima
+  :type '(string))
+
+(defcustom imaxima-latex-document-class
+  '("\\documentclass[%dpt,leqno]{article}" imaxima-pt-size)
+  "Default documentclass used by `imaxima-latex'. It should be a
+valid argument to `format'."
+  :group 'imaxima
+  :type '(sexp))
+
+(defcustom imaxima-latex-use-packages
+  "
+\\usepackage{verbatim}
+\\usepackage{color}
+\\usepackage{exscale}
+\\usepackage{amsmath}
+\\usepackage[cmbase]{flexisym}
+\\usepackage{breqn}
+\\setkeys{breqn}{compact}
+"
+  "Default latex packages and configuration used by `imaxima-latex'."
+  :group 'imaxima
+  :type '(string))
+
+(defcustom imaxima-latex-document-dimensions
+  "
+\\setlength{\\textwidth}{180mm}
+\\setlength{\\oddsidemargin}{15mm}
+\\addtolength{\\oddsidemargin}{-1in}
+\\setlength{\\evensidemargin}{15mm}
+\\addtolength{\\evensidemargin}{-1in}
+"
+   "Default dimensions of document created by `imaxima-latex'."
+  :group 'imaxima
+  :type '(string))
+
+(defcustom imaxima-latex-macros
+  "
+\\newcommand{\\ifrac}[2]{\\frac{#1}{#2}}
+\\newcommand{\\ifracd}[2]{\\frac{#1}{#2}}
+\\newcommand{\\ifracn}[2]{\\frac{#1}{#2}}
+\\newcommand{\\isubscript}[2]{{#1}_{#2}}
+\\newcommand{\\iexpt}[2]{{#1}^{#2}}
+\\newcommand{\\isqrt}[1]{\\sqrt{#1}}
+"
+  "Default macros used by `imaxima-latex'."
+  :group 'imaxima
+  :type '(string))
+
+(defcustom imaxima-latex-macros-linear
+  (concat
+   ;; braces in both denominator and numerator
+   "\\renewcommand{\\ifrac}[2]{\\left(#1\\right)/\\left(#2\\right)}"
+   ;; only braces denominator
+   "\\renewcommand{\\ifracd}[2]{#1/\\left(#2\\right)}"
+   ;; only braces in numerator
+   "\\renewcommand{\\ifracn}[2]{\\left(#1\\right)/#2}"
+   "\\renewcommand{\\isubscript}[2]{\\mathrm{subscript}\\left(#1,#2\\right)}"
+   "\\renewcommand{\\iexpt}[2]{\\mathrm{expt}\\left(#1,#2\\right)}"
+   "\\renewcommand{\\isqrt}[1]{\\left(#1\\right)^{1/2}}\n")
+  "Default linear macros used by `imaxima-latex'."
+  :group 'imaxima
+  :type '(string))
+
+(defcustom imaxima-latex-macros-format-file
+  (concat
+   "
+\\setlength{\\textheight}{200cm}
+%% define \\boxed from amsmath.sty
+\\makeatletter
+\\providecommand\\boxed{}
+\\providecommand\\operatorname{}
+\\renewcommand{\\boxed}[1]{\\fbox{\\m@th$\\displaystyle#1$}}
+\\renewcommand{\\operatorname}[1]{%
+\\mathop{\\relax\\kern\\z@\\operator@font{#1}}}
+\\makeatother
+")
+  "Default macros used by `imaxima-dump-tex'."
+  :group 'imaxima
+  :type '(string))
+  
+
+  
+
 (defface imaxima-latex-error-face
   '((t (:foreground "Blue" :underline t)))
   "Face used for LaTeX errors."
@@ -591,56 +678,24 @@ box, width, and height."
 	(kill-buffer buff))
       (list (vector 0 0 x y) x y))))
 
+(defun imaxima-latex-document-class ()
+  (apply #'format (mapcar #'eval imaxima-latex-document-class)))
+
 (defun imaxima-latex ()
   "Convert Maxima buffer to LaTeX.
 This command does not work in XEmacs."
   (interactive)
   (let (pos2 label (pos (make-marker))
-	     (buf (generate-new-buffer "*imaxima-latex*"))
+	     (buf (generate-new-buffer imaxima-latex-buffer-name))
 	     (oldbuf (current-buffer)))
     (set-buffer buf)
-    (insert-buffer oldbuf)
-;;     ;; Remove copyright notice
-;;     (goto-char (point-min))
-;;     (search-forward "(%i1)" nil t 2)
-;;     (search-backward "(%i1)" nil t)
-;;     (delete-region (point-min) (point))
-;;     (goto-char (point-min))
-    (insert "\\documentclass[leqno]{article}
-\\usepackage{verbatim}
-\\usepackage{color}
-\\usepackage{breqn}
-\\setkeys{breqn}{compact}
-\\usepackage{flexisym}
-
-\\setlength{\\textwidth}{180mm}
-\\setlength{\\oddsidemargin}{15mm}
-\\addtolength{\\oddsidemargin}{-1in}
-\\setlength{\\evensidemargin}{15mm}
-\\addtolength{\\evensidemargin}{-1in}
-
-\\newcommand{\\ifrac}[2]{\\frac{#1}{#2}}
-\\newcommand{\\ifracd}[2]{\\frac{#1}{#2}}
-\\newcommand{\\ifracn}[2]{\\frac{#1}{#2}}
-\\newcommand{\\isubscript}[2]{{#1}_{#2}}
-\\newcommand{\\iexpt}[2]{{#1}^{#2}}
-\\newcommand{\\isqrt}[1]{\\sqrt{#1}}
-\\begin{document}\n")
-;;     (while (and (not (eobp))
-;; 		(setq pos (next-single-property-change (point) 'display)))
-;;       (goto-char pos)
-;;       (insert "\\end{verbatim}\n\n")
-;;       (setq pos (copy-marker (next-single-property-change (point) 'display)))
-;;       (remove-text-properties (point) pos '(display nil))
-;;       (setq pos2 (point))
-;;       (re-search-forward "(\\([^)]*\\))")
-;;       (setq label (match-string 1))
-;;       (delete-region pos2 (point))
-;;       (insert (format "\\begin{dmath}[number={%s}]\n" label))
-;;       (goto-char pos)
-;;       (insert "\\end{dmath}\n\n\\begin{verbatim}"))
-;;     (goto-char (point-max))
-;;     (insert "\n\\end{verbatim}\n\\end{document}")
+    (insert-buffer-substring oldbuf)
+    (insert (imaxima-latex-document-class) "\n"
+	    imaxima-latex-use-packages "\n"
+	    imaxima-latex-document-dimensions "\n"
+	    imaxima-latex-macros "\n"
+	    "\\begin{document}\n"
+	    )
     (while (not (eobp))
       (let* ((region-start (copy-marker (point)))
 	     (region-end (copy-marker (next-single-property-change (point) 'display nil (point-max))))
@@ -655,14 +710,14 @@ This command does not work in XEmacs."
 	      (setq label (match-string 1))
 	      (delete-region region-start (point))
 	      (goto-char region-start)
-	      (insert (format "\\begin{dmath}[number={%s}]\n" label))
+	      (insert (format "\n\\begin{dmath}[number={%s}]\n" label))
 	      (goto-char region-end)
-	      (insert "\\end{dmath}\n\n"))
+	      (insert "\n\\end{dmath}\n\n"))
 	  (progn
 	    (goto-char region-start)
-	    (insert "\\begin{verbatim}")
+	    (insert "\n\\begin{verbatim}\n")
 	    (goto-char region-end)
-	    (insert "\\end{verbatim}\n\n")))))
+	    (insert "\n\\end{verbatim}\n\n")))))
     (insert "\n\\end{document}")
     (switch-to-buffer-other-window buf)
     (latex-mode)))
@@ -823,44 +878,45 @@ STR is offending LaTeX expression.  FILENAME is name of the LaTeX file."
 			 msg)
     (concat msg str)))
 
-
 (defun imaxima-dump-tex ()
   "Dump a TeX format file preloaded with the required packages."
   (with-temp-file (expand-file-name "mylatex.ltx" imaxima-tmp-subdir)
     (insert imaxima-mylatex))
   (with-temp-file (expand-file-name "format.tex" imaxima-tmp-subdir)
     (insert
-     ;;"\\batchmode\n"
-     (format "\\documentclass[%dpt,leqno]{article}\n" imaxima-pt-size)
-     imaxima-latex-preamble
-     "\\usepackage{color}\n"
-     "\\usepackage{exscale}\n"
-     "\\usepackage{breqn}\n"
-     "\\setkeys{breqn}{compact}\n"
-     "\\usepackage{flexisym}\n"
-     "\\setlength{\\textheight}{200cm}\n"
-     ;; define \boxed from amsmath.sty
-     "\\makeatletter
-      \\providecommand\\boxed{}
-      \\providecommand\\operatorname{}
-      \\renewcommand{\\boxed}[1]{\\fbox{\\m@th$\\displaystyle#1$}}
-      \\renewcommand{\\operatorname}[1]{%
-      \\mathop{\\relax\\kern\\z@\\operator@font{#1}}}
-      \\makeatother
-      \\newcommand{\\ifrac}[2]{\\frac{#1}{#2}}
-      \\newcommand{\\ifracd}[2]{\\frac{#1}{#2}}
-      \\newcommand{\\ifracn}[2]{\\frac{#1}{#2}}
-      \\newcommand{\\isubscript}[2]{{#1}_{#2}}
-      \\newcommand{\\iexpt}[2]{{#1}^{#2}}
-      \\newcommand{\\isqrt}[1]{\\sqrt{#1}}\n
-      \\nofiles
-      \\begin{document}
-      \\end{document}"))
+     (imaxima-latex-document-class)
+     imaxima-latex-preamble "\n"
+     imaxima-latex-use-packages "\n"
+     imaxima-latex-macros-format-file "\n"
+     imaxima-latex-macros "\n"
+     "\\begin{document}\n"
+     "\\end{document}"))
   (imaxima-with-temp-dir
    imaxima-tmp-subdir
-   (apply 'call-process imaxima-tex-program nil nil nil
+   (apply #'call-process imaxima-tex-program nil nil nil
 	  (list imaxima-initex-option "&latex" "mylatex.ltx" "format"))))
-;;		(format "\\input{%s}" "format.tex")))))
+
+(defun imaxima-latex-set-textwidth ()
+  (format "\\setlength{\\textwidth}{%dmm}\n"
+	  (round (/ (imaxima-get-window-width)
+		    imaxima-scale-factor))))
+(defun imaxima-latex-set-pagecolor ()
+  (apply #'format "\\pagecolor[rgb]{%f,%f,%f}\n"
+	 (imaxima-color-to-rgb (imaxima-get-bg-color))))
+(defun imaxima-latex-set-labelcolor ()
+  (apply #'format "\\color[rgb]{%f,%f,%f}\n"
+	 (imaxima-color-to-rgb imaxima-label-color)))
+(defun imaxima-latex-format-label (str label)
+  (concat
+   (format "\\begin{dmath}[number={%s}]\n" label)
+   (apply #'format "\\color[rgb]{%f,%f,%f}\n"
+ 	  (imaxima-color-to-rgb imaxima-equation-color))
+   str (format "\n\\end{dmath}\n")))
+(defun imaxima-latex-format-output (str)
+  (concat
+   (apply #'format "\\color[rgb]{%f,%f,%f}\n"
+ 	  (imaxima-color-to-rgb imaxima-equation-color))
+   "\\begin{math} \\displaystyle \n" str (format "\n\\end{math}\n")))
 
 (defun imaxima-tex-to-dvi (str label filename &optional linear)
 "Run LaTeX on STR.
@@ -868,42 +924,17 @@ Argument LABEL is used as equation label.  FILENAME is used for
 temporary files.  Use linearized form if LINEAR is non-nil."
   (with-temp-file filename
     (insert
-     ;;"\\batchmode\n"
-     (format "\\documentclass[%dpt,leqno]{article}\n" imaxima-pt-size)
-     "\n% mylatex\n"
-     (format "\\setlength{\\textwidth}{%dmm}\n"
-	     (round (/ (imaxima-get-window-width)
-		       imaxima-scale-factor)))
-     (if linear
-	 (concat
-	  ;; braces in both denominator and numerator
-	  "\\renewcommand{\\ifrac}[2]{\\left(#1\\right)/\\left(#2\\right)}"
-	  ;; only braces denominator
-	  "\\renewcommand{\\ifracd}[2]{#1/\\left(#2\\right)}"
-	  ;; only braces in numerator
-          "\\renewcommand{\\ifracn}[2]{\\left(#1\\right)/#2}"
-          "\\renewcommand{\\isubscript}[2]{\\mathrm{subscript}\\left(#1,#2\\right)}"
-          "\\renewcommand{\\iexpt}[2]{\\mathrm{expt}\\left(#1,#2\\right)}"
-	  "\\renewcommand{\\isqrt}[1]{\\left(#1\\right)^{1/2}}\n")
-       "")
+     (imaxima-latex-document-class)
+     (imaxima-latex-set-textwidth)
+     (if linear imaxima-latex-macros-linear "")
      "\\begin{document}\n"
-     (apply 'format "\\pagecolor[rgb]{%f,%f,%f}"
-	 (imaxima-color-to-rgb (imaxima-get-bg-color)))
+     (imaxima-latex-set-pagecolor)
      "\\pagestyle{empty}\n"
      (format "\\begin{%s}\n" imaxima-fnt-size)
-     (apply 'format "\\color[rgb]{%f,%f,%f}"
-	    (imaxima-color-to-rgb imaxima-label-color))
-     "\\tt"
+     (imaxima-latex-set-labelcolor)
      (if label
- 	 (concat
- 	  (format "\\begin{dmath}[number={%s}]\n" label)
- 	  (apply 'format "\\color[rgb]{%f,%f,%f}"
- 		 (imaxima-color-to-rgb imaxima-equation-color))
- 	  str (format "\\end{dmath}\n"))
-       (concat
- 	(apply 'format "\\color[rgb]{%f,%f,%f}"
- 	       (imaxima-color-to-rgb imaxima-equation-color))
- 	"\\begin{math} \\displaystyle " str (format " \\end{math}\n")))
+ 	 (imaxima-latex-format-label str label)
+       (imaxima-latex-format-output str))
      (format "\\end{%s}\n" imaxima-fnt-size)
      "\\end{document}"))
     (imaxima-with-temp-dir imaxima-tmp-subdir
