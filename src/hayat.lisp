c063b7804159081cf1f2447ddf323ff312db9ccd
Robert Dodier <robert_dodier@users.sourceforge.net>
2020-05-02 09:40:57 -0700

    In TAYLOR2, detect derivative wrt a non-taylor variable and handle it as a general expression.
    Apparently fixes bug reported to mailing list 2020-04-30: "Bug in taylor?",
    however, problem 34 (line 78) in share/tensor/rtest_ctensor.mac,
    which has something to do with Taylor series applied to ctensor, now fails.
    Committing this change in order to make it available for others to review.

--- src/hayat.lisp
+++ src/hayat.lisp
@@ -2242,7 +2242,10 @@
 	((and (member (caar e) '(%sum %product) :test #'eq)
 	      (mfreel (cddr e) tvars)) 
 	 (tsprsum (cadr e) (cddr e) (caar e)))
-	((eq (caar e) '%derivative) (tsdiff (cadr e) (cddr e) e))
+	;; If TSDIFF returns NIL, it means that the derivative is wrt to some variable
+	;; other than one of the taylor variables.
+	;; If so, keep going and handle E as if it were a general expression.
+	((and (eq (caar e) '%derivative) (tsdiff (cadr e) (cddr e) e)))
 	((or (eq (caar e) '%at)
 	     (do ((l (mapcar 'car tlist) (cdr l)))
 		 ((null l) t)
@@ -2987,7 +2990,10 @@
 			   (assoc (car l) tlist :test #'eq))
 		      (setq n (cons (cadr l) n) v (cons (car l) v))
 		      (setq u (cons (car l) (cons (cadr l) u)))))
-	      (or n (return (prep1 check)))
+	      ;; If N is null, it means that the derivative is wrt to some variable
+	      ;; other than one of the taylor variables.
+	      ;; If so, give up; let the caller handle it.
+	      (or n (return nil))
 	      (if u (setq e (meval (cons '($diff) (cons e l)))))
 	      (setq l (mapcar #'(lambda (x) (get-datum x)) v))
 	      (mapcar #'(lambda (datum pw)
