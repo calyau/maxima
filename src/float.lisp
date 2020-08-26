810eb1ddb0e39bf4c0a7ab46d8eb50fa91b909fa
Robert Dodier <dodier@ohsu.edu>
2020-08-26 10:02:55 -0700

    Print warning message about float to bigfloat conversion only if bigfloat precision is less than float precision (and float2bf is false).

--- src/float.lisp
+++ src/float.lisp
@@ -707,7 +707,9 @@ One extra decimal digit in actual representation for rounding purposes.")
   (if (float-inf-p x)
     (merror (intl:gettext "bfloat: attempted conversion of floating-point infinity.~%")))
   (unless $float2bf
-    (mtell (intl:gettext "bfloat: converting float ~S to bigfloat.~%") x))
+    (let ((p (float-precision x)))
+      (if (< fpprec p)
+        (mtell (intl:gettext "bfloat: converting float ~S to bigfloat.~%") x))))
 
   ;; Need to check for zero because different lisps return different
   ;; values for integer-decode-float of a 0.  In particular CMUCL
