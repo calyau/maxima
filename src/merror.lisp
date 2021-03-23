calyau/maxima

commit b50ab644b0de0be61a20adb9b1daa9f2fe9f8a8b

Define a MAXIMA-$ERROR condition and let MERROR signal it if ERRCATCH is non-NIL and *MDEBUG* is NIL.  Previously a SIMPLE-ERROR was signalled in this case.
(maxima-$error): New condition.

(merror): Use it.

(with-$error): Convenience macro.

src/merror.lisp
@@ -74,6 +74,13 @@
 ;;  (AND $ERRORMSG ($ERRORMSG))
 ;;  (MAXIMA-ERROR #+(OR CL NIL) STRING))
 
+(define-condition maxima-$error (error)
+  ((message :initform $error :reader the-$error))
+  (:documentation "Muser error, to be signalled by MERROR, usually.")
+  (:report (lambda (c stream)
+	     (declare (ignore c))
+	     (let ((*standard-output* stream))
+	       ($errormsg)))))
 
 (defun merror (sstring &rest l)
   (declare (special errcatch *mdebug*))
@@ -95,7 +102,7 @@
 	     ;;	           (t  (throw 'macsyma-quit t)
 	     ;;	      ))
 	     )))
-	(errcatch  (error " -- an error: macsyma error"))
+	(errcatch  (error 'maxima-$error))
 	(t
 	 (fresh-line *standard-output*)
 	 ($backtrace 3)
@@ -104,9 +111,18 @@
 					;(if errcatch (error "macsyma error"))
 	 )))
 
-;;sample:
-;;(defun h (he)
-;;  (merror "hi there ~:M and ~:M" he he))
+(defmacro with-$error (&body body)
+  "Let MERROR signal a MAXIMA-$ERROR condition."
+  `(let ((errcatch t) *mdebug*	       ;let merror signal a lisp error
+	 $errormsg)			;don't print $error
+     (declare (special errcatch))
+     ,@body))
+
+;; Sample:
+;; (defun h (he)
+;;   (merror "hi there ~:M and ~:M" he he))
+;; This will signal a MAXIMA-$ERROR condition:
+;; (with-$error (h '$you))
 
 (defmvar $error_syms '((mlist) $errexp1 $errexp2 $errexp3)
   "Symbols to bind the too-large `maxima-error' expresssions to")

commit 731ffbac97b22f3eab1ff6273f8af92a3a1298f5

Allow debugmode to be locally bound
Binding debugmode has been broken because *MDEBUG* was being bound
to NIL by MBIND via the WITH-$ERROR macro.

Instead of WITH-$ERROR binding *MDEBUG* and other variables in an
effort to force MERROR to signal a MAXIMA-$ERROR condition, let's
just have it bind the new *MERROR-SIGNALS-$ERROR-P* and have MERROR
check for this.

While here, also move the modified WITH-$ERROR so the code for the
MAXIMA-$ERROR stuff is together.

WITH-$ERROR was introduced in commit b50ab644 and has been used in
MBIND since commit 88d94f51 (both commits were in December 2004).

This was briefly discussed in the mailing list thread "debugmode:lisp"
on 2021-03-15.

No problems with the test suite or share test suite.

src/merror.lisp
@@ -75,10 +75,27 @@
 	     (let ((*standard-output* stream))
 	       ($errormsg)))))
 
+(defvar *merror-signals-$error-p* nil
+  "When T, MERROR will signal a MAXIMA-$ERROR condition.")
+
+;; Sample:
+;; (defun h (he)
+;;   (merror "hi there ~:M and ~:M" he he))
+;; This will signal a MAXIMA-$ERROR condition:
+;; (with-$error (h '$you))
+
+(defmacro with-$error (&body body)
+  "Let MERROR signal a MAXIMA-$ERROR condition."
+  `(let ((*merror-signals-$error-p* t))
+     (declare (special *merror-signals-$error-p*))
+     ,@body))
+
 (defun merror (sstring &rest l)
   (declare (special errcatch *mdebug*))
   (setq $error `((mlist simp) ,sstring ,@ l))
-  (cond ((eq *mdebug* '$lisp)
+  (cond (*merror-signals-$error-p*
+	 (error 'maxima-$error))
+	((eq *mdebug* '$lisp)
 	 ; Go immediately into the lisp debugger
 	 (let ((*debugger-hook* nil))
 	   (invoke-debugger (make-condition 'maxima-$error))))
@@ -108,20 +125,6 @@
 
 (defun mwarning (&rest l)
   (format t "Warning: ~{~a~^ ~}~%" (mapcar #'$sconcat l)))
-
-(defmacro with-$error (&body body)
-  "Let MERROR signal a MAXIMA-$ERROR condition."
-  `(let ((errcatch t)
-	 *mdebug*		       ;let merror signal a lisp error
-	 $errormsg)			;don't print $error
-     (declare (special errcatch *mdebug* $errormsg))
-     ,@body))
-
-;; Sample:
-;; (defun h (he)
-;;   (merror "hi there ~:M and ~:M" he he))
-;; This will signal a MAXIMA-$ERROR condition:
-;; (with-$error (h '$you))
 
 (defmvar $error_syms '((mlist) $errexp1 $errexp2 $errexp3)
   "Symbols to bind the too-large `maxima-error' expresssions to")