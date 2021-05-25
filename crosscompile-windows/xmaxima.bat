d9ef3784a454ffdc23a24558333b5f2c57f392bf
Wolfgang Dautermann <dauti@users.sourceforge.net>
2021-05-25 22:50:24 +0200

    Windows installer: Compile TCL/TK statically.
    For Windows/32bit I got a strange error message
    when TCL wanted to load the library tcl86.dll:
    "The procedure entry point AccessCheck could not
    be located in the dynamic link library
    C:\maxima-5.45.0\bin\tcl86.dll".
    
    It works, when using the configure parameter
    --enable-shared=no
    
    wish86.exe is called wish86s.exe in that case.
    
    This change is also included in the 32 bit Windows-installer
    for Maxima 5.45.0.

--- crosscompile-windows/xmaxima.bat
+++ crosscompile-windows/xmaxima.bat
@@ -9,5 +9,5 @@ set "xmpath=%xmpath:\=/%"
 
 set "MAXIMA_PREFIX=%xmpath%/.."
 
-start "Xmaxima" /b "%xmpath%wish86.exe" "%xmpath%xmaxima" %*
+start "Xmaxima" /b "%xmpath%wish86s.exe" "%xmpath%xmaxima" %*
 
