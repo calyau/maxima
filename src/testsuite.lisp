7936fa95e1c7ff084cfbd2383cd5455ef5e63724
Barton Willis <willisb@unk.edu>
2021-06-03 04:40:45 -0500

    Correctly mark test 146 of rtest3 as a known failure.  This corrects an error from my previous commit.

--- src/testsuite.lisp
+++ src/testsuite.lisp
@@ -67,7 +67,7 @@
 	 #+allegro ((mlist simp) 50 241 524 525))
         "rtestode"
 	"rtestode_zp"
-        ((mlist simp) "rtest3" 146)
+        ((mlist simp) "rtest3" ((mlist simp) 146))
 	;; ECL 16.1.2 still fails in #104
 	((mlist simp) "rtest8"
 	 #+ecl ((mlist simp) 104))
