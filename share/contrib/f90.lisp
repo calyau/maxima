61ca2a42706378843904df9efd9539db70f37bc9
Wolfgang Dautermann <dauti@users.sourceforge.net>
2021-07-27 23:16:25 +0200

    Update FSF address.

--- share/contrib/f90.lisp
+++ share/contrib/f90.lisp
@@ -15,8 +15,7 @@
 
 ;;;; You should have received a copy of the GNU General Public License
 ;;;; along with f90.lisp; see the file COPYING.  If not,
-;;;; write to the Free Software Foundation, Inc., 59 Temple Place -
-;;;; Suite 330, Boston, MA 02111-1307, USA.
+;;;; write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
 
 ;;;; Based on fortra.lisp. Copyright statements for fortra.lisp follow:
 ;;;;  Copyright (c) 1984,1987 by William Schelter,University of Texas
