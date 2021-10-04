7966357ccc86c5b245efc95e03f5be9b9de59eb0
Raymond Toy <toy.raymond@gmail.com>
2021-10-04 10:57:00 -0700

    Clean up and add comments describing how things work.
    Also add a defvar so we can enable debugging prints whenever we want
    to.

--- share/contrib/engineering-format.lisp
+++ share/contrib/engineering-format.lisp
@@ -41,53 +41,59 @@
 (defmvar $engineering_format_min 0.0)
 (defmvar $engineering_format_max 0.0)
 
-#+nil
-(defun engineering-format (x)
-  (if (= x 0.0)
-    (format nil "~e" x)
-    (let*
-      ((integer-log (floor (/ (log (abs x)) (log 10.0))))
-       (scale (1+ (mod integer-log 3)))
-       (effective-fpprintprec (if (= $fpprintprec 0) 16 $fpprintprec))
-       (digits (1- effective-fpprintprec)))
-      (declare (special $fpprintprec))
-      (format nil "~,v,,ve" digits scale x))))
+(defvar *debug-eng-format* nil
+  "Set to non-NIL to enable some debugging prints for engineering format")
 
 (defun engineering-format (x)
   (if (= x 0.0)
-    (format nil "~e" x)
-    (let*
-        ((integer-log (floor
-                       #+nil
-                       (log x 10)
-                       (/ (log (abs x)) (log 10.0)))))
-      (unless (and (<= (expt 10d0 integer-log)
-                       x)
-                   (< x (expt 10d0 (1+ integer-log))))
-        (incf integer-log))
-      (let*
-          ((scale (1+ (mod integer-log 3)))
-           (effective-fpprintprec (if (= $fpprintprec 0) 16 $fpprintprec))
-           (digits (1- effective-fpprintprec))
-           (result (format nil "~,v,,ve" digits scale x))
-           (dot-posn (position #\. result :test #'char-equal))
-           (expo (parse-integer result
-                                :start (1+ (position #\e result :test #'char-equal)))))
-        (declare (special $fpprintprec))
-        #+nil
-        (format t "X = ~A log = ~A scale = ~A digits ~A expo ~A dot ~A result ~A~%"
-                x integer-log scale digits expo dot-posn result)
-        (unless (and (< dot-posn 4)
-                     (zerop (rem expo 3)))
-          #+nil
-          (format t "Expo ~A posn ~A: Result is wrong:  ~A~%" expo dot-posn result)
-          (decf scale)
-          (setf result (format nil "~,v,,ve" digits scale x))
-          #+nil
-          (format t "New result: ~A~%" result)
-        
-          )
-        result))))
+      (format nil "~e" x)
+      ;; Cmucl has an accurate implementation of log10, which can be
+      ;; accessed via (log x 10).  For all other lisps, we can use it too.
+      (let* ((integer-log (floor (log x 10.0))))
+	;; Let n = integer-log.  If things worked correctly, then 10^n
+	;; <= x < 10^(n+1).  However if some rounding errors occurred,
+	;; it's possible that n is too small.  Check for that and adjust
+	;; n appropriately.
+	;;
+	;; This assumes (expt 10d0 n) works accurately.  We can't do
+	;; (expt 10 n) because (expt 10 -7) is actually slightly larger
+	;; than 1d-7.
+	(unless (and (<= (expt 10d0 integer-log)
+			 x)
+                     (< x (expt 10d0 (1+ integer-log))))
+          (incf integer-log))
+	(let*
+            ((scale (1+ (mod integer-log 3)))
+             (effective-fpprintprec (if (= $fpprintprec 0) 16 $fpprintprec))
+             (digits (1- effective-fpprintprec))
+             (result (format nil "~,v,,ve" digits scale x))
+	     ;; Find the location of the decimal point in the printed result.
+             (dot-posn (position #\. result :test #'char-equal))
+	     ;; Find the value of the exponent in the printed result.
+             (expo (parse-integer result
+                                  :start (1+ (position #\e result :test #'char-equal)))))
+          (declare (special $fpprintprec))
+
+          (when *debug-eng-format*
+            (format t "X = ~A log = ~A scale = ~A digits ~A expo ~A dot ~A result ~A~%"
+                    x integer-log scale digits expo dot-posn result))
+	
+	  ;; Check that the printed result does what we want.  This
+	  ;; accounts for some roundoff in lisp's format function.
+          (unless (and (< dot-posn 4)
+                       (zerop (rem expo 3)))
+            (when *debug-eng-format*
+              (format t "Expo ~A posn ~A: Result is wrong:  ~A~%" expo dot-posn result))
+	  
+	    ;; For all the cases I've seen, if the exponent is not a
+	    ;; multiple of 3, decrementing the scale by 1 will get lisp
+	    ;; to print out the right thing.
+            (decf scale)
+            (setf result (format nil "~,v,,ve" digits scale x))
+
+            (when *debug-eng-format* 
+              (format t "New result: ~A~%" result)))
+          result))))
 
 (let ((foo (symbol-function 'exploden)))
   (defun exploden (x)
