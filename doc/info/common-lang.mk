e82f971e8ac820dfdaefd9acbbbee0578cf4a7a6
Robert Dodier <robert_dodier@users.sourceforge.net>
2022-01-10 21:14:08 -0800

    In doc/info build, declare dependencies on maxima_toc.html instead of maxima.html.

--- doc/info/common-lang.mk
+++ doc/info/common-lang.mk
@@ -9,7 +9,7 @@ genericdirDATA = \
 contents.hhc index.hhk header.hhp maxima.hhp
 endif
 
-all-local: maxima-index.lisp maxima.html contents.hhc $(MAXIMA_CHM)
+all-local: maxima-index.lisp maxima_toc.html contents.hhc $(MAXIMA_CHM)
 
 install-data-local: $(INSTALL_CHM)
 
@@ -18,7 +18,7 @@ uninstall-local: $(UNINSTALL_CHM)
 maxima-index.lisp: maxima.info $(srcdir)/../build_index.pl
 	/usr/bin/env perl $(srcdir)/../build_index.pl maxima.info ':crlf' > maxima-index.lisp
 
-maxima.html: maxima.texi $(maxima_TEXINFOS)
+maxima_toc.html: maxima.texi $(maxima_TEXINFOS)
 	$(srcdir)/../build_html.sh -l $(lang) -D
 
 maxima.pdf: maxima.texi $(maxima_TEXINFOS)
@@ -26,8 +26,8 @@ maxima.pdf: maxima.texi $(maxima_TEXINFOS)
 	rm -f maxima.fns maxima.vr maxima.tp maxima.pg maxima.ky maxima.cp \
 	maxima.toc maxima.fn maxima.aux maxima.log maxima.vrs
 
-contents.hhc: maxima.html
-	/usr/bin/env perl $(srcdir)/../create_index `grep -l id..SEC_Contents maxima*.html`
+contents.hhc: maxima_toc.html
+	/usr/bin/env perl $(srcdir)/../create_index maxima_toc.html
 
 include $(top_srcdir)/common.mk
 
@@ -43,7 +43,7 @@ clean-info:
 	rm -f maxima-index.lisp
 
 clean-html:
-	rm -f maxima.html maxima_*.html
+	rm -f maxima*.html
 	rm -f maxima_singlepage.html
 	rm -f contents.hhc
 	rm -f index.hhk
@@ -51,7 +51,7 @@ clean-html:
 EXTRA_DIST = maxima-index.lisp $(genericdirDATA)
 
 # This builds the Windows help file maxima.chm
-maxima.chm: maxima.html maxima.hhp contents.hhc index.hhk
+maxima.chm: maxima_toc.html maxima.hhp contents.hhc index.hhk
 	$(MKDIR_P) chm
 	$(MKDIR_P) chm/figures
 	for hfile in *.html ; do \
