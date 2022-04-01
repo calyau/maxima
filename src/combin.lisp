cc07526de4fb72f8acf465172948346a3dd3f4a7
Kris Katterjohn <katterjohn@gmail.com>
2022-04-01 16:18:49 -0400

    SRRAT: use MRAT reader functions instead of CADDAR, etc.
    No functional changes.

--- src/combin.lisp
+++ src/combin.lisp
@@ -1400,8 +1400,8 @@
 	(t (ratf ($ratdisrep e)))))
 
 (defun srrat (e)
-  (cons (list 'mrat 'simp (caddar e) (cadddr (car e)))
-	(srrat2 (cdr e))))
+  (cons (list 'mrat 'simp (mrat-varlist e) (mrat-genvar e))
+	(srrat2 (mrat-ps e))))
 
 (defun srrat2 (e)
   (if (pscoefp e) e (srrat3 (terms e) (gvar e))))
