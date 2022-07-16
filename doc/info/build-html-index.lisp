2b20a3109f9a43234e033d22de485847fa15025f
Raymond Toy <toy.raymond@gmail.com>
2022-07-16 09:30:44 -0700

    Index was missing items with encoded characters.
    The HTML documentation was missing things like "? '".  This is because
    the entries were encoded in hex form, and we didn't see them while
    searching.  Find and convert more encoded characters.
    
    We also refactored the code to add a new function
    PROCESS-ONE-HTML-FILE to handle one file (useful for testing on just
    one file).  Also added a new function HANDLE-SPECIAL-CHARS to handle
    the encoding of the special characters.
    
    This makes things a bit slower.  Should consider some alternatives.

--- doc/info/build-html-index.lisp
+++ doc/info/build-html-index.lisp
@@ -6,6 +6,67 @@
   documentation.  The key is the topic we're looking for and the value
   is the html file containing the documentation for the topic.")
 
+
+;; This might be rather slow.  Perhaps an alternative solution is to
+;; leave these alone and have $hdescribe encode any special characters
+;; before looking them up.  Since ? only used occasionally, we don't
+;; incur the cost here and move it to ? where the impact is lower.
+(defun handle-special-chars (item)
+  "Handle special encoded characters in HTML file.  Texinfo encodes
+  special characters to hexadecimal form and this needs to be undone
+  so we know what the actual character is when looking up the
+  documentation."
+  ;; This is probably not the best way to do this.  Regexp searches
+  ;; are probably pretty expensive.
+  (dolist (spec-char '(#\% #\$ #\? #\. #\< #\> #\#
+		       #\= #\: #\* #\- #\\ #\^ #\+ #\/ #\'))
+    (let ((code (string-downcase
+		 (format nil "_~4,'0x" (char-code spec-char)))))
+      (setf item
+	    (pregexp:pregexp-replace* code item (string spec-char)))))
+  item)
+
+(defun process-one-html-file (file entry-regexp section-regexp)
+  "Process one html file to find all the documentation entries.
+  ENTRY-REGEXP is the regexp to use for find function and variable
+  items.  SECTION-REGEXP is the regexp to find sections to include."
+  (format *debug-io*  "Processing: ~S~%" file)
+  (let ((base-name (make-pathname :name (pathname-name file)
+                                  :type (pathname-type file))))
+    (with-open-file (s file :direction :input)
+      (loop for line = (read-line s nil)
+            while line
+	    do
+	       (let (match)
+		 (cond
+		   ((setf match (pregexp:pregexp-match-positions entry-regexp line))
+		    (let ((item-id (subseq line
+					   (car (elt match 1))
+					   (cdr (elt match 1))))
+			  item)
+		      ;; Remove "005f" which texinfo adds before every "_".
+		      #+nil
+		      (format t "item-id = ~A~%" item-id)
+                      (setf item
+			    (pregexp:pregexp-replace* "005f" item-id ""))
+		      (setf item (handle-special-chars item))
+                      #+nil
+                      (format t "match = ~S ~A~%" match item)
+                      (setf (gethash item
+                                     *html-index*)
+			    (cons base-name item-id))))
+		   ((setf match (pregexp:pregexp-match-positions section-regexp line))
+		    (let ((item-id (subseq line
+					   (car (elt match 1))
+					   (cdr (elt match 1))))
+			  (item (subseq line
+					(car (elt match 2))
+					(cdr (elt match 2)))))
+		      #+nil
+		      (format t "section item = ~A~%" item)
+		      (setf (gethash item *html-index*)
+			    (cons base-name item-id))))))))))
+
 ;; Run this build a hash table from the topic to the HTML file
 ;; containing the documentation.  The single argument DIR should be a
 ;; directory that contains the html files to be searched for the
@@ -32,41 +93,8 @@
     (dolist (file (directory dir))
       ;; We want to ignore maxima_singlepage.html for now.
       (unless (string-equal (pathname-name file)
-                            "maxima_singlepage")
-        (format *debug-io*  "Processing = ~S~%" file)
-        (let ((base-name (make-pathname :name (pathname-name file)
-                                        :type (pathname-type file))))
-          (with-open-file (s file :direction :input)
-            (loop for line = (read-line s nil)
-                  while line
-		  do
-		     (let (match)
-		       (cond
-			 ((setf match (pregexp:pregexp-match-positions entry-regexp line))
-			  (let ((item-id (subseq line
-						 (car (elt match 1))
-						 (cdr (elt match 1))))
-				item)
-			    ;; Remove "005f" which texinfo adds before every "_".
-                            (setf item
-				  (pregexp:pregexp-replace* "005f" item-id ""))
-			    ;; Replace "_0025" which texinfo uses to represent "%".
-			    (setf item
-				  (pregexp:pregexp-replace* "_0025" item "%"))
-                            #+nil
-                            (format t "match = ~S ~A~%" match item)
-                            (setf (gethash item
-                                           *html-index*)
-				  (cons base-name item-id))))
-			 ((setf match (pregexp:pregexp-match-positions section-regexp line))
-			  (let ((item-id (subseq line
-						 (car (elt match 1))
-						 (cdr (elt match 1))))
-				(item (subseq line
-					      (car (elt match 2))
-					      (cdr (elt match 2)))))
-			    (setf (gethash item *html-index*)
-				  (cons base-name item-id)))))))))))))
+                        "maxima_singlepage")
+	(process-one-html-file file entry-regexp section-regexp)))))
 
 (defun build-and-dump-html-index (dir)
   (build-html-index dir)
