24f8d6bcfc6fb476c20ad3756924b61e176df024
Raymond Toy <toy.raymond@gmail.com>
2022-12-13 15:56:12 -0800

    Add setting-predicate  for $parsewindow
    We inadvertently deleted the `msetchk` code for `$parsewindow` without
    updating it to use a `:setting-predicate`.  Look up the old code and
    create a predicate for this.

--- src/nparse.lisp
+++ src/nparse.lisp
@@ -837,7 +837,9 @@ each side of the error-point when a syntax (parsing) MAXIMA-ERROR occurs.  This
 option is especially useful on slow terminals.  Setting it to -1 causes the
 entire input string to be printed out when an MAXIMA-ERROR occurs."
   fixnum
-  :properties ((assign 'msetchk)))
+  :setting-predicate #'(lambda (val)
+			 (and (fixnump val)
+			      (>= val -1))))
 
 
 ;;;; Misplaced definitions
