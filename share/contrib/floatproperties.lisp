1057408205445ef45d1c75754a55ac83bd0c65f5
Raymond Toy <toy.raymond@gmail.com>
2023-02-22 16:33:24 -0800

    Update float properties and numdistrib for float constants
    Since we renamed the float constants to use the Lisp convention +foo+,
    we need to update files in the share directory to use the new names.

--- share/contrib/floatproperties.lisp
+++ share/contrib/floatproperties.lisp
@@ -2,24 +2,24 @@
 ;; Note: floatbits is one plus the number of bits in the fractional part.
 
 (defun $float_eps ()
-  flonum-epsilon)
+  +flonum-epsilon+)
 
-(defmvar $largest_float most-positive-flonum)
+(defmvar $largest_float +most-positive-flonum+)
 (setf (get '$largest_float 'assign) 'neverset)
 
-(defmvar $largest_negative_float most-negative-flonum)
+(defmvar $largest_negative_float +most-negative-flonum+)
 (setf (get '$largest_negative_float 'assign) 'neverset)
 
-(defmvar $least_positive_float least-positive-flonum)
+(defmvar $least_positive_float +least-positive-flonum+)
 (setf (get '$least_positive_float 'assign) 'neverset)
 
-(defmvar $least_negative_float least-negative-flonum)
+(defmvar $least_negative_float +least-negative-flonum+)
 (setf (get '$least_negative_float 'assign) 'neverset)
 
-(defmvar $least_positive_normalized_float least-positive-normalized-flonum)
+(defmvar $least_positive_normalized_float +least-positive-normalized-flonum+)
 (setf (get '$least_positive_normalized_float 'assign) 'neverset)
 
-(defmvar $least_negative_normalized_float least-negative-normalized-flonum)
+(defmvar $least_negative_normalized_float +least-negative-normalized-flonum+)
 (setf (get '$least_negative_normalized_float 'assign) 'neverset)
 
 (defun $float_bits ()
--- share/distrib/numdistrib.lisp
+++ share/distrib/numdistrib.lisp
@@ -316,10 +316,10 @@
 ;; Conditions: 0<p<1; f>0 (degrees of freedom) ; theta>0 (non centrality param.)
 (defun qnchi2 (p df lambda)
    (let ((accu 1e-13)
-         (racc (* 8 flonum-epsilon))
-         (dbl_epsilon (* 8 flonum-epsilon))
-         (dbl_min least-positive-normalized-flonum)
-         (dbl_max most-positive-flonum)
+         (racc (* 8 +flonum-epsilon+))
+         (dbl_epsilon (* 8 +flonum-epsilon+))
+         (dbl_min +least-positive-normalized-flonum+)
+         (dbl_max +most-positive-flonum+)
          (eps 1e-11)    ; must be > accu
          (reps 1e-10)   ; relative tolerance
          (ux 1.0) (lx 1.0) (nx 1.0) (pp 1.0))
