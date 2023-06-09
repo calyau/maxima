3983ef744dfba88bb92a69911ef7a45a3d074467
Raymond Toy <toy.raymond@gmail.com>
2023-06-08 18:20:59 -0700

    More cleanups and documentation

--- doc/info/create-html-index.lisp
+++ doc/info/create-html-index.lisp
@@ -6,15 +6,18 @@
   documentation.  The key is the topic we're looking for and the value
   is the html file containing the documentation for the topic.")
 
-(defvar *log-file* nil)
+(defvar *log-file* nil
+  "Log file containing info for each entry that is added to the index
+  table.")
 
 (let ((maxima_nnn-pattern (pregexp:pregexp "^maxima_[0-9][0-9]*$")))
   (defun maxima_nnn-p (f)
+    "Determine if F is a pathname-name that looks like
+    \"maxima_<digits>\".  Return non-NIL if so."
     (pregexp:pregexp-match maxima_nnn-pattern f)))
 
-;; Similar to grep -l: returns F-PATH if file contains CONTENT-PATTERN, otherwise NIL.
-
 (defun grep-l (content-pattern f-path)
+  "Similar to grep -l: returns F-PATH if file contains CONTENT-PATTERN, otherwise NIL."
   (with-open-file (s f-path)
     (loop for line = (read-line s nil)
           while line
@@ -22,30 +25,36 @@
                  (return f-path)))))
 
 (defun add-entry (item item-id file line &key replace-dash-p prefix)
-  ;; Add entry to the hash table.
-  ;;
+  "Add entry to the html hash table.  ITEM is the key, FILE is the html
+  file where this item is found, ITEM-ID is the html id of this item.
+  LINE is the line where this was found and is used for information if
+  a duplicate key was found."
+
   ;; Replace any special chars that texinfo has encoded.
   (setf item (handle-special-chars item))
 
   ;; Replace "-" with space, but only if "-" is not followed by a
   ;; digit.  "foo-1" is the info index named "foo <1>", so we don't
-  ;; want to change that.
+  ;; want to change that.  This isn't perfect.  See HANDLE-SPECIAL-CASES.
   (when replace-dash-p
     (setf item (pregexp:pregexp-replace* "-([^[:digit:]])" item " \\1")))
 
-  ;; Check if the entry already exists and print
-  ;; a message.  Presumably, this shouldn't happen, so warn if it
-  ;; does.
+  ;; Check if the entry already exists and print a message.  This
+  ;; shouldn't happen, so print a message if it does.
   (when (gethash item *html-index*)
     (format t "Already added entry ~S ~S: ~S~%"
 	    item (gethash item *html-index*)
 	    line))
   (format *log-file* "~A: ~S -> ~S ~S~%"
 	  prefix item file item-id)
+
   (setf (gethash item *html-index*)
 	(cons file item-id)))
 
 (defun process-line (line matcher &key replace-dash-p (prefix "Add:"))
+  "Process the LINE using the function MATCHER to determine if this line
+  contains something interesting to add to the index. REPLACE-DASH-P
+  and PREFIX are passed to ADD-ENTRY."
   (multiple-value-bind (item item-id file line)
       (funcall matcher line)
     (when item
@@ -54,6 +63,9 @@
 		 :prefix prefix))))
 
 (defun process-one-html-file (file matcher replace-dash-p prefix)
+  "Process one html file named FILE using MATCHER to determine matches.
+  REPLACE-DASH-P and PREFIX are passed to PROCESS-LINE which will
+  handle these."
   (format *debug-io*  "Processing: ~S~%" file)
   (with-open-file (s file :direction :input)
     (loop for line = (read-line s nil)
@@ -64,14 +76,19 @@
 			   :prefix prefix))))
 
 (defun handle-special-cases ()
-  ;; These HTML topics need special handling because we didn't quite
-  ;; process them correctly previously because we accidentally changed
-  ;; #\- to #\space.
+  "These HTML topics need special handling because we didn't quite
+  process them correctly previously because we accidentally changed
+  #\- to #\space."
   (flet ((update-entry (old new)
+	   ;; Set the new index entry with the value of the old index
+	   ;; entry and remove the old entry from the index.
 	   (when (gethash old *html-index*)
 	     (setf (gethash new *html-index*)
 		   (gethash old *html-index*))
 	     (remhash old *html-index*))))
+    ;; List of special cases.  The car of each item is the index entry
+    ;; that should not have had the hyphen replaced.  The cdr is what
+    ;; it should be.
     (dolist (items '(("Quote quote operator" "Quote-quote operator")
 		     ("Assignment operator (evaluates left hand side)"
 		      "Assignment operator (evaluates left-hand side)")
@@ -81,6 +98,14 @@
 	  items
 	(update-entry old new)))))
 
+;; Find entries from the function and variable index.  An example of
+;; what we're looking for:
+;;
+;;   <a href="maxima_55.html#index-asinh">
+;;
+;; We extract the file name and the stuff after "#index-" which is the
+;; html id that we need.  It's also the key we need for the hash
+;; table.
 (let ((href (pregexp:pregexp "<a href=\"(maxima_[[:digit:]]+\.html)#index-([^\"]*)\">")))
   (defun match-entries (line)
     (let ((match (pregexp:pregexp-match href line)))
@@ -90,6 +115,12 @@
 	  (declare (ignore whole))
 	  (values item-id item-id file line))))))
 
+;; Find entries from the TOC.  An example of what we're looking for:
+;;
+;;  <a id="toc-Bessel-Functions-1" href="maxima_14.html#Bessel-Functions">15.2 Bessel Functions</a>
+;;
+;; We extract the file name and then the title of the subsection.
+;; Further subsections are ignored.
 (let ((regexp (pregexp:pregexp "<a id=\"toc-.*\" href=\"(maxima_[^\"]+)\">[[:digit:]]+\.[[:digit:]]+ ([^\"]+?)<")))
   (defun match-toc (line)
     (let ((match (pregexp:pregexp-match regexp line)))
