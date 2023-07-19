b0d3ac2a9048251746cebd03324b78d2d261e81d
Raymond Toy <toy.raymond@gmail.com>
2023-07-19 14:02:24 -0700

    Fix #4158: Make float(%i) return 1.0*%i
    float(li[2](%i)) was  not resulting in a numerical value because
    float(%i) is %i.  The simplifier sees the arg as %i and doesn't
    numerically evaluate.
    
    To fix this, modify $float to convert %i to be 1.0*%i.  Need to do the
    same for bfloat.
    
    This also fixes the issue where li[2](%i),numer was causing a lisp
    error.  This works because we call $float before calling
    bigfloat::li2numer to compute the numerical result.

--- src/comm.lisp
+++ src/comm.lisp
@@ -1202,6 +1202,9 @@
 (defmfun $float (e)
   (cond ((numberp e)
 	 (let ((e1 (float e))) (if (float-inf-p e1) (signal 'floating-point-overflow) e1)))
+	((eq e '$%i)
+	 ;; Handle %i specially.
+	 (mul 1.0 '$%i))
 	((and (symbolp e) (mget e '$numer)))
 	((or (atom e) (member 'array (cdar e) :test #'eq)) e)
 	((eq (caar e) 'rat) (fpcofrat e))
