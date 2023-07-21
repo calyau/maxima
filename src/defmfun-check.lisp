adff8dbd85a6cb538d8f04b5504a538fd12871bc
Raymond Toy <toy.raymond@gmail.com>
2023-07-21 07:39:16 -0700

    Declare verb function as inline
    Since the created verb function is so small, might as well declare it
    inline so it can be inlined by compilers that support inlining.
    
    Might cause a percent or two speedup, but that might be in the noise.

--- src/defmfun-check.lisp
+++ src/defmfun-check.lisp
@@ -636,7 +636,9 @@
 			       and count from 1
 			       collect (list arg `(simpcheck (nth ,count ,form-arg) ,z-arg)))))))
       `(progn
-	 ;; Define the noun function
+	 ;; Define the noun function.  Since it's so short, make in
+	 ;; inline too.
+	 (declaim (inline ,verb-name))
 	 (defun ,verb-name (,@lambda-list)
 	   (ftake ',noun-name ,@lambda-list))
 
