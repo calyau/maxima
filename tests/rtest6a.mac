calyau/maxima

commit c9dd89bd44d9b0289aaba2de559f3664e38f2625

Fix bug #4176: fullratsubst
fullratsubst can be called in two ways:

fullratsubst(new,old,expr);
fullratsubst(old = new,expr);

This patch ensures that the second form gets punted to the first form.

Two tests are included, courtesy of Baron Willis.

share/simplification/lrats.mac
@@ -65,7 +65,7 @@ fullratsubst([arglist]):=block(
    partswitch:true,inflag:true,piece],
   if larglistdum=2
   then if listp(farglist:first(arglist)) or inpart(farglist,0)="="
-       then lratsubst(farglist,last(arglist))
+       then fullratsubst(second(farglist),first(farglist),last(arglist))
        else error(messlrats2,arglist)
   else if larglistdum=3
        then apply('fullratsubst1,arglist)

tests/rtest6a.mac
@@ -162,6 +162,16 @@ cos(11/10)/1000;
 (load(lrats), 0);
 0;
 
+/* SF bug #4176: fullratsubst
+   In these examples, we make sure that both forms of calling
+   fullratsubst work as described in the documentation.
+*/
+fullratsubst(3*z^8 + 13, z^9, 147*z^13 -637);
+35721*z^8+1911*z^4+5733*z^3+17199*z^2+51597*z+154154;
+
+fullratsubst(z^9 = 3*z^8 + 13, 147*z^13 -637);
+35721*z^8+1911*z^4+5733*z^3+17199*z^2+51597*z+154154;
+
 /* SF bug #3376: fullratsubst doesn't catch infinite recursions
    In this example, fullratsubst1 should loop exactly lrats_max_iter=30 times,
    adding a power of b each time.

commit e3d20bcfcfbd2bc16975010e68a569e86596f32a

Fix bug #4176, bug #4177.
Bug #4176:
Commit c9dd89bd4 introduced a bug where the third form of calling
fullratsubst no longer works:

fullratsubst(listofeqns,expr);

This commit makes that syntax work. To do this,

1. lratsubst1 iterates *only* over a list of substitutions;
2. a fullratsubst2 now does the iteration over successive applications
of lratsubst1;
3. fullratsubst0,1,2 are called by fullratsubst, depending on the
syntactic form of the inputs;

In addition, 7 tests have been added to tests/rtest6a.mac to exercise
all these types of inputs.

Bug #4177:
The test for lratsubst1 has been replaced by the equivalent test for
lratsubst.

share/simplification/lrats.mac
@@ -36,39 +36,31 @@ lratsubst(listofeqns,expr):=block(
 define_variable(lrats_max_iter,100000,integer)$
 qput(lrats_max_iter,lambda([v], if v<=0 then error("lrats_max_iter must be set to a positive integer.")),value_check);
 
-lratsubst1(listofeqns,expr):=block([dum,lrats_iter:0,l],
-  begin_outer_loop,
-  l:listofeqns,
-  while dum='dum do (
-    dum:if l = [] then expr
-    else (if rest(l) = []
-      then ratsubst(inpart(l,1,2),inpart(l,1,1),expr)
-      else 'dum),
-    expr:if fullratsubstflag = true
-    then fullratsubst1(inpart(l,1,2),inpart(l,1,1),expr)
-    else ratsubst(inpart(l,1,2),inpart(l,1,1),expr),
-    l:rest(l)),
-  if fullratsubstflag = true and dum # expr and lrats_iter<lrats_max_iter then (lrats_iter:lrats_iter+1,go(begin_outer_loop))
-  else (if dum # expr then dum else expr))$
+lratsubst1(listofeqns,expr):=block([dum,lrats_iter:0],
+  for eqn in listofeqns do [dum,expr]:[expr,ratsubst(inpart(eqn,2),inpart(eqn,1),expr)],
+  expr)$
 
-fullratsubst1(substexpr,forexpr,expr):=block(
+fullratsubst0(eqn,expr):= fullratsubst1(second(eqn),first(eqn),expr)$
+
+fullratsubst1(substexpr,forexpr,expr):=fullratsubst2([forexpr=substexpr],expr)$
+
+fullratsubst2(listofeqns,expr):=block(
   [dum,lrats_iter:0],
   while lrats_iter<lrats_max_iter and dum#expr do (
-    [dum,expr]:[expr,ratsubst(substexpr,forexpr,expr)],
+    [dum,expr]:[expr,lratsubst1(listofeqns,expr)],
     lrats_iter:1+lrats_iter),
-  if lrats_iter>=lrats_max_iter then warning("fullratsubst1(substexpr,forexpr,expr): reached maximum iterations of",lrats_max_iter,". Increase `lrats_max_iter' to increase this limit."),
+  if lrats_iter>=lrats_max_iter then warning("fullratsubst2(listofeqns,expr): reached maximum iterations of",lrats_max_iter,". Increase `lrats_max_iter' to increase this limit."),
   expr)$
-    
 
 fullratsubst([arglist]):=block(
-  [fullratsubstflag:true,larglistdum:length(arglist),farglist,
-   partswitch:true,inflag:true,piece],
+  [larglistdum:length(arglist),farglist,partswitch:true,inflag:true,piece],
   if larglistdum=2
-  then if listp(farglist:first(arglist)) or inpart(farglist,0)="="
-       then fullratsubst(second(farglist),first(farglist),last(arglist))
-       else error(messlrats2,arglist)
+  then (farglist:first(arglist),
+    if listp(farglist) then fullratsubst2(farglist,second(arglist))
+    else if inpart(farglist,0)="=" then fullratsubst0(farglist,last(arglist))
+    else error(messlrats2,arglist))
   else if larglistdum=3
-       then apply('fullratsubst1,arglist)
-       else error(messlrats2,arglist))$
+  then apply('fullratsubst1,arglist)
+  else error(messlrats2,arglist))$
 
 eval_when(batch,ttyoff:false)$

tests/rtest6a.mac
@@ -172,6 +172,31 @@ fullratsubst(3*z^8 + 13, z^9, 147*z^13 -637);
 fullratsubst(z^9 = 3*z^8 + 13, 147*z^13 -637);
 35721*z^8+1911*z^4+5733*z^3+17199*z^2+51597*z+154154;
 
+fullratsubst([z^9 = 3*z^8 + 13], 147*z^13 -637);
+35721*z^8+1911*z^4+5733*z^3+17199*z^2+51597*z+154154;
+
+
+/* The following are examples from the documentation for
+   fullratsubst, ratsubst and fullratsubstflag
+*/
+lratsubst ([a^2 = b, c^2 = d], (a + e)*c*(a + c));
+(d + a*c)*e + a*d + b*c;
+
+lratsubst (a^2 = b, a^3);
+a*b;
+
+ratsubst (b*a, a^2, a^3);
+a^2*b;
+
+fullratsubst (b*a, a^2, a^3);
+a*b^2;
+
+fullratsubst ([a^2 = b, b^2 = c, c^2 = a], a^3*b*c);
+b;
+
+fullratsubst (a^2 = b*a, a^3);
+a*b^2;
+
 /* SF bug #3376: fullratsubst doesn't catch infinite recursions
    In this example, fullratsubst1 should loop exactly lrats_max_iter=30 times,
    adding a power of b each time.
@@ -191,7 +216,7 @@ c $
 errcatch(lratsubst([[a=b,b=c],[b=c]],a)) $
 [] $
 
-lratsubst1 ([phi_2 = 1.1], 0.001*cos(phi_2)), numer;
+lratsubst ([phi_2 = 1.1], 0.001*cos(phi_2)), numer;
 0.001*cos(11/10);
 
 /* original example from #3658;