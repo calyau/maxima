a36280e4573b07bb896fdf4d11713314295474a7
Raymond Toy <toy.raymond@gmail.com>
2024-01-01 14:24:00 -0800

    Rename *exp* to expr in superexpt
    `superexpt` receives the `*exp*` arg and just passes it on to other
    routines.  It seems like none of the called routines modify it so
    rename it.
    
    Full testsuite + share passes with cmucl.

--- src/sin.lisp
+++ src/sin.lisp
@@ -814,7 +814,7 @@
                       ; stored in a list which is returned from m2.
       (exptflag nil)) ; When T, the substitution is not possible.
   
-  (defun superexpt (*exp* var2 bas1 pow1)
+  (defun superexpt (expr var2 bas1 pow1)
     (prog (y ($logabs nil) (new-var (gensym "NEW-VAR-")))
       (putprop new-var t 'internal)
       (setq base bas1
@@ -823,7 +823,7 @@
       ;; Transform the integrand. At this point resimplify, because it is not
       ;; guaranteed, that a correct simplified expression is returned.
       ;; Use a new variable to prevent facts on the old variable to be wrongly used.
-      (setq y (resimplify (maxima-substitute new-var var2 (elemxpt *exp* var2))))
+      (setq y (resimplify (maxima-substitute new-var var2 (elemxpt expr var2))))
       (when exptflag (return nil))
       ;; Integrate the transformed integrand and substitute back.
       (return
@@ -838,7 +838,7 @@
                                           (take '(%log) base)))
                                 new-var)
                     var2
-                    *exp*)))))
+                    expr)))))
   
   ;; Transform expressions like g^(b*x+a) to the common base base and
   ;; do the substitution y = base^(b*x+a) in the expr.
