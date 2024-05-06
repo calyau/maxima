154c2eb8638fe3639da6cbdc6a4528925dea1f30
Raymond Toy <toy.raymond@gmail.com>
2024-05-05 18:39:12 -0700

    Fix #4298:  Limit the number of terms in the atan sum
    In fpatan, for the case |x| <= 1/2, we use a Taylor series to compute
    atan.  The criteria for termination is if the previous sum equals the
    current sum.  This happens when the additional term is so small that
    it doesn't contribute anymore.  But I think round-off causes the
    previous sum and current sum to alternate so they're never equal.
    
    To fix this, compute a rough estimate of the number of terms needed so
    that the relative error between the term and x is less than a
    tolerance (2^(-fpprec)).  With this change, the series then terminates
    because we reach the limit of the number of terms.
    
    For the test case, we want the atan of -7.7037b-34.  The last term
    computed is 9.888b-1756, which is way smaller than needed, but at
    least we terminate the loop instead of looping forever.

--- src/float.lisp
+++ src/float.lisp
@@ -901,12 +901,23 @@
 	   (t
 	    ;; |x| <= 1/2.  Use Taylor series (A&S 4.4.42, first
 	    ;; formula).
+            ;;
+            ;; The n'th term is (-1)^n*x^(2*n+1)/(2*n+1).  We want to
+            ;; stop summing when the relative error between the n'th
+            ;; term and the first is small.  That is
+            ;; |x^(2*n+1)/(2*n+1)/x| <= tol. Hence, |x^(2*n)|/(2*n+1)
+            ;; <= tol.  Or |x^(2*n)| <= tol.  But we know |x| <= 1/2,
+            ;; so (1/2)^(2*n) <= tol.  Then n = -log(tol)/2/log(2) =
+            ;; -log2(tol)/2.  Since tol is basically 2^(-fpprec), n =
+            ;; fpprec/2.
 	    (setq ans x x2 (fpminus (fptimes* x x)) term x)
-	    (do ((n 3 (+ n 2)))
-		((equal ans oans))
-	      (setq term (fptimes* term x2))
-	      (setq oans ans
-		    ans (fpplus ans (fpquotient term (intofp n)))))))
+            (let ((max-n (floor fpprec 2)))
+	      (do ((n 3 (+ n 2)))
+		  ((or (equal ans oans)
+                       (>= n max-n)))
+	        (setq term (fptimes* term x2))
+	        (setq oans ans
+		      ans (fpplus ans (fpquotient term (intofp n))))))))
      (return ans)))
 
 ;; atan(y/x) taking into account the quadrant.  (Also equal to
