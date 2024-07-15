7e03b6358308e7928c6646b3cf73413ad569c161
Leo Butler <l_butler@users.sourceforge.net>
2024-07-15 12:34:42 -0500

    tests/command-line-options.el: clean-up
    Remove unused functions (maxima-cli-call-process, maxima-cli-deftest),
    rename function (maxima-cli-deftest* -> maxima-cli-deftest) and add
    maxima-cli-deftests.

--- tests/command-line-options.el
+++ tests/command-line-options.el
@@ -33,12 +33,7 @@ Return nil if LINE is zero-length or it matches a regexp in
     (shell-command cmd stdout stderr)
     (list (maxima-cli--buffer-string stdout) (maxima-cli--buffer-string stderr))))
 
-(defun maxima-cli-deftest (plist lisps &optional results)
-  (cond ((null plist)
-	 results)
-	(t
-	 (maxima-cli-deftest (cddr plist) lisps (append (maxima-cli-deftest* plist lisps) results)))))
-(defun maxima-cli-deftest* (plist &optional lisps)
+(defun maxima-cli-deftest (plist &optional lisps)
   (let* ((name (car plist))
 	 (plist (cadr plist))
 	 (lisps (or (plist-get plist :lisps) lisps))
@@ -52,6 +47,12 @@ Return nil if LINE is zero-length or it matches a regexp in
 				    (should (equal (car results) ,(plist-get plist :stdout)))
 				    (should (equal (cadr results) ,(plist-get plist :stderr))))))
 			lisps results))))
+
+(defun maxima-cli-deftests (tests)
+  (let (results)
+    (cl-do ((test tests (cddr test)))
+	((null test) results)
+      (setq results (cons (maxima-cli-deftest test) results)))))
 
 (defvar maxima-cli--tests
   '(maxima-cli--vvq+batch-string
@@ -67,18 +68,8 @@ Return nil if LINE is zero-length or it matches a regexp in
 	  :stdout "(display2d:false,integrate(x^n,x))\nlog(x)" :stderr ""
 	  :exit 1 :lisps (gcl sbcl ecl clisp) :results (t t t t))
     )
-  "A list of property lists of the form (:cmd MAXIMA-COMMAND :stdout EXPECTED-OUTPUT :stderr EXPECTED-ERROR-OUPUT :exit EXPECTED-EXIT-CODE).")
+  "List of tests of command-line switches.
 
-(defun maxima-cli-call-process (maxima-command name args)
-  ""
-  (let ((stdout (format "*%s-stdout*" name))
-	(stderr.out (make-temp-file (format "%s-stderr.out" name)))
-	(stderr (format "%s-stderr.out" name))
-	(args (cons "-c" (cons maxima-command args))))
-    (apply #'call-process (append (list "/usr/bin/echo" nil (list stdout stderr.out) nil) args))
-    (save-excursion
-      (find-file-literally stderr.out)
-      (list (maxima-cli--buffer-string stdout) (maxima-cli--buffer-string (current-buffer))))))
-    
-	
-;;(maxima-cli-call-process maxima-command "test1" (split-string "--lisp=sbcl --very-very-quiet --no-init --batch-string='printf(true,\"HELLO~%%\");1/0;printf(true,\"Integral_xdx=~a~%%\",integrate(x,x));'"))
+A list of property lists of the form (:cmd MAXIMA-COMMAND :stdout
+EXPECTED-OUTPUT :stderr EXPECTED-ERROR-OUPUT :exit
+EXPECTED-EXIT-CODE).")
