3fc4b17af56d97e86a569f8b10b751f8febe87ba
Leo Butler <l_butler@users.sourceforge.net>
2024-10-21 17:42:02 -0500

    src/mload.lisp: add answers_from_file option to run_testsuite
    Fixes bug reported by Wolfgang Dautermann.
    
    Ref: https://sourceforge.net/p/maxima/mailman/maxima-discuss/thread/16214749-58a8-4449-91b3-b18a85a6382f%40dautermann.at/#msg58831037

--- src/mload.lisp
+++ src/mload.lisp
@@ -814,15 +814,17 @@
 		      :test #'string=))
 	   (mwarning "Unknown test: " test)))))
   
-(defmfun $run_testsuite (&key tests display_all display_known_bugs share_tests time debug)
+(defmfun $run_testsuite (&key tests display_all display_known_bugs share_tests time debug answers_from_file)
   "Run the testsuite.  Options are
   tests                List of tests to run
   display_all          Display output from each test entry
-  display_known_bugs   Include tests that are known to fail.
+  display_known_bugs   Include tests that are known to fail
   time                 Display time to run each test entry
   share_tests          Whether to include the share testsuite or not
-  debug                Set to enable some debugging prints.
+  debug                Set to enable some debugging prints
+  answers_from_file    Read interactive answers from source file.
 "
+  (declare (special $batch_answers_from_file))
   (enable-some-lisp-warnings)
   (let ((test-file)
 	(expected-failures)
@@ -836,9 +838,10 @@
       (merror (intl:gettext "run_testsuite: display_all must be true or false; found: ~M") display_all))
     (unless (member time '(t nil $all))
       (merror (intl:gettext "run_testsuite: time must be true, false, or all; found: ~M") time))
-
     (unless (member share_tests '(t nil $only))
       (merror (intl:gettext "run_testsuite: share_tests must be true, false or only: found ~M") share_tests))
+    (unless (member answers_from_file '(t nil))
+      (merror (intl:gettext "run_testsuite: answers_from_file must be true or false only; found ~M") answers_from_file))
     
     (setq *collect-errors* nil)
 
@@ -867,6 +870,7 @@
 	     (test-count 0)
 	     (total-count 0)
 	     (error-count 0)
+	     ($batch_answers_from_file answers_from_file)
 	     filename
 	     diff
 	     upass)
