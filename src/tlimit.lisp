181001fe9094ae6a13eb704c63da248c24432af7
Barton Willis <willisb@unk.edu>
2024-11-23 18:28:16 -0600

    Fix for #4416 limit of Newton quotient involving asin
    This commit introduces a workaround for bug #4416. A proper fix likely requires changes to the Taylor code.
    
        Workaround in tlimp:
            Modified `tlimp` to avoid applying tlimit on expressions containing `asin` or `acos`. Such expressions are instead handled
            by `limit1`, which computes the limit correctly for this case.
    
        Simplification of `tlimit-taylor`:
            Removed the call to `partial-logarc`. Eliminated the local definition of `taylor_simplify` to simplify the code.
    
        Append tests to rtest_limit_extra.mac
    
    These changes fix `rtest_limit_extra.mac` test 256, so remove this as a known failure.

--- src/tlimit.lisp
+++ src/tlimit.lisp
@@ -31,10 +31,18 @@
 ;; Taylor cannot handle conjugate, ceiling, floor, unit_step, or signum 
 ;; expressions, so let's tell tlimit to *not* try. We also disallow 
 ;; expressions containing $ind.
+
+;; We have subst([h=0,x=0], taylor(asin(x+h)-asin(x),h,0,1)) = %pi (see
+;; bug #4416 limit of Newton quotient involving asin). This bug causes
+;; trouble for tlimit((asin(x+h) - asin(x))/h,h,0). Until such bugs are
+;; sorted, we will disallow tlimit from attempting limits involving 
+;; acos and asin.
 (defun tlimp (e x)	
   (or (and ($mapatom e) (not (eq e '$ind)) (not (eq e '$und)))
 	  (and (consp e) 
 	       (consp (car e)) 
+		   (or (not (among x e)) 
+		     (not (member (caar e) '($conjugate $ceiling $floor $unit_step %signum %acos %asin) :test #'eq)))
 	       (or 
 		      (known-ps (caar e)) 
 			  (and (eq (caar e) 'mqapply) (known-ps (subfunname e)))
@@ -44,43 +52,50 @@
 		    (every #'(lambda (q) (tlimp q x)) (cdr e)))))
 
 ;; Dispatch Taylor, but recurse on the order until either the recursion
-;; depth is 15 or the Taylor polynomial is nonzero. When Taylor 
-;; fails to find a nonzero Taylor polynomial or the recursion depth is
-;; too great, return nil.
+;; depth reaches 15 or the Taylor polynomial is nonzero. If Taylor 
+;; fails to find a nonzero Taylor polynomial or the recursion depth 
+;; exceeds the limit, return NIL.
 
 ;; This recursion on the order attempts to handle limits such as 
-;; tlimit(2^n/n^5, n, inf) correctly. 
+;; tlimit(2^n / n^5, n, inf) correctly.
 
-;; We set up a reasonable environment for calling taylor. When $taylor_logexpand 
-;; is true, taylor does some principal branch violating transformations, so we set 
-;; it to nil.
+;; We set up a reasonable environment for calling Taylor. When $taylor_logexpand 
+;; is false, Taylor sometimes generates expressions that vanish but do not readily
+;; simplify to zero. For example:
+;;
+;;   block([taylor_logexpand: false], subst(h=0, taylor(atan(x+h) - atan(x), h, 0, 1)));
+;;
+;; For the limit code, such expressions can cause errors. To avoid this, we set 
+;; taylor_logexpand to true. Previously, this code set the value of $taylor_simplifier
+;; to  #'(lambda (q) (sratsimp (extra-simp q)))), but that doesn't seem to be needed,
+;; so this version doesn't set a value for this option variable.
 
-;; I know of no compelling reason for defaulting the taylor order to 
-;; lhospitallim, but this is documented in the user documentation). 
+;; Also previously, this code applied partial-logarc before calling $taylor. This
+;; too doesn't seem to be needed, so it has been removed.	
+
+;; There is no compelling reason to default the Taylor order to 
+;; lhospitallim, but this is documented in the user documentation.
 
 (defun tlimit-taylor (e x pt n &optional (d 0))
-	(let ((ee 0) 
+	(let ((ee) 
 	      (silent-taylor-flag t) 
 	      ($taylordepth 8)
 		  ($radexpand nil)
-		  ($logexpand nil)
-		  ($taylor_logexpand nil)
-		  ($taylor_simplifier #'(lambda (q) (sratsimp (extra-simp q)))))
-		(setq e (partial-logarc e (list '%atan)))
-	    (setq ee  (catch 'taylor-catch (let (($logexpand t)) (ratdisrep ($taylor e x pt n)))))
+		  ($taylor_logexpand t)
+		  ($logexpand t))
+	    (setq ee (catch 'taylor-catch (ratdisrep ($taylor e x pt n))))
 		(cond ((and ee (not (alike1 ee 0))) ee)
-			  ;; When taylor returns zero and the depth d is less than 16, 
-			  ;; declare a do-over; otherwise return nil.
+			  ;; Retry if taylor returns zero and depth is less than 16
               ((and ee (< d 16))
-			    (tlimit-taylor e x pt (* 4 (max 1 n)) (+ d 1)))
+			    (tlimit-taylor e x pt (* 4 (max 1 n)) (1+ d)))
 			  (t nil))))
 
-;; Previously when the taylor series failed, there was code for deciding
+;; Previously, when the Taylor series failed, there was code to decide
 ;; whether to call limit1 or simplimit. The choice depended on the last
 ;; argument to taylim (previously named *i*) and the main operator of the 
-;; expression. This code dispenses with this logic and dispatches limit1
-;; when Maxima is unable to find the taylor polynomial. This change orphans 
-;; the last argument of taylim.
+;; expression. This updated code eliminates that logic and always dispatches
+;; limit1 when Maxima is unable to find the Taylor polynomial. As a result, 
+;; the last argument of taylim is now unused (orphaned).
 (defun taylim (e var val flag)
     (declare (ignore flag))
 	(let ((et nil))
