37e6ebdd94c64788bf271e83b7baaeeb811f93d8
Raymond Toy <toy.raymond@gmail.com>
2025-01-20 11:34:50 -0800

    Remove old lambert_w simplifier stuff
    All of these have been subsumed by the def-simplifier for lambert_w.

--- src/specfn.lisp
+++ src/specfn.lisp
@@ -639,24 +639,6 @@
 ;;
 ;; See also http://en.wikipedia.org/wiki/Lambert's_W_function
 
-#+nil
-(defmfun $lambert_w (z)
-  (ftake* '%lambert_w z))
-
-;;; Set properties to give full support to the parser and display
-#+nil
-(defprop $lambert_w %lambert_w alias)
-#+nil
-(defprop $lambert_w %lambert_w verb)
-#+nil
-(defprop %lambert_w $lambert_w reversealias)
-#+nil
-(defprop %lambert_w $lambert_w noun)
-
-;;; lambert_w is a simplifying function
-#+nil
-(defprop %lambert_w simp-lambertw operators)
-
 ;;; Derivative of lambert_w
 (defprop %lambert_w
   ((x) 
@@ -677,36 +659,6 @@
      1)
     ((mexpt) ((%lambert_w) x) -1)))
   integral)
-
-#+nil
-(defun simp-lambertw (x yy z)
-  (declare (ignore yy))
-  (oneargcheck x)
-  (setq x (simpcheck (cadr x) z))
-  (cond ((equal x 0) 0)
-	((equal x 0.0) 0.0)
-	((zerop1 x) ($bfloat 0))	;bfloat case
-	((alike1 x '$%e)
-	 ;; W(%e) = 1
-	 1)
-	((alike1 x '((mtimes simp) ((rat simp) -1 2) ((%log simp) 2)))
-	 ;; W(-log(2)/2) = -log(2)
-	 '((mtimes simp) -1 ((%log simp) 2)))
-	((alike1 x '((mtimes simp) -1 ((mexpt simp) $%e -1)))
-	 ;; W(-1/e) = -1
-	 -1)
-	((alike1 x '((mtimes) ((rat) -1 2) $%pi))
-	 ;; W(-%pi/2) = %i*%pi/2
-	 '((mtimes simp) ((rat simp) 1 2) $%i $%pi))
-        ;; numerical evaluation
-	((complex-float-numerical-eval-p x)
-          ;; x may be an integer.  eg "lambert_w(1),numer;"
-	  (if (integerp x)
-	    (to (bigfloat::lambert-w-k 0 (bigfloat:to ($float x))))
-	    (to (bigfloat::lambert-w-k 0 (bigfloat:to x)))))
-	((complex-bigfloat-numerical-eval-p x)
-	 (to (bigfloat::lambert-w-k 0 (bigfloat:to x))))
-	(t (list '(%lambert_w simp) x))))
 
 (def-simplifier lambert_w (x)
   (cond ((equal x 0) 0)
