5e797a0ceed34b3e9345f0a308e18ed48fbb708d
David Scherfgen <d.scherfgen@googlemail.com>
2026-09-15 14:24:48 +0200

    Make csign(imaginary^...) work exponents involving variables declared even/odd
    It used to give precise answers only for literal integer exponents, not
    for exponents involving variables that are declared even/odd. Most of
    the information was there already - the parity of the exponent was
    already computed, not only for integers. The mod(..., 4) check has been
    changed by calling EVOD on the exponent divided by 2 distributed.
    
    The MINUS special variable was set incorrectly by the old code, but this
    was never noticed, because it is only relevant to asksign(), which works
    in real mode.

--- src/compar.lisp
+++ src/compar.lisp
@@ -1981,11 +1981,14 @@ TDNEG TDZERO TDPN) to store it, and also sets SIGN."
 	   (when *debug-compar*
 	     (format t "~&in SIGN-MEXPT for ~A, base is $imaginary.~%" x))
 	   (cond
-	     ((and (integerp expt) (eq evod '$even))
-	      (setq sign (if (eql (mod expt 4) 0) '$pz '$nz)))
-	     ((and (integerp expt) (eq evod '$odd))
+	     ((eq evod '$even)
+	      (let ((evod-half (evod ($distrib (div expt 2)))))
+	        (setq sign (cond ((eq evod-half '$even) '$pz)
+	                         ((eq evod-half '$odd) '$nz)
+	                         (t '$pnz)))))
+	     ((eq evod '$odd)
 	      (setq sign '$imaginary
-		    minus (if (eql (mod (- expt 1) 4) 0) t nil)))
+	            minus (eq (evod ($distrib (div (sub expt 1) 2))) '$odd)))
 	     (t (setq sign '$complex))))
 
 	  ((and *complexsign*
